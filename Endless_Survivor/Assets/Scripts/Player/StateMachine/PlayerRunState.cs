f664f480e4407598b9e62f746a9510353ba1626f
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-16 17:32:17 -0300

    feat(PlayerRunState): Applying movement values to animation

--- Endless_Survivor/Assets/Scripts/Player/StateMachine/PlayerRunState.cs
+++ Endless_Survivor/Assets/Scripts/Player/StateMachine/PlayerRunState.cs
@@ -14,6 +14,8 @@ public class PlayerRunState : PlayerBaseState
         CheckSwitchStates();
 
         Vector2 movement = (context.Movement * context.RunSpeed * Time.deltaTime).normalized;
+        context.PlayerAnimator.SetFloat("Horizontal", movement.x);
+        context.PlayerAnimator.SetFloat("Vertical", movement.y);
         context.PlayerRb.velocity = new Vector3(movement.x, movement.y);
     }
 
