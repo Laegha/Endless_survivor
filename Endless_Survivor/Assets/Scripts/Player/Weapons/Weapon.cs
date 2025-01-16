9f6885db7711e0b12c4ca2c7efb31794840b005b
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-16 20:43:41 -0300

    feat(Weapon): range stat, specific for each weapon

--- Endless_Survivor/Assets/Scripts/Player/Weapons/Weapon.cs
+++ Endless_Survivor/Assets/Scripts/Player/Weapons/Weapon.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] float _range;
+
+    public float Range { get { return _range; } }
     public void Attack()
     {
 
