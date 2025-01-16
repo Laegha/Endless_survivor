1b3920822ffc2d37ef7a4b773f4ed888b2b2ac27
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-15 22:38:04 -0300

    feat(MainMenu): script that manages gameflow

---
+++ Endless_Survivor/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject charSelectMenu;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OpenCharSelect()
+    {
+        charSelectMenu.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void CloseCharSelect() 
+    {
+        charSelectMenu.SetActive(false);
+        gameObject.SetActive(true);
+    }
+}
