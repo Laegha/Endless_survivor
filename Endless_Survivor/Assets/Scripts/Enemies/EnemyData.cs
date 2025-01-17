a43316eda9d6473471a58c9c04fb3be5aa576801
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-17 20:02:30 -0300

    feat(EnemyData): Scriptable Object to make new enemies

---
+++ Endless_Survivor/Assets/Scripts/Enemies/EnemyData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyData : ScriptableObject
+{
+    [SerializeField]int _initialHP;
+    [SerializeField] GameObject _enemyPrefab;
+
+    public int InitialHP { get { return _initialHP; } }
+    public GameObject EnemyPrefab { get { return _enemyPrefab; } }
+}
