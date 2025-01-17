5903d6f3257ab360513370abefeea0b2ff7731dc
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-17 19:59:18 -0300

    feat(Weapon): shoot cooldown behaviour and storing stats on a WeaponStats that copies WeaponData

--- Endless_Survivor/Assets/Scripts/Player/Weapons/Weapon.cs
+++ Endless_Survivor/Assets/Scripts/Player/Weapons/Weapon.cs
@@ -4,11 +4,36 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    [SerializeField] float _range;
+    float _shootCooldown;
+    bool _canShoot;
+    [SerializeField] WeaponData _data;
+    WeaponStats _weaponStats;
 
-    public float Range { get { return _range; } }
-    public void Attack()
+    public WeaponData Data {  get { return _data; } }
+
+    private void Start()
     {
+        _weaponStats = new WeaponStats(_data.WeaponStats);
+    }
 
+    private void Update()
+    {
+        if (_shootCooldown > 0)
+        {
+            _shootCooldown -= Time.deltaTime;
+            return;
+        }
+        if(!_canShoot)
+            _canShoot = true;
+    }
+
+
+    public virtual void Attack()
+    {
+        if (!_canShoot)
+            return;
+
+        _canShoot = false;
+        _shootCooldown = 1/(Data.WeaponStats.AttackSpeed + GameManager.gm.selectedCharacter.PlayerStats.AttackSpeed);
     }
 }
