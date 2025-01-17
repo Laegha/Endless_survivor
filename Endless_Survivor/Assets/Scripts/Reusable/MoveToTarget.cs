ca9a58579aee30ec0fd4c45d8ec298b15bf68600
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-17 19:00:05 -0300

    refactor(Changed version): Moved from 2022.3.16 to 2022.3.56
    This was for the sdk to install properly, since archive versions behave a bit weird sometimes

--- Endless_Survivor/Assets/Scripts/Reusable/MoveToTarget.cs
+++ Endless_Survivor/Assets/Scripts/Reusable/MoveToTarget.cs
@@ -17,7 +17,7 @@ public class MoveToTarget : MonoBehaviour
 
     void Update()
     {
-        Vector2 direction = _target.position - transform.position;
+        Vector2 direction = (_target.position - transform.position).normalized;
         _rb.velocity = direction * _moveSpeed * Time.deltaTime;
     }
 }
