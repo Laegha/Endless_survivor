c228f99998ab4f131e5422d943d468a463ad1e41
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-18 02:44:56 -0300

    feat(PlayerSpawner): Initial weapons generation

--- Endless_Survivor/Assets/Scripts/Player/PlayerSpawner.cs
+++ Endless_Survivor/Assets/Scripts/Player/PlayerSpawner.cs
@@ -6,8 +6,18 @@ public class PlayerSpawner : MonoBehaviour
 {
     void Start()
     {
-        GameObject player = Instantiate(GameManager.gm.selectedCharacter.CharacterPrefab, transform.position, Quaternion.identity);
+        CharacterData selectedChar = GameManager.gm.selectedCharacter;
+        GameObject player = Instantiate(selectedChar.CharacterPrefab, transform.position, Quaternion.identity);
         GameManager.gm.player = player.transform;
+        
+        PlayerWeaponManager playerWeaponManager = player.GetComponent<PlayerWeaponManager>();
+        
         //generate initial weapons and passives
+        foreach(WeaponData weaponData in selectedChar.InitialWeapons)
+        {
+            playerWeaponManager.PickupGun(weaponData.WeaponPrefab);
+
+        }
+
     }
 }
