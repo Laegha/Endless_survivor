d82b7a05a11e5424fd78087d046c886898d2d4a5
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-01-18 02:46:15 -0300

    feat(WeaponData): weaponPrefab

--- Endless_Survivor/Assets/Scripts/Player/Weapons/WeaponData.cs
+++ Endless_Survivor/Assets/Scripts/Player/Weapons/WeaponData.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Weapon", menuName = "ScriptableObjects/Weapon", order = 2)]
 public class WeaponData : ScriptableObject
 {
     [SerializeField] WeaponStats _weaponStats;
+    [SerializeField] GameObject _weaponPrefab;
 
     public WeaponStats WeaponStats { get { return _weaponStats; } }
+    public GameObject WeaponPrefab { get { return _weaponPrefab; } }
 }
