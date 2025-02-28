8f9ad9d077847ee26601739dc0d2a38d7354ebdb
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-02-28 19:22:23 -0300

    feat(Joystic): Imported package and input reader that transfers data to state machine

---
+++ Endless_Survivor/Assets/Scripts/Player/PlayerInputReader.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputReader : MonoBehaviour
+{
+    [SerializeField] PlayerStateMachine _playerStateMachine;
+    bl_Joystick joystick;
+
+    private void Start()
+    {
+        joystick = FindObjectOfType<bl_Joystick>();
+    }
+
+    void Update()
+    {
+        Vector2 movement = new Vector2(joystick.Horizontal, joystick.Vertical);
+        print(movement);
+        _playerStateMachine.Movement = movement.normalized;
+    }
+}
