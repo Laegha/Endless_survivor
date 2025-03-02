14a920a1fbd80bb879520ccac5b7b3086f2faaca
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-02 19:41:13 -0300

    feat(WeaponDataTransferInterface): class that passes data when spawning a weapon
    This class will be inherited by specific weapons data transfer, since every weapon type behaves differently

---
+++ Endless_Survivor/Assets/Scripts/Player/Weapons/WeaponDataTransferInterface.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDataTransferInterface
+{
+    public virtual void TransferData(GameObject weaponObject, WeaponData weaponData)
+    {
+        weaponObject.GetComponent<Weapon>().WeaponStats = new WeaponStats(weaponData.WeaponStats);
+    }
+}
