6033c19e40a1f3a9648cb188ce425f5082115c1b
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-02 16:01:19 -0300

    feat(CustomAnimation): Names

--- Endless_Survivor/Assets/Scripts/Player/Animator/CustomAnimation.cs
+++ Endless_Survivor/Assets/Scripts/Player/Animator/CustomAnimation.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 [System.Serializable]
 public class CustomAnimation
 {
+    [SerializeField] string _animationName;
     [SerializeField]Sprite[] _frames;
     [SerializeField]float _framesPerSecond;
 
+    public string AnimationName {  get { return _animationName; } }
     public Sprite[] Frames {  get { return _frames; } }
     public float FramesPerSecond {  get { return _framesPerSecond; } }
 }
