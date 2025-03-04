44e07a8cfbb777abaaf88717f1fd82c3e4709f6b
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-04 20:52:56 -0300

    refactor(PlayerWeaponManager): Generate default hands, then change the sprite

--- Endless_Survivor/Assets/Scripts/Player/PlayerWeaponManager.cs
+++ Endless_Survivor/Assets/Scripts/Player/PlayerWeaponManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerWeaponManager : MonoBehaviour
 {
     [SerializeField] GameObject _weaponPrefab;
+    [SerializeField] GameObject _handPrefab;
     [SerializeField] Transform[] _gunPositions;
     [SerializeField] Transform _gunsHolder;
     List<Transform> _heldGuns = new List<Transform>();
@@ -20,10 +21,11 @@ public class PlayerWeaponManager : MonoBehaviour
     {
         Vector2 handPosition = _gunPositions[_heldGuns.Count].position;
 
-        GameObject hand = Instantiate(GameManager.gm.selectedCharacter.CharacterHands[Random.Range(0, GameManager.gm.selectedCharacter.CharacterHands.Length)], handPosition, Quaternion.identity);
-        hand.transform.SetParent(_gunsHolder);
         Transform newGun = Instantiate(_weaponPrefab, handPosition, Quaternion.identity).transform;
-        newGun.SetParent(hand.transform);
+        newGun.transform.SetParent(_gunsHolder);
+        GameObject hand = Instantiate(_handPrefab, handPosition, Quaternion.identity);
+        hand.GetComponent<SpriteRenderer>().sprite = GameManager.gm.selectedCharacter.CharacterHands[Random.Range(0, GameManager.gm.selectedCharacter.CharacterHands.Length)];
+        hand.transform.SetParent(newGun.transform);
         _heldGuns.Add(newGun);
         weaponData.WeaponDataTransferInterface.TransferData(newGun.gameObject, weaponData);
     }
