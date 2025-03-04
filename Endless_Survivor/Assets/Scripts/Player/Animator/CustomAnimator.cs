4a3e6ddeed801c4077d9928acbff0260fe86683d
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-04 20:18:26 -0300

    feat(CustomAnimator): CustomAnimation list
    Allows for using the CustomAnimator itself and no child classes if no further behaviour is needed

--- Endless_Survivor/Assets/Scripts/Player/Animator/CustomAnimator.cs
+++ Endless_Survivor/Assets/Scripts/Player/Animator/CustomAnimator.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CustomAnimator : MonoBehaviour
 {
+    List<CustomAnimation> _animations;
     CustomAnimation _currAnim;
     int _currFrameIndex;
     [SerializeField] SpriteRenderer _spriteRenderer;
     float _animTimer;
 
     public CustomAnimation CurrAnim { get {  return _currAnim; } set { _currAnim = value; } }
+    public List<CustomAnimation> Animations{ get {  return _animations; } set { _animations = value; } }
+
+    public virtual void AddAnimations(List<CustomAnimation> newAnimations) => newAnimations.ForEach(anim => _animations.Add(anim));
+
     void Update()
     {
         _animTimer -= Time.time;
@@ -25,11 +31,16 @@ public class CustomAnimator : MonoBehaviour
         _spriteRenderer.sprite = _currAnim.Frames[_currFrameIndex];
         _currFrameIndex++;
         if(_currFrameIndex >= _currAnim.Frames.Length)
+        {
             _currFrameIndex = 0;
+            CurrAnim.AnimationEnded(this);
+        }
     }
 
-    public virtual void ChangeAnim(string anim)
+    public virtual void ChangeAnim(string animName)
     {
         _animTimer = 0;
+        CurrAnim = _animations.Where(anim => anim.AnimationName == animName).ToList()[0];
     }
+
 }
