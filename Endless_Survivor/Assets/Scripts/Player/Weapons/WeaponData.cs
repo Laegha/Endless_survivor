cc549e01c9963c825bf7e00679be09aa8c9c7260
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-04 20:38:45 -0300

    feat(WeaponData): Idle and Attack animation

--- Endless_Survivor/Assets/Scripts/Player/Weapons/WeaponData.cs
+++ Endless_Survivor/Assets/Scripts/Player/Weapons/WeaponData.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Weapon", menuName = "ScriptableObjects/Weapon", order = 2)]
@@ -16,7 +17,19 @@ public class WeaponData : ScriptableObject
     [SerializeField] IWeaponType _weaponType = IWeaponType.Proyectile;
     [SerializeReference]WeaponDataTransferInterface _weaponDataTransferInterface;
 
+    [InspectorLabel("Animations")]
+    [SerializeField] CustomAnimation _idleAnimation;
+    [SerializeField] ChangeOnEndAnimation _attackAnimation;
+
     public WeaponStats WeaponStats { get { return _weaponStats; } }
     public WeaponDataTransferInterface WeaponDataTransferInterface { get { return _weaponDataTransferInterface; } set { _weaponDataTransferInterface = value; } }
     public IWeaponType WeaponType { get { return _weaponType; } set { _weaponType = value; } }
+    public List<CustomAnimation> Animations { get { 
+            return new List<CustomAnimation>
+            {
+                _idleAnimation,
+                _attackAnimation
+            };
+        }
+    }
 }
