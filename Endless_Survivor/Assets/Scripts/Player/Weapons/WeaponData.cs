d7b9434adf37be1c950dc912b743d257e9cfae22
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-03 21:05:08 -0300

    refactor(WeaponData): WeaponType and Data Transfer public

--- Endless_Survivor/Assets/Scripts/Player/Weapons/WeaponData.cs
+++ Endless_Survivor/Assets/Scripts/Player/Weapons/WeaponData.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Weapon", menuName = "ScriptableObjects/Weapon", order = 2)]
 public class WeaponData : ScriptableObject
 {
-    enum WeaponTypes
+    public enum IWeaponType
     {
         Proyectile,
         Ray,
         Custom
     }
+
     [SerializeField] WeaponStats _weaponStats;
-    WeaponDataTransferInterface _weaponDataTransferInterface;
+    [SerializeField] IWeaponType _weaponType = IWeaponType.Proyectile;
+    [SerializeReference]WeaponDataTransferInterface _weaponDataTransferInterface;
 
     public WeaponStats WeaponStats { get { return _weaponStats; } }
-    public WeaponDataTransferInterface WeaponDataTransferInterface { get { return _weaponDataTransferInterface; } }
+    public WeaponDataTransferInterface WeaponDataTransferInterface { get { return _weaponDataTransferInterface; } set { _weaponDataTransferInterface = value; } }
+    public IWeaponType WeaponType { get { return _weaponType; } set { _weaponType = value; } }
 }
