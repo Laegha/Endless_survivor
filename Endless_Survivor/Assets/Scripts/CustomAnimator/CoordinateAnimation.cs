ac56c3163f2af31e26401d66f800e61dbfae23d8
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-10 00:06:45 -0300

    feat(CoordinateAnimation): made Serializable

--- Endless_Survivor/Assets/Scripts/CustomAnimator/CoordinateAnimation.cs
+++ Endless_Survivor/Assets/Scripts/CustomAnimator/CoordinateAnimation.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public class CoordinateAnimation : CustomAnimation
 {
     [SerializeField] Vector2 _coordinates;
