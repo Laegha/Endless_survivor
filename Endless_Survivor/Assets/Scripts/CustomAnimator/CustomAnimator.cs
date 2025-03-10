921a196ff55f7f217a776faceb2f03efe3b579d6
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-09 23:56:11 -0300

    fix(CustomAnimator): using deltaTime for frameTimer and changing sprite after increasing index

--- Endless_Survivor/Assets/Scripts/CustomAnimator/CustomAnimator.cs
+++ Endless_Survivor/Assets/Scripts/CustomAnimator/CustomAnimator.cs
@@ -9,16 +9,17 @@ public class CustomAnimator : MonoBehaviour
     CustomAnimation _currAnim;
     int _currFrameIndex;
     [SerializeField] SpriteRenderer _spriteRenderer;
-    float _animTimer;
+    float _animTimer = 0;
 
     public CustomAnimation CurrAnim { get {  return _currAnim; } set { _currAnim = value; } }
     public List<CustomAnimation> Animations{ get {  return _animations; } set { _animations = value; } }
+    public float AnimTimer { get { return _animTimer; } set { _animTimer = value; } }
 
     public virtual void AddAnimations(List<CustomAnimation> newAnimations) => newAnimations.ForEach(anim => _animations.Add(anim));
 
     void Update()
     {
-        _animTimer -= Time.time;
+        _animTimer -= Time.deltaTime;
         if(_animTimer <= 0)
         {
             NextFrame();
@@ -28,19 +29,19 @@ public class CustomAnimator : MonoBehaviour
 
     public void NextFrame()
     {
-        _spriteRenderer.sprite = _currAnim.Frames[_currFrameIndex];
         _currFrameIndex++;
         if(_currFrameIndex >= _currAnim.Frames.Length)
         {
             _currFrameIndex = 0;
             CurrAnim.OnAnimationEnd?.Invoke(this);
         }
+        _spriteRenderer.sprite = _currAnim.Frames[_currFrameIndex];
     }
 
     public virtual void ChangeAnim(string animName)
     {
+        _currAnim = _animations.Where(anim => anim.AnimationName == animName).ToList()[0];
         _animTimer = 0;
-        CurrAnim = _animations.Where(anim => anim.AnimationName == animName).ToList()[0];
     }
 
 }
