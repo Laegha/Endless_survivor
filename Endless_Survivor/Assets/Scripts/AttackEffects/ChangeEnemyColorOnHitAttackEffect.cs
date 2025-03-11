50ab59cf01b49674517ef736937f9d69952f59ab
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-11 12:28:15 -0300

    feat(ChangeEnemyColorOnHitAttackEffect)

---
+++ Endless_Survivor/Assets/Scripts/AttackEffects/ChangeEnemyColorOnHitAttackEffect.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeEnemyColorOnHitAttackEffect : AttackEffect
+{
+    public override void OnEnemyHit(EnemyControl hitEnemyControl)
+    {
+        base.OnEnemyHit(hitEnemyControl);
+        //hitEnemyControl.Anim
+    }
+}
