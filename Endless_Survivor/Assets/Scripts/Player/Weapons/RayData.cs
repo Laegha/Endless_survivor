9f6d1ed0b0c26c4e7823fa9b6d06ce8f61303fb8
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-13 00:15:39 -0300

    fix(RayData): Erased Monobehaviour

--- Endless_Survivor/Assets/Scripts/Player/Weapons/RayData.cs
+++ Endless_Survivor/Assets/Scripts/Player/Weapons/RayData.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RayData : MonoBehaviour
+[Serializable]
+public class RayData
 {
     [SerializeField] Material _rayMaterial;
     [SerializeField] float _rayStartWidth;
