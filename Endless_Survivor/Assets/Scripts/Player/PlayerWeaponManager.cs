d7dbded8995605d4e9e4985ffafef06fc8a292fc
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-13 00:16:45 -0300

    feat(PlayerWeaponManager): Setting PlayerControl on generated weapons and using PrefabHolder

--- Endless_Survivor/Assets/Scripts/Player/PlayerWeaponManager.cs
+++ Endless_Survivor/Assets/Scripts/Player/PlayerWeaponManager.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class PlayerWeaponManager : MonoBehaviour
 {
-    [SerializeField] GameObject _weaponPrefab;
-    [SerializeField] GameObject _handPrefab;
     [SerializeField] Transform[] _gunPositions;
     [SerializeField] Transform _gunsHolder;
+    [SerializeField] PlayerControl _playerControl;
     List<Transform> _heldGuns = new List<Transform>();
 
     public void PickupGun(WeaponData weaponData)
@@ -21,12 +20,13 @@ public class PlayerWeaponManager : MonoBehaviour
     {
         Vector2 handPosition = _gunPositions[_heldGuns.Count].position;
 
-        Transform newGun = Instantiate(_weaponPrefab, handPosition, Quaternion.identity).transform;
-        newGun.transform.SetParent(_gunsHolder);
-        GameObject hand = Instantiate(_handPrefab, handPosition, Quaternion.identity);
+        Transform newWeapon = Instantiate(GameManager.gm.prefabHolder.Prefabs["Weapon"], handPosition, Quaternion.identity).transform;
+        newWeapon.transform.SetParent(_gunsHolder);
+        GameObject hand = Instantiate(GameManager.gm.prefabHolder.Prefabs["Hand"], handPosition, Quaternion.identity);
         hand.GetComponent<SpriteRenderer>().sprite = GameManager.gm.selectedCharacter.CharacterHands[Random.Range(0, GameManager.gm.selectedCharacter.CharacterHands.Length)];
-        hand.transform.SetParent(newGun.transform);
-        _heldGuns.Add(newGun);
-        weaponData.WeaponDataTransferInterface.TransferData(newGun.gameObject, weaponData);
+        hand.transform.SetParent(newWeapon.transform);
+        _heldGuns.Add(newWeapon);
+        weaponData.WeaponDataTransferInterface.TransferData(newWeapon.gameObject, weaponData);
+        newWeapon.GetComponent<Weapon>().PlayerControl = _playerControl;
     }
 }
