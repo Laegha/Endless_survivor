cebfa39b1d61be36853579eee879580bfea64fdd
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-15 16:22:22 -0300

    feat(EnemyBehaviour): base class for scritps that will handle transfering behaviour to an enemy gameobject

---
+++ Endless_Survivor/Assets/Scripts/Enemies/EnemyBehaviour.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[Serializable]
+public class EnemyBehaviour
+{
+    public virtual void TransferData(EnemyControl enemyControl)
+    {
+
+    }
+}
