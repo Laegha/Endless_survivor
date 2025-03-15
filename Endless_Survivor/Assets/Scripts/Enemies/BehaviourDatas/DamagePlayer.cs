7ae53c8c15238d8f196eb65cd95dcd81dd8c144e
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-15 16:23:37 -0300

    feat(DamagePlayer): EnemyBehaviour that creates a damage collider on the enemy. Actual damage is still not supported

---
+++ Endless_Survivor/Assets/Scripts/Enemies/BehaviourDatas/DamagePlayer.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class DamagePlayer : EnemyBehaviour
+{
+    [SerializeReference] float _damage;
+    public override void TransferData(EnemyControl enemyControl)
+    {
+        base.TransferData(enemyControl);
+
+        GameObject damageColliderGO = new GameObject("DamageCollider");
+        damageColliderGO.transform.SetParent(enemyControl.transform, false);
+        CapsuleCollider2D regularCollider = enemyControl.CapsuleCollider;
+        CapsuleCollider2D damageCollider = damageColliderGO.AddComponent<CapsuleCollider2D>();
+
+        damageCollider.direction = regularCollider.direction;
+        damageCollider.size = regularCollider.size * 0.9f;
+        damageCollider.isTrigger = true;
+        //damageColliderGO.AddComponent<>
+        damageColliderGO.transform.localPosition = regularCollider.transform.localPosition;
+    }
+}
