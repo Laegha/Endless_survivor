067f72bcd43b64e69fba81ab847ad667339bd04f
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-15 16:29:08 -0300

    feat(EnemyDataEditor): Provides a system to add different behaviours to the EnemyBehaviour list on EnemyData by showing the different subclasses of EnemyBehaviour

---
+++ Endless_Survivor/Assets/Editor/Scripts/EnemyDataEditor.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(EnemyData))]
+public class EnemyDataEditor : Editor
+{
+    Dictionary<Type, bool> _behaviourTypes = new Dictionary<Type, bool>();
+    
+    SerializedProperty _behaviours;
+    private void OnEnable()
+    {
+        _behaviours = serializedObject.FindProperty("_enemyBehaviours");
+        List<Type> behaviourTypes = Utility.GetSubclassesOf(typeof(EnemyBehaviour));
+        EnemyData enemyData = (EnemyData)target;
+        behaviourTypes.ForEach(type => _behaviourTypes.Add(type, enemyData.EnemyBehaviours.Exists(behaviour => behaviour.GetType() == type)));
+    }
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        serializedObject.Update();
+
+        EnemyData enemyData = (EnemyData)target;
+
+        SyncBehaviourTypes(enemyData);
+
+        if (GUILayout.Button("Add Behaviour"))
+        {
+            GenericMenu menu = new GenericMenu();
+            foreach (var type in _behaviourTypes)
+            {
+                if (type.Value)
+                    continue;
+
+                menu.AddItem(new GUIContent(type.Key.Name), false, () =>
+                {
+                    EnemyBehaviour newBehaviour = (EnemyBehaviour)Activator.CreateInstance(type.Key);
+                    enemyData.EnemyBehaviours.Add(newBehaviour);
+                    EditorUtility.SetDirty(enemyData);
+                });
+            }
+            menu.ShowAsContext();
+        }
+
+        serializedObject.ApplyModifiedProperties();
+
+    }
+
+    private void SyncBehaviourTypes(EnemyData enemyData)
+    {
+        // Reiniciar el diccionario antes de actualizar
+        foreach (var key in new List<Type>(_behaviourTypes.Keys))
+        {
+            _behaviourTypes[key] = false;
+        }
+
+        // Marcar los tipos presentes en la lista actual
+        foreach (var behaviour in enemyData.EnemyBehaviours)
+        {
+            if(behaviour == null) continue;
+            _behaviourTypes[behaviour.GetType()] = true;
+        }
+    }
+}
