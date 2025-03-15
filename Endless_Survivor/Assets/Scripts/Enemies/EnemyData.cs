f3df0e35d5ed73e971a7edbcacbaf6c3fc74f87d
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-15 16:24:28 -0300

    feat(EnemyData): EnemyBehaviour list, initial hp and enemy collider data

--- Endless_Survivor/Assets/Scripts/Enemies/EnemyData.cs
+++ Endless_Survivor/Assets/Scripts/Enemies/EnemyData.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Enemy", menuName = "ScriptableObjects/Enemy", order = 3)]
 public class EnemyData : ScriptableObject
 {
-    [SerializeField]int _initialHP;
-    [SerializeField] GameObject _enemyPrefab;
+    [SerializeField] int _initialHP;
+    [SerializeField] Vector2 _colliderSize;
+    [SerializeField] CapsuleDirection2D _colliderDirection;
+    [SerializeReference] List<EnemyBehaviour> _enemyBehaviours;
 
     public int InitialHP { get { return _initialHP; } }
-    public GameObject EnemyPrefab { get { return _enemyPrefab; } }
+    public Vector2 ColliderSize {  get { return _colliderSize; } }
+    public List<EnemyBehaviour> EnemyBehaviours { get { return _enemyBehaviours; } }
+
+    public virtual void TransferEnemyData(GameObject enemy)
+    {
+        EnemyControl enemyControl = enemy.GetComponent<EnemyControl>();
+        enemyControl.EnemyHP.LeftHP = _initialHP + WaveManager.wm.LapsedWaves * 3;
+        enemyControl.CapsuleCollider.direction = _colliderDirection;
+        enemyControl.CapsuleCollider.size = _colliderSize;
+        foreach(var enemyBehaviour in _enemyBehaviours)
+        {
+            //enemyBehaviour.TransferData(enemyControl);
+        }
+    }
 }
