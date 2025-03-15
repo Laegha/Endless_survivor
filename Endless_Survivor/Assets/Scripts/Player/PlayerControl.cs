4a31fe7d6588674173386871c822a7df080bd049
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-15 19:54:32 -0300

    feat(PlayerControl): Reference to HPManager

--- Endless_Survivor/Assets/Scripts/Player/PlayerControl.cs
+++ Endless_Survivor/Assets/Scripts/Player/PlayerControl.cs
@@ -7,12 +7,15 @@ public class PlayerControl : MonoBehaviour
     [SerializeField] PlayerAnimator _playerAnimator;
     PlayerStats _playerStats;
     [SerializeField] Rigidbody2D _playerRb;
+    [SerializeField] PlayerHPManager _playerHPManager;
     public PlayerAnimator PlayerAnimator { get { return _playerAnimator; } }
     public PlayerStats PlayerStats { get { return _playerStats; } set { _playerStats = value; } }
     public Rigidbody2D PlayerRb { get { return _playerRb; } }
+    public PlayerHPManager PlayerHPManager { get { return _playerHPManager; } }
 
     private void Start()
     {
         _playerStats = new PlayerStats(GameManager.gm.selectedCharacter.PlayerStats);
+        _playerHPManager.Initialize(this);
     }
 }
