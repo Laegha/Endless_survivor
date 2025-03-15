375b0062cddb0c799ba1dfbc4fc221e897f66342
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-15 16:26:00 -0300

    feat(MoveToTarget): made variables public so it could be set from another script

--- Endless_Survivor/Assets/Scripts/Reusable/MoveToTarget.cs
+++ Endless_Survivor/Assets/Scripts/Reusable/MoveToTarget.cs
@@ -9,6 +9,9 @@ public class MoveToTarget : MonoBehaviour
     Transform _target;
     Rigidbody2D _rb;
 
+    public float MoveSpeed { get { return _moveSpeed; } set { _moveSpeed = value; } }
+    public string TargetTag { get { return _targetTag; } set { _targetTag = value; } }
+
     void Start()
     {
         _target = GameObject.FindWithTag(_targetTag).transform;
