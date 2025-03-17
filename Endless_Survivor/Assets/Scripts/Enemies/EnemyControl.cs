39f9e19a2f7b53912f85ac9c8a02e17b8daba2a3
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-17 19:14:32 -0300

    feat(EnemyControl): CustomAnimator

--- Endless_Survivor/Assets/Scripts/Enemies/EnemyControl.cs
+++ Endless_Survivor/Assets/Scripts/Enemies/EnemyControl.cs
@@ -7,14 +7,16 @@ public class EnemyControl : MonoBehaviour
     [SerializeField] SpriteRenderer _spriteRenderer;
     [SerializeField] EnemyHP _enemyHP;
     [SerializeField] CapsuleCollider2D _capsuleCollider;
+    [SerializeField] CustomAnimator _customAnimator;
     List<EnemyStatusEffect> _currentEffects = new List<EnemyStatusEffect>();
 
 
     public EnemyHP EnemyHP { get { return _enemyHP; } }
     public CapsuleCollider2D CapsuleCollider { get { return _capsuleCollider; } }
+    public CustomAnimator CustomAnimator { get { return _customAnimator; } }
     void Start()
     {
-        
+        _customAnimator.ChangeAnim("Idle");
     }
 
     void Update()
--- Endless_Survivor/Assets/Scripts/Enemies/EnemyData.cs
+++ Endless_Survivor/Assets/Scripts/Enemies/EnemyData.cs
@@ -10,18 +10,24 @@ public class EnemyData : ScriptableObject
     [SerializeField] Vector2 _colliderOffset;
     [SerializeField] CapsuleDirection2D _colliderDirection;
     [SerializeReference] List<EnemyBehaviour> _enemyBehaviours;
+    [SerializeField] List<CustomAnimation> _customAnimations;
 
     public int InitialHP { get { return _initialHP; } }
     public Vector2 ColliderSize {  get { return _colliderSize; } }
     public List<EnemyBehaviour> EnemyBehaviours { get { return _enemyBehaviours; } }
+    public List<CustomAnimation> CustomAnimations { get { return _customAnimations; } }
 
     public virtual void TransferEnemyData(GameObject enemy)
     {
         EnemyControl enemyControl = enemy.GetComponent<EnemyControl>();
         enemyControl.EnemyHP.LeftHP = _initialHP + WaveManager.wm.CurrWave * 3;
+       
         enemyControl.CapsuleCollider.direction = _colliderDirection;
         enemyControl.CapsuleCollider.size = _colliderSize;
         enemyControl.CapsuleCollider.offset = _colliderOffset;
+
+        enemyControl.CustomAnimator.AddAnimations(_customAnimations);
+
         foreach(var enemyBehaviour in _enemyBehaviours)
         {
             enemyBehaviour.TransferData(enemyControl);
