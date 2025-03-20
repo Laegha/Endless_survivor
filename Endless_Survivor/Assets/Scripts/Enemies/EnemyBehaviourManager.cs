484727d60916d997470a8d37c190e6b2cf237cbb
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-19 21:08:21 -0300

    feat(EnemyBehaviourManager): Handling behaviour overriding, killing and initialization

--- Endless_Survivor/Assets/Scripts/Enemies/EnemyBehaviourManager.cs
+++ Endless_Survivor/Assets/Scripts/Enemies/EnemyBehaviourManager.cs
@@ -12,21 +12,30 @@ public class EnemyBehaviourManager : MonoBehaviour
     void Update()
     {
         _behaviours.ForEach(behaviour => behaviour.PassiveUpdate());
-        _activeBehaviours.ForEach(behaviour => behaviour.ActiveUpdate());
+        List<EnemyBehaviour> activeBehaviours = new List<EnemyBehaviour>(_activeBehaviours);
+        activeBehaviours.ForEach(behaviour => behaviour.ActiveUpdate());
     }
 
     public void AddBehaviour(EnemyBehaviour newBehaviour, EnemyControl enemyControl)
     {
-        _behaviours.Add((EnemyBehaviour)Activator.CreateInstance(newBehaviour.GetType()));
-        newBehaviour.Start(enemyControl);
+        EnemyBehaviour addedBehaviour = (EnemyBehaviour)Activator.CreateInstance(newBehaviour.GetType());
+        addedBehaviour.Initialize(newBehaviour, enemyControl);
+        _behaviours.Add(addedBehaviour);
+    }
+
+    public void RewriteAllOverrides()
+    {
+        foreach(EnemyBehaviour behaviour in _behaviours)
+            behaviour.RewriteOverrides(_behaviours); 
     }
 
     public bool ActivateBehaviour(EnemyBehaviour newBehaviour)
     {
         foreach(var behaviour in _activeBehaviours)
         {
-            bool isOverridenByActiveBehaviour = behaviour.OverrideBehaviours.Where(overrideBehaviour => overrideBehaviour.GetType() == newBehaviour.GetType()).Count() != 0;
-            if(isOverridenByActiveBehaviour)
+            bool isOverridenByActiveBehaviour = behaviour.OverrideBehaviours.Contains(newBehaviour);
+            print("Behaviour " + behaviour + "Overrides " + newBehaviour + ": " + isOverridenByActiveBehaviour);
+            if (isOverridenByActiveBehaviour)
                 return false;
         }
         _activeBehaviours.Add(newBehaviour);
@@ -38,7 +47,13 @@ public class EnemyBehaviourManager : MonoBehaviour
     {
         foreach(var behaviour in addedBehaviour.OverrideBehaviours)
         {
-            //if()
+            bool overridesActiveBehaviour = _activeBehaviours.Contains(behaviour);
+            if (overridesActiveBehaviour)
+                behaviour.KillBehaviour();
         }
+    }
+    public void KillBehaviour(EnemyBehaviour removedBehaviour)
+    {
+        _activeBehaviours.Remove(removedBehaviour);
     }
 }
