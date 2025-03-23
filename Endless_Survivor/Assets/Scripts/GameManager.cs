b06ef7a217205cd42e56f5a73bfc1d690732c38a
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-23 16:52:39 -0300

    feat(GameManager): UnlockmentsInfo and UnlockmentsHolder reference

--- Endless_Survivor/Assets/Scripts/GameManager.cs
+++ Endless_Survivor/Assets/Scripts/GameManager.cs
@@ -1,16 +1,23 @@
+using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public CharacterData selectedCharacter;
-
     public static GameManager instance;
+
+    public CharacterData selectedCharacter;
 
     [HideInInspector] public Transform player;
 
     public PrefabHolder prefabHolder;
+    public UnlockmentsHolder unlockmentsHolder;
+    static string unlockmentsFileName = "unlockments.json";
+    [HideInInspector] public List<CharacterData> unlockedCharacters;
+    [HideInInspector] public List<WeaponData> unlockedWeapons;
 
     public static GameManager gm
     {
@@ -27,5 +34,19 @@ public class GameManager : MonoBehaviour
 
         instance = this;
         DontDestroyOnLoad(gameObject);
+    }
+    public UnlockmentsInfo Unlockments 
+    {
+        get
+        {
+            string path = Path.Combine(Application.streamingAssetsPath, unlockmentsFileName);
+            string json = File.ReadAllText(path);
+            UnlockmentsInfo unlockmentsData = JsonConvert.DeserializeObject<UnlockmentsInfo>(json);
+            return unlockmentsData;
+        }
+    }
+    public void StartRun()
+    {
+        unlockedWeapons = unlockmentsHolder.UnlockedWeapons();
     }
 }
