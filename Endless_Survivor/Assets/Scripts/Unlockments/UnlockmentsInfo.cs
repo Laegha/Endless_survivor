7e86caa0b1c3d2ffa5d697d61a9fa02549d8d29c
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-23 16:02:30 -0300

    feat(UnlockmentsInfo): Interface between json and c#

---
+++ Endless_Survivor/Assets/Scripts/Unlockments/UnlockmentsInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class UnlockmentsInfo
+{
+    public Dictionary<string, bool> unlocked_characters = new Dictionary<string, bool>();
+    public Dictionary<string, bool> unlocked_weapons = new Dictionary<string, bool>();
+
+
+}
