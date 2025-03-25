080f265c11f27f729f77b794e80cc764bfceccd5
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-25 12:05:38 -0300

    refactor(EnemyData): HP scaling based on scaling functions for a higher math complexity

--- Endless_Survivor/Assets/Scripts/Enemies/EnemyData.cs
+++ Endless_Survivor/Assets/Scripts/Enemies/EnemyData.cs
@@ -11,6 +11,7 @@ public class EnemyData : ScriptableObject
     [SerializeField] CapsuleDirection2D _colliderDirection;
     [SerializeReference] List<EnemyBehaviour> _enemyBehaviours;
 
+
     public int InitialHP { get { return _initialHP; } }
     public Vector2 ColliderSize {  get { return _colliderSize; } }
     public List<EnemyBehaviour> EnemyBehaviours { get { return _enemyBehaviours; } }
@@ -18,7 +19,7 @@ public class EnemyData : ScriptableObject
     public void TransferEnemyData(GameObject enemy)
     {
         EnemyControl enemyControl = enemy.GetComponent<EnemyControl>();
-        enemyControl.EnemyHP.LeftHP = _initialHP + WaveManager.wm.CurrWave * 3;
+        enemyControl.EnemyHP.LeftHP = _initialHP + ScalingFunctions.EnemyHPIncrease(WaveManager.wm.CurrWave);
        
         enemyControl.CapsuleCollider.direction = _colliderDirection;
         enemyControl.CapsuleCollider.size = _colliderSize;
