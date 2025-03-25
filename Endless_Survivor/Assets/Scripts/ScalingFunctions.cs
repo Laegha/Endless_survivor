12af97779523f4c2f7f14b8c8ec96144852e7488
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-25 11:29:15 -0300

    feat(ScalingFunctions): Static class that holds functions for game progression

---
+++ Endless_Survivor/Assets/Scripts/ScalingFunctions.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ScalingFunctions
+{
+    public static int EnemyHPIncrease(int x)
+    {
+        return 0;
+    }
+
+    public static int WeaponStatIncrease(float increaseScale, int x)
+    {
+        return 0;
+    }
+
+}
