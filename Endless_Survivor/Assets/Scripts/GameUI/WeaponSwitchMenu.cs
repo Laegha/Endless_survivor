ecc095c93226b8c488c64e03c64905eaf5b8a6c6
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-31 14:52:22 -0300

    refactor(WeaponOverrideMenu): Changed name to WeaponSwitchMenu

--- Endless_Survivor/Assets/Scripts/GameUI/WeaponSwitchMenu.cs
+++ Endless_Survivor/Assets/Scripts/GameUI/WeaponSwitchMenu.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class WeaponOverrideMenu : MonoBehaviour
+public class WeaponSwitchMenu : MonoBehaviour
 {
     [SerializeField] GameObject _weaponButtonPrefab;
     [SerializeField] GameObject _menuGfx;
@@ -23,6 +23,7 @@ public class WeaponOverrideMenu : MonoBehaviour
         foreach (var weapon in holdingWeapons)
         {
             Transform button = Instantiate(_weaponButtonPrefab).transform;
+            button.SetParent(_menuGfx.transform);
             button.GetComponent<WeaponOverrideButton>().SetData(weapon.WeaponData.WeaponDisplaySprite, weapon, WeaponSelected);
             button.position = (Vector2)_weaponDisplayCircleCenter.position + Utility.GetPointInCircle(_weaponDisplayCircleRadius, currAngle);
             currAngle += angleStep;
@@ -32,13 +33,24 @@ public class WeaponOverrideMenu : MonoBehaviour
 
     void WeaponSelected(Weapon selectedWeapon)
     {
+        CloseMenu();
+        _onWeaponSelected?.Invoke(selectedWeapon);
+    }
+
+    public void DiscardWeapon()
+    { 
+        CloseMenu();
+    }
+
+    void CloseMenu()
+    {
+        //re-start game
         _menuGfx.SetActive(false);
         foreach (var button in _generatedButtons)
         {
             Destroy(button);
         }
         _generatedButtons.Clear();
-        _onWeaponSelected?.Invoke(selectedWeapon);
     }
 
 }
