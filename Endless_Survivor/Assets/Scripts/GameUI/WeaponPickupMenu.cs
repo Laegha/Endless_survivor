a12194cc1e2ea4bc5945bbf545b5e0c75fb41c3e
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-31 14:50:46 -0300

    feat(WeaponPickupMenu): Scale displayed weapon image

--- Endless_Survivor/Assets/Scripts/GameUI/WeaponPickupMenu.cs
+++ Endless_Survivor/Assets/Scripts/GameUI/WeaponPickupMenu.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class WeaponPickupMenu : MonoBehaviour
 {
     [SerializeField] GameObject _menuGfx;
     [SerializeField] Image _weaponImage;
+    [SerializeField] RectTransform _weaponImageTargetSize;
     [SerializeField] TextMeshProUGUI _weaponLevelDisplay;
     WeaponData _currDisplayingWeapon;
     WeaponStats _currWeaponStats;
@@ -15,13 +16,15 @@ public class WeaponPickupMenu : MonoBehaviour
     public WeaponData CurrDisplayingWeapon {  get { return _currDisplayingWeapon; } }
     public WeaponStats CurrWeaponStats {  get { return _currWeaponStats; } }
 
-    public void DisplayWeapon(WeaponData displayingWeapon)
+    public void DisplayMenu(WeaponData displayingWeapon)
     {
+        //stop game
+        _menuGfx.SetActive(true);
         _currDisplayingWeapon = displayingWeapon;
+        Utility.ScaleImageToFitTarget(_weaponImage.rectTransform, displayingWeapon.WeaponDisplaySprite, _weaponImageTargetSize.sizeDelta);
         _weaponImage.sprite = _currDisplayingWeapon.WeaponDisplaySprite;
         _currWeaponStats = new WeaponStats(_currDisplayingWeapon.WeaponStats);
         _currWeaponStats.ScaleStats(_currDisplayingWeapon.StatsIncreaseScale, WeaponStats.CurrWaveLevel);
-        _menuGfx.SetActive(true);
     }
     public void TakeWeapon()
     {
@@ -29,7 +32,7 @@ public class WeaponPickupMenu : MonoBehaviour
         _menuGfx.SetActive(false);
     }
 
-    public void LeaveWeapon()
+    public void DiscardWeapon()
     {
         _menuGfx.SetActive(false);
 
