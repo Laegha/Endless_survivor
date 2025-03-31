6e5403b7f24b2ea30b5287eaf0cef0d3dca201c0
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-03-31 15:29:02 -0300

    feat(PlayerWeaponManager): Handling weapon positions when switching weapons

--- Endless_Survivor/Assets/Scripts/Player/PlayerWeaponManager.cs
+++ Endless_Survivor/Assets/Scripts/Player/PlayerWeaponManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerWeaponManager : MonoBehaviour
@@ -7,21 +8,24 @@ public class PlayerWeaponManager : MonoBehaviour
     [SerializeField] Transform[] _gunPositions;
     [SerializeField] Transform _gunsHolder;
     [SerializeField] PlayerControl _playerControl;
-    List<Weapon> _heldWeapons = new List<Weapon>();
+    Dictionary<Weapon, Transform> _heldWeapons = new Dictionary<Weapon, Transform>();
 
     public void PickupWeapon(WeaponData weaponData, WeaponStats weaponStats)
     {
         if(_heldWeapons.Count == _gunPositions.Length)
         {
-            GameUIManager.uiManager.WeaponOverrideMenu.DisplayMenu(_heldWeapons, SwitchWeapon);
+            GameUIManager.uiManager.WeaponOverrideMenu.DisplayMenu(_heldWeapons.Keys.ToList(), SwitchWeapon);
             return;
         }
         GenerateWeapon(weaponData, weaponStats);
     }
 
-    void GenerateWeapon(WeaponData weaponData, WeaponStats weaponStats)
+    void GenerateWeapon(WeaponData weaponData, WeaponStats weaponStats, Transform gunPosition = null)
     {
-        Vector2 handPosition = _gunPositions[_heldWeapons.Count].position;
+        if (gunPosition == null)
+            gunPosition = _gunPositions[_heldWeapons.Count];
+            
+        Vector2 handPosition = gunPosition.position;
 
         Transform newWeapon = Instantiate(GameManager.gm.prefabHolder.Prefabs["Weapon"], handPosition, Quaternion.identity).transform;
         newWeapon.transform.SetParent(_gunsHolder);
@@ -32,14 +36,15 @@ public class PlayerWeaponManager : MonoBehaviour
 
         weaponData.WeaponDataTransferInterface.TransferData(newWeapon.gameObject, weaponData, weaponStats);
         Weapon weapon = newWeapon.GetComponent<Weapon>();
-        _heldWeapons.Add(weapon);
+        _heldWeapons.Add(weapon, gunPosition);
         weapon.PlayerControl = _playerControl;
     }
 
     void SwitchWeapon(Weapon removedWeapon)
     {
+        Transform gunPosition = _heldWeapons[removedWeapon];
         _heldWeapons.Remove(removedWeapon);
         Destroy(removedWeapon.gameObject);
-        GenerateWeapon(GameUIManager.uiManager.WeaponPickupMenu.CurrDisplayingWeapon, GameUIManager.uiManager.WeaponPickupMenu.CurrWeaponStats);
+        GenerateWeapon(GameUIManager.uiManager.WeaponPickupMenu.CurrDisplayingWeapon, GameUIManager.uiManager.WeaponPickupMenu.CurrWeaponStats, gunPosition);
     }
 }
