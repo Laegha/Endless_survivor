b90b13b75a5873385eae8ef6fb1e0274d5322e12
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-04-01 04:30:47 -0300

    feat(PickupData): Base class for scriptable objects that set-up in-game pickups

--- Endless_Survivor/Assets/Scripts/Pickups/Pickup.cs
+++ Endless_Survivor/Assets/Scripts/Pickups/Pickup.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
-    [SerializeField] CustomAnimator _pickupAnimator;
-    public CustomAnimator PickupAnimator { get { return _pickupAnimator; } }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         PickUp(collision.GetComponent<PlayerControl>());
---
+++ Endless_Survivor/Assets/Scripts/Pickups/PickupData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupData : ScriptableObject
+{
+    public virtual void TransferData(GameObject pickupGameObject)
+    {
+
+    }
+}
