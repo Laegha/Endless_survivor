bd49fb0acda68a57250bc34ec94f391e7bb45faf
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-04-29 01:52:27 -0300

    feat(WeaponHolder): Class that stores data for held weapons

---
+++ Endless_Survivor/Assets/Scripts/Player/WeaponHolder.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponHolder
+{
+    public bool positionStays;
+    public Weapon holdingWeapon;
+    public Transform handTransform;
+}
