975ddedb1cbfbe846ba3fedfacacd9a913a92b15
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-05-06 00:42:50 -0300

    feat(PassiveItemDataEditor): Allows to manipulate different behaviours

---
+++ Endless_Survivor/Assets/Editor/Scripts/PassiveItemDataEditor.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PassiveItemData))]
+public class PassiveItemDataEditor : Editor
+{
+    Dictionary<Type, bool> _behaviourTypes = new();
+    
+    private void OnEnable()
+    {
+        List<Type> behaviourTypes = Utility.GetSubclassesOf(typeof(PassiveItemBehaviour));
+        PassiveItemData passiveItemData = (PassiveItemData)target;
+        behaviourTypes.ForEach(type => _behaviourTypes.Add(type, passiveItemData.ItemBehaviours.Exists(behaviour => behaviour.GetType() == type)));
+        passiveItemData.ItemBehaviours.RemoveAll(behaviour => behaviour == null);
+    }
+    public override void OnInspectorGUI()
+    {
+        PassiveItemData passiveItemData = (PassiveItemData)target;
+        SerializedProperty itemBehaviours = serializedObject.FindProperty("_itemBehaviours");
+
+        EditorGUILayout.LabelField("Object Behaviours", EditorStyles.boldLabel);
+
+        //EditorGUILayout.PropertyField(_objectBehaviours);
+        //for (int i = 0; i < _objectBehaviours.arraySize; i++)
+        //{
+        //SerializedProperty behaviourProperty = _objectBehaviours.GetArrayElementAtIndex(i);
+
+        //EditorGUILayout.PropertyField(behaviourProperty);
+        //}
+
+
+        for (int i = 0; i < passiveItemData.ItemBehaviours.Count; i++)
+        {
+            SerializedProperty behaviourProp = itemBehaviours.GetArrayElementAtIndex(i);
+            if (behaviourProp != null)
+                EditorGUILayout.PropertyField(behaviourProp, new GUIContent(passiveItemData.ItemBehaviours[i].GetType().Name), true);
+        }
+
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Add Behaviour"))
+        {
+            GenericMenu menu = new GenericMenu();
+            foreach (var type in _behaviourTypes)
+            {
+                var isTypeUsable = type.Key.GetProperty("isUsable", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
+                if (type.Value || isTypeUsable == null || !(bool)isTypeUsable.GetValue(null))
+                    continue;
+
+                menu.AddItem(new GUIContent(type.Key.Name), false, () =>
+                {
+                    PassiveItemBehaviour newBehaviour = (PassiveItemBehaviour)Activator.CreateInstance(type.Key);
+                    passiveItemData.ItemBehaviours.Add(newBehaviour);
+                    _behaviourTypes[type.Key] = true;
+                    EditorUtility.SetDirty(passiveItemData);
+                });
+            }
+            menu.ShowAsContext();
+        }
+        if (GUILayout.Button("Remove Behaviour"))
+        {
+            GenericMenu menu = new GenericMenu();
+            foreach (var behaviour in passiveItemData.ItemBehaviours)
+            {
+                menu.AddItem(new GUIContent(behaviour.GetType().Name), false, () =>
+                {
+                    passiveItemData.ItemBehaviours.Remove(behaviour);
+                    _behaviourTypes[behaviour.GetType()] = false;
+                    EditorUtility.SetDirty(passiveItemData);
+                });
+            }
+            menu.ShowAsContext();
+        }
+        EditorGUILayout.EndHorizontal();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+}
