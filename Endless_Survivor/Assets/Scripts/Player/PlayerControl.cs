bd0a8a67a9bc7218af97c2653b9f532c92c19d07
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-05-06 00:42:12 -0300

    feat(PlayerControl): PassiveItemManager

--- Endless_Survivor/Assets/Scripts/Player/PlayerControl.cs
+++ Endless_Survivor/Assets/Scripts/Player/PlayerControl.cs
@@ -15,11 +15,13 @@ public class PlayerControl : MonoBehaviour
     [SerializeField] Rigidbody2D _playerRb;
     [SerializeField] PlayerHPManager _playerHPManager;
     [SerializeField] PlayerWeaponManager _playerWeaponManager;
+    [SerializeField] PassiveItemManager _passiveItemManager;
     [SerializeField] SpriteRenderer[] _renderers;
     public PlayerAnimator PlayerAnimator { get { return _playerAnimator; } }
     public PlayerStats PlayerStats { get { return _playerStats; } set { _playerStats = value; } }
     public Rigidbody2D PlayerRb { get { return _playerRb; } }
     public PlayerHPManager PlayerHPManager { get { return _playerHPManager; } }
+    public PassiveItemManager PassiveItemManager {  get { return _passiveItemManager; } }
     public PlayerWeaponManager WeaponManager { get { return _playerWeaponManager; } }
     public SpriteRenderer[] Renderers { get { return _renderers; } }
 
