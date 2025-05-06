0cfa09b854f43c75cc11a09390aa971f97003ac4
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-05-06 00:39:53 -0300

    feat(CreateSomethingOnPositionItemBehaviour): Item behaviour that will probably take a prefab and Instantiate on a given possition

---
+++ Endless_Survivor/Assets/Scripts/PassiveItems/Behaviours/CreateSomethingOnPositionItemBehaviour.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class CreateSomethingOnPositionItemBehaviour : PassiveItemBehaviour
+{
+    public static bool isUsable => false;
+    public override void CopyValues(PassiveItemBehaviour original, PassiveItemBehaviourManager behaviourManager)
+    {
+        
+    }
+    public void CreateSomething(Vector2 position)
+    {
+
+    }
+}
