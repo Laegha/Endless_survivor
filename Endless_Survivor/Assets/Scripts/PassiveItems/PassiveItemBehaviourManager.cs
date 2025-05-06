3679c91b14710170838fe917d0a1378f4f2d6d1f
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-05-06 00:22:15 -0300

    feat(PassiveItemBehaviourManager): stores behaviours and Actions that behaviours will subscribe to (this is the main course of the passive items)

---
+++ Endless_Survivor/Assets/Scripts/PassiveItems/PassiveItemBehaviourManager.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassiveItemBehaviourManager
+{
+    public Action onPicked;
+    public Action onUpdate;
+    public Action onAttack;//make a system for attacks, so passive items can modify them
+    public Action onDamageRecieved;
+    public Action<EnemyControl> onEnemyHit;
+    public Action<EnemyControl> onEnemyKilled;
+    public Action onWaveChanged;
+
+    List<PassiveItemBehaviour> _itemBehaviours;
+    public void CopyBehaviours(List<PassiveItemBehaviour> itemBehaviours)
+    {
+        foreach (PassiveItemBehaviour behaviour in itemBehaviours)
+        {
+            PassiveItemBehaviour addedBehaviour = (PassiveItemBehaviour)Activator.CreateInstance(behaviour.GetType(), behaviour, this);
+            _itemBehaviours.Add(addedBehaviour);
+        }
+    }
+}
