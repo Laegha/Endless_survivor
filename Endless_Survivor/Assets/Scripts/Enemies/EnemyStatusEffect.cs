ba4179d9912135157645fe8919305ab6af706d27
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-05-07 01:35:28 -0300

    feat(EnemyStatusEffect): base class with common behaviour for all EnemyStatusEffects and functions to implement

--- Endless_Survivor/Assets/Scripts/Enemies/EnemyStatusEffect.cs
+++ Endless_Survivor/Assets/Scripts/Enemies/EnemyStatusEffect.cs
@@ -1,16 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public class EnemyStatusEffect
 {
+    //can be duplicated bool (should be virtual or smth)
     EnemyControl _affectedEnemyControl;
+    [SerializeField] Sprite _statusEffectIndicator;
+    [SerializeField] Material _statusEffectMaterial;
+    public EnemyControl AffectedEnemyControl {  get { return _affectedEnemyControl; } }
 
-    public EnemyStatusEffect(EnemyControl affectedEnemyControl)
+    public virtual void Initialize(EnemyControl affectedEnemyControl, EnemyStatusEffect original)
     {
         _affectedEnemyControl = affectedEnemyControl;
+        _statusEffectIndicator = original._statusEffectIndicator;
+        _statusEffectMaterial = original._statusEffectMaterial;
     }
 
-    public virtual void Start(){ }
+    public virtual void Start() 
+    {
+        //show indicator and change material
+        _affectedEnemyControl.StatusEffectManager.AddStatusGraphics(_statusEffectIndicator, _statusEffectMaterial);
+    }
     public virtual void Update() { }
+    public virtual void End() { }
+    public virtual void EnemyHit() { }
+    public virtual void EnemyKilled() { }
 }
