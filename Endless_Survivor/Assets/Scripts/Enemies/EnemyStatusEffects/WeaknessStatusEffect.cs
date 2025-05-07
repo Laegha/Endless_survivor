c502c3c1d5a339bfecd491802b7d6ffc6511a3b0
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-05-07 01:43:33 -0300

    feat(WeaknessStatusEffect): EnemyStatusEffect that increases incomingDamageMultiplier and decreases it again on end

---
+++ Endless_Survivor/Assets/Scripts/Enemies/EnemyStatusEffects/WeaknessStatusEffect.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class WeaknessStatusEffect : EnemyStatusEffect
+{
+    public static bool isUsable => true;
+    [SerializeField] float _damageMultiplierAddition = .25f;
+    [SerializeField] float _duration = 5;
+    float _timer = 0;
+    public override void Initialize(EnemyControl enemyControl, EnemyStatusEffect original) 
+    {
+        base.Initialize(enemyControl, original);
+        WeaknessStatusEffect originalWeaknessEffect = (WeaknessStatusEffect)original;
+        _damageMultiplierAddition = originalWeaknessEffect._damageMultiplierAddition;
+        _duration = originalWeaknessEffect._duration;
+    }
+    public override void Start()
+    {
+        base.Start();
+        AffectedEnemyControl.EnemyHP.IncomingDamageMultiplier += _damageMultiplierAddition;
+    }
+    public override void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer >= _duration)
+        {
+            AffectedEnemyControl.StatusEffectManager.RemoveEffect(this);
+        }
+    }
+    public override void End()
+    {
+        base.End();
+        AffectedEnemyControl.EnemyHP.IncomingDamageMultiplier -= _damageMultiplierAddition;
+
+    }
+}
