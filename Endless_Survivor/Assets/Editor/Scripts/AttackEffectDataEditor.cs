801b5f006e94e8698be27369099b52804ce970de
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-05-30 17:20:54 -0300

    feat(AttackEffectData): Holds the attack effects that apply to every attack when obtained

---
+++ Endless_Survivor/Assets/Editor/Scripts/AttackEffectDataEditor.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AttackEffectData))]
+public class AttackEffectDataEditor : Editor
+{
+    Dictionary<Type, bool> _attackEffects = new();
+    SerializedProperty _attackEffectChance;
+
+    private void OnEnable()
+    {
+        _attackEffectChance = serializedObject.FindProperty("_effectChance");
+        List<Type> behaviourTypes = Utility.GetSubclassesOf(typeof(AttackEffect));
+        AttackEffectData attackEffectData = (AttackEffectData)target;
+        behaviourTypes.ForEach(type => _attackEffects.Add(type, attackEffectData.AttackEffects.Exists(behaviour => behaviour.GetType() == type)));
+        attackEffectData.AttackEffects.RemoveAll(behaviour => behaviour == null);
+    }
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.PropertyField(_attackEffectChance);
+
+        AttackEffectData attackEffectData = (AttackEffectData)target;
+        SerializedProperty attackEffects = serializedObject.FindProperty("_attackEffects");
+
+        EditorGUILayout.LabelField("Effects", EditorStyles.boldLabel);
+
+
+        serializedObject.Update();
+        for (int i = 0; i < attackEffectData.AttackEffects.Count; i++)
+        {
+            if (attackEffects.arraySize <= i)
+            {
+                break;
+            }
+            SerializedProperty effectProp = attackEffects.GetArrayElementAtIndex(i);
+            if (effectProp == null)
+                continue;
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.PropertyField(effectProp, new GUIContent(attackEffectData.AttackEffects[i].GetType().Name), true);
+
+            if (GUILayout.Button("Remove " + attackEffectData.AttackEffects[i].GetType().Name))
+            {
+                _attackEffects[attackEffectData.AttackEffects[i].GetType()] = false;
+                attackEffectData.AttackEffects.RemoveAt(i);
+                EditorUtility.SetDirty(attackEffectData);
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+        if (GUILayout.Button("Add Effect"))
+        {
+            GenericMenu menu = new GenericMenu();
+            foreach (var type in _attackEffects)
+            {
+                var isTypeUsable = type.Key.GetProperty("isUsable", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
+                if (type.Value || isTypeUsable == null || !(bool)isTypeUsable.GetValue(null))
+                    continue;
+
+                menu.AddItem(new GUIContent(type.Key.Name), false, () =>
+                {
+                    AttackEffect newEffect = (AttackEffect)Activator.CreateInstance(type.Key);
+                    
+                    attackEffectData.AttackEffects.Add(newEffect);
+                    _attackEffects[type.Key] = true;
+                    EditorUtility.SetDirty(attackEffectData);
+                });
+            }
+            menu.ShowAsContext();
+        }
+
+        serializedObject.ApplyModifiedProperties();
+        serializedObject.Update();
+    }
+}
---
+++ Endless_Survivor/Assets/Scripts/AttackEffects/AttackEffectData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New AttackEffect", menuName = "ScriptableObjects/AttackEffect", order = 1)]
+public class AttackEffectData : ScriptableObject
+{
+    [SerializeField][Range(0, 100)] int _effectChance;
+    [SerializeField] List<AttackEffect> _attackEffects = new List<AttackEffect>();
+
+    public List<AttackEffect> AttackEffects { get { return _attackEffects; } }
+    
+    List<AttackEffect> GetActiveEffects(int attackRand)
+    {
+        return _attackEffects.Where(x => attackRand <= (x.UsesSeparateChance ? x.EffectChance : _effectChance)).ToList();
+    }
+}
