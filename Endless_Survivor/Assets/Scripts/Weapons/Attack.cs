879afadedea663fe5f3441f1020f12765804e521
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-06-01 03:51:03 -0300

    feat(Attack): TryEffects on Start

--- Endless_Survivor/Assets/Scripts/Weapons/Attack.cs
+++ Endless_Survivor/Assets/Scripts/Weapons/Attack.cs
@@ -6,4 +6,8 @@ public class Attack : MonoBehaviour
 {
     [SerializeField] AttackEffectsHandler _effectsHandler;
     public AttackEffectsHandler EffectsHandler { get { return _effectsHandler; } }
+    private void Start()
+    {
+        _effectsHandler.TryEffects(this);
+    }
 }
