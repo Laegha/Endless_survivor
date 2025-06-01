07f9bbd5df322bdf2d783b0a9c40c92674a1a7e9
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-06-01 02:45:01 -0300

    feat(Attack): Class that will be inherited by weapon attacks, like proyectiles and lasers.
    This was needed to implement attack effect synergies properly

--- Endless_Survivor/Assets/Scripts/Weapons/Proyectile.cs
+++ Endless_Survivor/Assets/Scripts/Weapons/Proyectile.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Proyectile : MonoBehaviour
+public class Proyectile : Attack
 {
     [SerializeField] Rigidbody2D _rb;
     [SerializeField] SpriteRenderer _spriteRenderer;
