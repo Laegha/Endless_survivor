f941cddf3c7312a6ebc5a92123f25722872fad91
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-06-08 21:40:38 -0300

    refactor(RayWeapon): Instantiating a RayAttack when attacking, which will manage the actual attack behaviour

--- Endless_Survivor/Assets/Scripts/Weapons/RayWeapon.cs
+++ Endless_Survivor/Assets/Scripts/Weapons/RayWeapon.cs
@@ -5,56 +5,20 @@ using UnityEngine;
 
 public class RayWeapon : ShootingWeapon
 {
-    LineRenderer _lineRenderer;
-    bool _shooting = false;
-    public LineRenderer LineRenderer {  get { return _lineRenderer; } set { _lineRenderer = value; } }
-
+    RayData _rayData;
+    public RayData RayData { set {_rayData = value; } }
     public override void Start()
     {
         base.Start();
-        WeaponControl.WeaponAnimator.Animations.Where(anim => anim.AnimationName == "Attack").ToArray()[0].OnAnimationEnd += StopShooting;
-    }
-
-    public override void Update()
-    {
-        base.Update();
-        if(_shooting)
-            UpdateLinePosition();
     }
     public override void Attack()
     {
         base.Attack();
 
         WeaponControl.WeaponAnimator.ChangeAnim("Attack");
-        StartShooting();
-        RaycastHit2D hit = Physics2D.Raycast(FirePoint.position, -FirePoint.right);
-        if (!hit)
-        {
-            _lineRenderer.SetPosition(_lineRenderer.positionCount - 1, FirePoint.position + -FirePoint.right * 100);//if the player didn't hit nothing (which should not happen), setting the end of the ray far enough so the player can't see it
-            return;
-            
-        }
-        _lineRenderer.SetPosition(_lineRenderer.positionCount - 1, hit.point);
-        EnemyHP enemyHP = hit.collider.GetComponent<EnemyHP>();
-        if (enemyHP != null)
-            enemyHP.TakeDamage(WeaponStats.Damage + GameManager.gm.selectedCharacter.PlayerStats.Damage);
+        RayAttack laserAttack = Instantiate(GameManager.gm.prefabHolder.Prefabs["Laser"], Vector2.zero, Quaternion.identity).GetComponent<RayAttack>();
+
+        laserAttack.Attack(_rayData.RayExitSpeed, WeaponStats.Damage + GameManager.gm.selectedCharacter.PlayerStats.Damage, _rayData, FirePoint);
     }
 
-    public void UpdateLinePosition()
-    {
-        _lineRenderer.SetPosition(0, FirePoint.position);
-    }
-
-    public void StartShooting()
-    {
-        _shooting = true;
-        _lineRenderer.gameObject.SetActive(true);
-    }
-
-    public void StopShooting(CustomAnimator placeholder)
-    {
-        _shooting = false;
-        print("Stop shooting");
-        _lineRenderer.gameObject.SetActive(false);
-    }
 }
