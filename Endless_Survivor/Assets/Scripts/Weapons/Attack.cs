fc6fe598ce2808ecb6aca82cdc22f813ed1bf7d0
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-06-09 01:41:24 -0300

    feat(Attack): attack's own damage and multiplier, intended to be used by the attack effects

--- Endless_Survivor/Assets/Scripts/Weapons/Attack.cs
+++ Endless_Survivor/Assets/Scripts/Weapons/Attack.cs
@@ -5,5 +5,9 @@ using UnityEngine;
 public class Attack : MonoBehaviour
 {
     [SerializeField] AttackEffectsHandler _effectsHandler;
+    int _attackDamage = 0;
+    float _attackDamageMultiplier = 1;
     public AttackEffectsHandler EffectsHandler { get { return _effectsHandler; } }
+    public int AttackDamage { get { return (int)(_attackDamage * _attackDamageMultiplier); } set { _attackDamage = value; } }
+    public float AttackDamageMultiplier { get { return _attackDamageMultiplier; } set { _attackDamageMultiplier = value; } }
 }
