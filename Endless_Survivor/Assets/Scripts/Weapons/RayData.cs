442b94c7a8384c005fe7657b20c9e8a58e6cb3c4
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-06-08 21:42:06 -0300

    feat(RayData): Copy constructor and _rayExitSpeed for how fast the bullets gfx dissapear

--- Endless_Survivor/Assets/Scripts/Weapons/RayData.cs
+++ Endless_Survivor/Assets/Scripts/Weapons/RayData.cs
@@ -9,8 +9,18 @@ public class RayData
     [SerializeField] Material _rayMaterial;
     [SerializeField] float _rayStartWidth;
     [SerializeField] float _rayEndWidth;
+    [SerializeField] float _rayExitSpeed;
 
     public Material RayMaterial {  get { return _rayMaterial; } }
     public float RayStartWidth { get { return _rayStartWidth; } }
     public float RayEndWidth { get { return _rayEndWidth; } }
+    public float RayExitSpeed { get { return _rayExitSpeed; } }
+
+    public RayData(RayData original)
+    {
+        _rayMaterial = original._rayMaterial;
+        _rayStartWidth = original._rayStartWidth;
+        _rayEndWidth = original._rayEndWidth;
+        _rayExitSpeed = original._rayExitSpeed;
+    }
 }
