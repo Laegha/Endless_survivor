0faf77ed9467670598b602ac9d02038994522035
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-06-10 01:49:55 -0300

    feat(EnemyStatusEffectManager): Manage grid sprite status icons with EnemyStatusEffectGFX

--- Endless_Survivor/Assets/Scripts/Enemies/EnemyStatusEffectManager.cs
+++ Endless_Survivor/Assets/Scripts/Enemies/EnemyStatusEffectManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class EnemyStatusEffectManager : MonoBehaviour
@@ -8,15 +9,20 @@ public class EnemyStatusEffectManager : MonoBehaviour
     [SerializeField] EnemyControl _enemyControl;
     [SerializeField] SpriteGrid _statusIndicatorsGrid;
     List<EnemyStatusEffect> _currentEffects = new();
+    Dictionary<EnemyStatusEffect, EnemyStatusEffectGFX> _activeGfx;
 
-    public void AddStatusGraphics(Sprite statusIcon, Material statusMaterial)
+    public void AddStatusGraphics(Sprite statusIcon, Material statusMaterial, EnemyStatusEffect status)
     {
         //display icon and add material to a kind of queue
-        _statusIndicatorsGrid.AddSpriteToGrid(statusIcon);
+        var statusSprite = _statusIndicatorsGrid.AddSpriteToGrid(statusIcon);
+        _activeGfx.Add(status, new EnemyStatusEffectGFX(statusSprite, statusMaterial));
     }
-    public void RemoveStatusGraphics()
+    public void RemoveStatusGraphics(EnemyStatusEffect statusEffect)
     {
-
+        _statusIndicatorsGrid.RemoveSpriteFromGrid(_activeGfx[statusEffect].statusSprite);
+        _activeGfx.Remove(statusEffect);
+        Material updatedMaterial = _activeGfx.Count == 0 ? _activeGfx.Last().Value.statusMaterial /*<-change this to the default material*/ : _activeGfx.Last().Value.statusMaterial;
+        
     }
     public void AddEffect(EnemyStatusEffect effect)
     {
