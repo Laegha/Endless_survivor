3b73d8c6ef35dd08a5d70ea6e306ecb0f914c760
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-06-13 02:10:33 -0300

    feat(MaterialManager): Handles switching the material of given renderers based on the list of overrides, which is modified externally

---
+++ Endless_Survivor/Assets/Scripts/Reusable/MaterialManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MaterialManager : MonoBehaviour
+{
+    [SerializeField] List<SpriteRenderer> _renderers;
+    MaterialOverride _currentOverride;
+    List<MaterialOverride> _overridesQueue = new();   
+    Dictionary<SpriteRenderer, Material> _defaultMaterials = new();
+
+    private void Start()
+    {
+        foreach (var renderer in _renderers)
+        {
+            _defaultMaterials.Add(renderer, renderer.material);
+        }
+    }
+
+    public void SetMaterialOverride(MaterialOverride newMaterial)
+    {
+        _overridesQueue.Add(newMaterial);
+        if(_currentOverride == null || newMaterial.authority > _currentOverride.authority)
+            UpdateMaterials();
+    }
+    public void UnsetMaterialOverride(MaterialOverride unsetMaterial)
+    {
+        _overridesQueue.Remove(unsetMaterial);
+        if(unsetMaterial == _currentOverride)
+            UpdateMaterials();
+
+    }
+    void UpdateMaterials()
+    {
+        _overridesQueue.Sort(new MaterialAuthorityComparer());
+        _currentOverride = _overridesQueue.LastOrDefault();
+        foreach (var renderer in _renderers)
+        {
+            renderer.material = _currentOverride != null ? _currentOverride.material : _defaultMaterials[renderer];
+        }
+    }
+    public void AddRenderer(SpriteRenderer renderer) => _renderers.Add(renderer);
+    public void CleanRenderers() => _renderers.RemoveAll(x => x == null);
+}
