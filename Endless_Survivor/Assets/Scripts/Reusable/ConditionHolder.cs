4a8192f33252391c465d81e619b097e56c644336
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-06-17 22:24:32 -0300

    feat(ConditionHolder): Class that holds a bool for shared usage between objects

---
+++ Endless_Survivor/Assets/Scripts/Reusable/ConditionHolder.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConditionHolder
+{
+    public bool value;
+
+    public ConditionHolder(bool startValue)
+    {
+        this.value = startValue; 
+    }
+}
