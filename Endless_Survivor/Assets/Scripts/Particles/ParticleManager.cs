455c884bba20de19fc43be6f03b52f960185c7bd
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-06-22 03:06:00 -0300

    feat(ParticleManager): Singleton that enables to spawn particles and manipulate them

---
+++ Endless_Survivor/Assets/Scripts/Particles/ParticleManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+    static ParticleManager instance;
+    public static ParticleManager pm { get { return instance; } }
+    private void Awake()
+    {
+        if (instance != null)
+            Destroy(gameObject);
+        instance = this;
+    }
+    public ParticleSystem SpawnParticles(ParticleConfig config)
+    {
+        var instantiatedParticles = Instantiate(config.particlesPrefab, config.particlesParentTransform);
+        if(config.particlesDuration >= 0)
+            Destroy(instantiatedParticles, config.particlesDuration);
+        instantiatedParticles.transform.position = config.particlesPosition;
+        instantiatedParticles.transform.rotation = config.particlesRotation;
+        return instantiatedParticles;
+    }
+}
