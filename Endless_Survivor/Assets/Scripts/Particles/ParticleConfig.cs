77bec2da4d1fe7fa17b333b5b2b3b30728d5cc11
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-06-22 03:05:25 -0300

    feat(ParticleConfig): Holds data for spawning particles

---
+++ Endless_Survivor/Assets/Scripts/Particles/ParticleConfig.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleConfig
+{
+    public ParticleSystem particlesPrefab;
+    public float particlesDuration;//pass a negative number on the constructor for no duration limit
+    public Transform particlesParentTransform;
+    public Vector3 particlesPosition;
+    public Quaternion particlesRotation;
+
+    public ParticleConfig(ParticleSystem particlesPrefab, Vector3 particlesPosition, Quaternion particlesRotation, float particlesDuration = 1f, Transform particlesParentTransform = null)
+    {
+        this.particlesPrefab = particlesPrefab;
+        this.particlesPosition = particlesPosition;
+        this.particlesRotation = particlesRotation;
+        this.particlesDuration = particlesDuration;
+        this.particlesParentTransform = particlesParentTransform;
+    }
+}
