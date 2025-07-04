10814defde110ce759ad939a96bbcf98501858c7
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-07-04 16:18:17 -0300

    feat(CharacterButton): rescale gfx to fit target

--- Endless_Survivor/Assets/Scripts/Menu/CharacterButton.cs
+++ Endless_Survivor/Assets/Scripts/Menu/CharacterButton.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterButton : MonoBehaviour
 {
     [HideInInspector] public CharSelectMenu charSelectMenu;
     [HideInInspector] public CharacterData characterData;
+    public Image buttonImage;
+    public RectTransform imageTargetSize;
 
     public void SelectCharacter()
     {
         GameManager.gm.selectedCharacter = characterData;
         charSelectMenu.CloseMenu();
+    }
+
+    public void SetImage(Sprite imageSprite)
+    {
+        buttonImage.sprite = imageSprite;
+        Utility.ScaleImageToFitTarget(buttonImage.GetComponent<RectTransform>(), imageSprite, imageTargetSize.sizeDelta);
     }
 }
