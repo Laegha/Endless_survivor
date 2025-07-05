83b77844e9fd1179e41c0ce5d5f208aa6c3b49f7
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-07-05 00:52:52 -0300

    feat(CharSelectMenu): Change button clip bounds based on a max and min from objects

--- Endless_Survivor/Assets/Scripts/Menu/CharSelectMenu.cs
+++ Endless_Survivor/Assets/Scripts/Menu/CharSelectMenu.cs
@@ -5,20 +5,25 @@ using UnityEngine.UI;
 
 public class CharSelectMenu : MonoBehaviour
 {
+    [SerializeField] GameObject _mainMenu;
     [SerializeField] CharacterData[] _elegibleCharacters;
     [SerializeField] CharacterButton _selectCharBtnPrefab;
     [SerializeField] HorizontalLayoutGroup _gridRowPrefab;
     [SerializeField] RectTransform _buttonGrid;
-    [SerializeField] GameObject _mainMenu;
+
+    [SerializeField] RectTransform _gridMax;
+    [SerializeField] RectTransform _gridMin;
 
     public void OnEnable()
     {
         var currentRow = Instantiate(_gridRowPrefab, _buttonGrid);
         float rowFilledSpace = 0;
-
         foreach (CharacterData character in _elegibleCharacters)
         {
             var characterButton = Instantiate(_selectCharBtnPrefab, currentRow.transform);
+            characterButton.buttonImage.material = new Material(characterButton.buttonImage.material);
+            characterButton.buttonImage.material.SetFloat("_MaxHeight", _gridMax.anchoredPosition.y);
+            characterButton.buttonImage.material.SetFloat("_MinHeight", _gridMin.anchoredPosition.y);
             //characterButton.buttonImage.sprite = character.MenuImage;
             characterButton.characterData = character;
             characterButton.charSelectMenu = this;
