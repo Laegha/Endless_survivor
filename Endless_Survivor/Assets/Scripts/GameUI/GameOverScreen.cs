3e30dbe86efc2d512673e132af50a4f76c56215c
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-07-23 13:35:42 -0300

    feat(GameOverScreen): Displays run stats and earned coins when run is over

--- Endless_Survivor/Assets/Scripts/GameUI/GameOverScreen.cs
+++ Endless_Survivor/Assets/Scripts/GameUI/GameOverScreen.cs
@@ -3,30 +3,30 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class GameOverScreen : MonoBehaviour
-{
-    [SerializeField] GameObject _menuObject;
-    [SerializeField] TextMeshProUGUI _damageDealtDisplay;
-    [SerializeField] TextMeshProUGUI _killedEnemiesDisplay;
-
-    float _waveCoinRelation = 10;
-    float _maxWaveCoins = 3;
-
-    private void Start()
+    public class GameOverScreen : MonoBehaviour
     {
-        GameManager.gm.GachaCoins = 3;
-    }
-    public void DisplayMenu()
-    {
-        _menuObject.SetActive(true);
-        _damageDealtDisplay.text = RunStatsManager.runStatsManager.totalDamageDealt + "";
-        _killedEnemiesDisplay.text = RunStatsManager.runStatsManager.regularEnemiesKilled + "";
-    }
+        [SerializeField] GameObject _menuObject;
+        [SerializeField] TextMeshProUGUI _damageDealtDisplay;
+        [SerializeField] TextMeshProUGUI _killedEnemiesDisplay;
 
-    void CalculateEarnedCoins()
-    {
-        var wavesSurvived = RunStatsManager.runStatsManager.wavesSurvived;
-        var waveCoins = Mathf.Floor(wavesSurvived / _waveCoinRelation);
-        waveCoins = Mathf.Clamp(waveCoins, 0, _maxWaveCoins);
+        float _waveCoinRelation = 10;
+        float _maxWaveCoins = 3;
+
+        private void Start()
+        {
+            GameManager.gm.GachaCoins = 3;
+        }
+        public void DisplayMenu()
+        {
+            _menuObject.SetActive(true);
+            _damageDealtDisplay.text = RunStatsManager.runStatsManager.totalDamageDealt + "";
+            _killedEnemiesDisplay.text = RunStatsManager.runStatsManager.regularEnemiesKilled + "";
+        }
+
+        void CalculateEarnedCoins()
+        {
+            var wavesSurvived = RunStatsManager.runStatsManager.wavesSurvived;
+            var waveCoins = Mathf.Floor(wavesSurvived / _waveCoinRelation);
+            waveCoins = Mathf.Clamp(waveCoins, 0, _maxWaveCoins);
+        }
     }
-}
