06a0e908c6f9dc75b3694e07e51c547ec6768265
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-07-23 13:38:10 -0300

    feat(GameManager): Connection with json, get for unlockments and set for coins
    This will surely be refactored to be part of a separate class and use separate jsons

--- Endless_Survivor/Assets/Scripts/GameManager.cs
+++ Endless_Survivor/Assets/Scripts/GameManager.cs
@@ -14,10 +14,30 @@ public class GameManager : MonoBehaviour
 
     public PrefabHolder prefabHolder;
     public UnlockmentsHolder unlockmentsHolder;
+
     static string unlockmentsFileName = "unlockments.json";
+    static string unlockmentsFilePath = Path.Combine(Application.streamingAssetsPath, unlockmentsFileName);
+    static string unlockmentsJson = File.ReadAllText(unlockmentsFilePath);
     [HideInInspector] public List<CharacterData> unlockedCharacters;
     [HideInInspector] public List<WeaponData> unlockedWeapons;
+
+    int _gachaCoins = 0;
+    public int GachaCoins 
+    { get { return _gachaCoins; } 
+        set 
+        {
+            _gachaCoins = value;
+            UnlockmentsConverter unlockments = JsonConvert.DeserializeObject<UnlockmentsConverter>(unlockmentsJson);
+            unlockments.gachaCoins = value;
+            string json = JsonConvert.SerializeObject(unlockments);
+            File.WriteAllText(unlockmentsFilePath, json);
+
+        }
+    }
+
     [SerializeField] AudioMixer _audioMixer;
+
+    [SerializeField] WeaponData _testWeapon;
 
     public static GameManager gm
     {
@@ -35,13 +55,17 @@ public class GameManager : MonoBehaviour
         instance = this;
         DontDestroyOnLoad(gameObject);
     }
-    public UnlockmentsInfo Unlockments 
+    private void Start()
+    {
+       _testWeapon = Resources.Load("ScriptableObjects/Weapons/Balance") as WeaponData;
+    }
+    public UnlockmentsConverter Unlockments 
     {
         get
         {
             string path = Path.Combine(Application.streamingAssetsPath, unlockmentsFileName);
             string json = File.ReadAllText(path);
-            UnlockmentsInfo unlockmentsData = JsonConvert.DeserializeObject<UnlockmentsInfo>(json);
+            UnlockmentsConverter unlockmentsData = JsonConvert.DeserializeObject<UnlockmentsConverter>(json);
             return unlockmentsData;
         }
     }
