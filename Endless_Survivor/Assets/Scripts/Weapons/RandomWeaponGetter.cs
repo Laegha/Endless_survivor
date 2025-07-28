9686e538bc316178d5c27e3af84ae6c0b7e4712c
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-07-27 23:27:20 -0300

    feat(RandomWeaponGetter): Auxiliar class with a static function that returns a random unlocked weapon
    Possibility to filter by pools and that chances are affected by the player's current build

---
+++ Endless_Survivor/Assets/Scripts/Weapons/RandomWeaponGetter.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RandomWeaponGetter
+{
+    static int _sharedTagChanceIncrementBase = 10;
+    static int _sharedTagChanceIncrementPerTag = 1;
+    public static WeaponData GetWeapon(bool useBuildTags = true, WeaponFlags.IWeaponPool weaponPool = WeaponFlags.IWeaponPool.None)
+    {
+        var availableWeapons = UnlockmentsManager.UnlockedWeapons;
+        if(weaponPool != WeaponFlags.IWeaponPool.None)
+        {
+            availableWeapons = availableWeapons.Where(x => (x.WeaponPools & weaponPool) != WeaponFlags.IWeaponPool.None).ToList();
+        }
+
+        if(!useBuildTags)
+        {
+            return availableWeapons[Random.Range(0, availableWeapons.Count)];
+        }
+        var heldTags = PlayerControl.pc.WeaponManager.HeldWeaponTags;
+        Dictionary<WeaponData, int> rouletteMaterial = new Dictionary<WeaponData, int>();
+        foreach(var weaponData in availableWeapons)
+        {
+            rouletteMaterial.Add(weaponData, 10);
+        }
+
+        foreach(var weaponData in availableWeapons)
+        {
+            var sharedTags = weaponData.WeaponTags.Intersect(heldTags.Keys).Count();
+            var chanceIncrement = sharedTags == 0 ? 0 : _sharedTagChanceIncrementBase + sharedTags * _sharedTagChanceIncrementPerTag - 1;
+            rouletteMaterial[weaponData] += chanceIncrement;
+        }
+        Roulette<WeaponData> weaponRoulette = new(rouletteMaterial);
+        return weaponRoulette.Spin();
+    }
+}
