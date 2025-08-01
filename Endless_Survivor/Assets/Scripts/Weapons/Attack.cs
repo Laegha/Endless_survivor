5af048169d0982aea1eca8de7980da6dc243086a
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-08-01 14:26:45 -0300

    feat(Attack): Reference to parent weapon

--- Endless_Survivor/Assets/Scripts/Weapons/Attack.cs
+++ Endless_Survivor/Assets/Scripts/Weapons/Attack.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Attack : MonoBehaviour
 {
     [SerializeField] AttackEffectsHandler _effectsHandler;
+    Weapon _parentWeapon;
     int _attackDamage = 0;
     float _attackDamageMultiplier = 1;
     public AttackEffectsHandler EffectsHandler { get { return _effectsHandler; } }
     public int AttackDamage { get { return (int)(_attackDamage * _attackDamageMultiplier); } set { _attackDamage = value; } }
     public float AttackDamageMultiplier { get { return _attackDamageMultiplier; } set { _attackDamageMultiplier = value; } }
+    public Weapon ParentWeapon { get { return _parentWeapon; } set { _parentWeapon = value; } }
 }
