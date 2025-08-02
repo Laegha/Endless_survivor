78911d12c01fd43f90db990f80b6e0cf9300f30e
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-08-02 18:15:56 -0300

    feat(WeaponDataTransferInterface): Transfer the attack effects as a new AttackEffectsHolder

--- Endless_Survivor/Assets/Scripts/Weapons/WeaponDataTransferInterface.cs
+++ Endless_Survivor/Assets/Scripts/Weapons/WeaponDataTransferInterface.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [System.Serializable]
@@ -11,6 +12,9 @@ public class WeaponDataTransferInterface
 
         weapon.WeaponStats = new WeaponStats(weaponStats);
         weapon.WeaponData = weaponData;
+        AttackEffectsHolder attackEffectsHolder = new();
+        attackEffectsHolder.availableEffects = weaponData.AttackEffects.ToList();
+        weapon.WeaponAttackEffects = attackEffectsHolder;
         WeaponControl weaponControl = weaponObject.GetComponent<WeaponControl>();
         weaponControl.WeaponAnimator.AddAnimations(weaponData.Animations);
 
