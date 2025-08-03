1d627a7bc33c4f641b19ab039ca95cd2ba042b1f
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-08-03 01:46:11 -0300

    fix(Unlockments management): removed UnlockmentsHolder, since it's no longer used

--- Endless_Survivor/Assets/Scripts/GameManager.cs
+++ Endless_Survivor/Assets/Scripts/GameManager.cs
@@ -13,28 +13,9 @@ public class GameManager : MonoBehaviour
     public CharacterData selectedCharacter;
 
     public PrefabHolder prefabHolder;
-    public UnlockmentsHolder unlockmentsHolder;
 
-    static string unlockmentsFileName = "unlockments.json";
-    static string unlockmentsFilePath = Path.Combine(Application.streamingAssetsPath, unlockmentsFileName);
-    static string unlockmentsJson = File.ReadAllText(unlockmentsFilePath);
     [HideInInspector] public List<CharacterData> unlockedCharacters;
     [HideInInspector] public List<WeaponData> unlockedWeapons;
-
-    int _gachaCoins = 0;
-    public int GachaCoins 
-    { get { return _gachaCoins; } 
-        set 
-        {
-            _gachaCoins = value;
-            UnlockmentsConverter unlockments = JsonConvert.DeserializeObject<UnlockmentsConverter>(unlockmentsJson);
-            unlockments.gachaCoins = value;
-            string json = JsonConvert.SerializeObject(unlockments);
-            File.WriteAllText(unlockmentsFilePath, json);
-
-        }
-    }
-
     [SerializeField] AudioMixer _audioMixer;
 
     [SerializeField] WeaponData _testWeapon;
@@ -58,20 +39,6 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
        _testWeapon = Resources.Load("ScriptableObjects/Weapons/Balance") as WeaponData;
-    }
-    public UnlockmentsConverter Unlockments 
-    {
-        get
-        {
-            string path = Path.Combine(Application.streamingAssetsPath, unlockmentsFileName);
-            string json = File.ReadAllText(path);
-            UnlockmentsConverter unlockmentsData = JsonConvert.DeserializeObject<UnlockmentsConverter>(json);
-            return unlockmentsData;
-        }
-    }
-    public void StartRun()
-    {
-        unlockedWeapons = unlockmentsHolder.UnlockedWeapons();
     }
     public void SetVolume(string volumeGroup, float volume)
     {
