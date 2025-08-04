dc76ee121458e28610553d2b4c2517955eface5f
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-08-04 00:32:08 -0300

    feat(ScalingFunctions): Separated true level and induced level increase

--- Endless_Survivor/Assets/Scripts/ScalingFunctions.cs
+++ Endless_Survivor/Assets/Scripts/ScalingFunctions.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 
 public static class ScalingFunctions
 {
+    static float _weaponStatIncreaseTrueLevelBase = 2;
+    static float _weaponStatIncreaseInducedLevelBase = 8;
     public static int EnemyHPIncrease(int x)
     {
         return 0;
     }
 
-    public static float WeaponStatIncrease(float increaseScale, int x)
+    public static float WeaponStatIncreaseTrueLevel(float increaseScale, int level)
     {
-        float result = increaseScale * Mathf.Log(x, 2);
+        float result = increaseScale * Mathf.Log(level+1, _weaponStatIncreaseTrueLevelBase);//+1 because else lvl 1 would be always 0
         return result;
     }
-
+    public static float WeaponStatIncreaseInducedLevel(float increaseScale, int x)
+    {
+        float result = increaseScale * Mathf.Log(x+1, _weaponStatIncreaseInducedLevelBase);//+1 because else lvl 1 would be always 0
+        return result;
+    }
 }
