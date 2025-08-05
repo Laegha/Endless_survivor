eba9675853bf4c59a411b59167699ada8664137a
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-08-05 01:41:16 -0300

    refactor(PassiveItemPickupData): no longer using animations for a pedestal, will use particles instead

--- Endless_Survivor/Assets/Scripts/Pickups/PassiveItemPickupData.cs
+++ Endless_Survivor/Assets/Scripts/Pickups/PassiveItemPickupData.cs
@@ -2,31 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using static CustomFlags;
 
 [CreateAssetMenu(fileName = "New Passive Item Pickup", menuName = "ScriptableObjects/Pickups/PassiveItem", order = 2)]
 public class PassiveItemPickupData : PickupData
 {
     [SerializeField] CustomFlags.IPassiveItemPool _itemPools;
     static readonly string _itemVariableKey = "pickupItem";
-    [SerializeField] CustomAnimation _itemBaseIdle;
-    [SerializeField] CustomAnimation _itemBasePickup;
     public override void TransferData(PickupControl pickupControl)
     {
         base.TransferData(pickupControl);
-        var unlockedPassiveItems = UnlockmentsManager.UnlockedPassiveItems;
-        var availableItems = unlockedPassiveItems.Where(x => (x.ItemPools & _itemPools) != CustomFlags.IPassiveItemPool.None).ToList();
+        var availableItems = UnlockmentsManager.UnlockedPassiveItems;
+        if (_itemPools != CustomFlags.IPassiveItemPool.None)
+            availableItems = availableItems.Where(x => (x.ItemPools & _itemPools) != CustomFlags.IPassiveItemPool.None).ToList();
         
         PassiveItemData pickupPassiveItem = availableItems[Random.Range(0, availableItems.Count)];//get a random passive
         pickupControl.Pickup.AddVariable(_itemVariableKey, pickupPassiveItem);
-        pickupControl.Animator.AddAnimations(new List<CustomAnimation> { _itemBaseIdle, _itemBasePickup });
-        pickupControl.Animator.ChangeAnim(_itemBaseIdle.AnimationName);
-        //pickupControl.Renderer.sprite 
+        pickupControl.Renderer.sprite = pickupPassiveItem.ItemSprite;
     }
     public override void PickUp(PickupControl pickupControl)
     {
         base.PickUp(pickupControl);
-        pickupControl.Animator.ChangeAnim(_itemBasePickup.AnimationName);
         GameUIManager.uiManager.PassiveItemPickupMenu.DisplayMenu(pickupControl.Pickup.GetVariable<PassiveItemData>(_itemVariableKey));
-        Destroy(pickupControl.gameObject, _itemBasePickup.AnimDuration);
     }
 }
