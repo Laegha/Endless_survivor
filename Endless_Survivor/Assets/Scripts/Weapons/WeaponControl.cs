17791752e35292e84fd79640bb5ac62b636baa95
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-09-02 15:19:04 -0300

    fea(WeaponControl): Reference to WeaponAim used by ShootingWeapon

--- Endless_Survivor/Assets/Scripts/Weapons/WeaponControl.cs
+++ Endless_Survivor/Assets/Scripts/Weapons/WeaponControl.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class WeaponControl : MonoBehaviour
 {
+    [SerializeField] WeaponAim _weaponAim;
     [SerializeField] CustomAnimator _weaponAnimator;
     [SerializeField] SpriteRenderer _gfx;
     [SerializeField] ParticleSystem _onLevelUpParticle;
+    public WeaponAim WeaponAim { get { return _weaponAim; } }
     public CustomAnimator WeaponAnimator { get { return _weaponAnimator; } }
     public SpriteRenderer Gfx { get { return _gfx; } }
     public ParticleSystem OnLevelUpParticle { get { return _onLevelUpParticle; } }
