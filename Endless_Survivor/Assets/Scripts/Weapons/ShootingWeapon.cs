83b328c12be3a6ce65b78f305c83d0ef096b3b4a
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-09-02 15:14:05 -0300

    feat(ShootingWeapon): invert firepoint position depending on sprite flip

--- Endless_Survivor/Assets/Scripts/Weapons/ShootingWeapon.cs
+++ Endless_Survivor/Assets/Scripts/Weapons/ShootingWeapon.cs
@@ -5,5 +5,25 @@ using UnityEngine;
 public class ShootingWeapon : Weapon
 {
     Transform _firePoint;
+    bool _isYFlipped = false;
+    bool _isXFlipped = false;
+    Vector2 _firePointPos;
     public Transform FirePoint { get { return _firePoint; } set { _firePoint = value; } }
+
+    public override void Start()
+    {
+        base.Start();
+        _firePointPos = _firePoint.localPosition;
+        //WeaponControl.WeaponAim.ChangeDirectionBase(_firePoint);
+    }
+    public override void Update()
+    {
+        base.Update();
+        if(_isYFlipped != WeaponControl.Gfx.flipY || _isXFlipped != WeaponControl.Gfx.flipX)
+        {
+            _isYFlipped = WeaponControl.Gfx.flipY;
+            _isXFlipped = WeaponControl.Gfx.flipX;
+            _firePoint.localPosition = new Vector2(_isXFlipped ? -_firePointPos.x : _firePointPos.x, _isYFlipped ? -_firePointPos.y : _firePointPos.y);
+        }
+    }
 }
