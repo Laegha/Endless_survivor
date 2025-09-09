4f115d05a2eecbe08a441817b6163de5a2c613f1
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-09-09 04:24:23 -0300

    refactor(ShootingWeaponDataTransferInterface): firepoint position now set in pixels, the script then divides that by the game pixel resolution (32)

--- Endless_Survivor/Assets/Scripts/Weapons/DataTransferInterfaces/ShootingWeaponDataTransferInterface.cs
+++ Endless_Survivor/Assets/Scripts/Weapons/DataTransferInterfaces/ShootingWeaponDataTransferInterface.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class ShootingWeaponDataTransferInterface : WeaponDataTransferInterface
 {
+    [Header("Fire point position in pixels (from center of sprite, set in the sprite editor")]
     [SerializeField] Vector2 _firePointPosition;
     public override void TransferData(GameObject weaponObject, WeaponData weaponData, WeaponStats weaponStats)
     {
         Transform firePoint = new GameObject().transform;
         firePoint.transform.SetParent(weaponObject.transform, false);
         firePoint.name = "FirePoint";
-        firePoint.localPosition = _firePointPosition;
+        firePoint.localPosition = _firePointPosition / 32;
         firePoint.localRotation = Quaternion.identity;
         weaponObject.GetComponent<ShootingWeapon>().FirePoint = firePoint;
         
