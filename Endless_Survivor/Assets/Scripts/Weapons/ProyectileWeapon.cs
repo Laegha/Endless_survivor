7001cf8dab47f6f2bb02acbc505c52dfbac0fbcf
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-09-12 19:34:19 -0300

    feat(ProyectileWeapon): Defined the offset attack function by applying the offset x along the firepoint direction and y along its perpendicular

--- Endless_Survivor/Assets/Scripts/Weapons/ProyectileWeapon.cs
+++ Endless_Survivor/Assets/Scripts/Weapons/ProyectileWeapon.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
 using UnityEngine;
 
 public class ProyectileWeapon : ShootingWeapon
@@ -11,24 +12,41 @@ public class ProyectileWeapon : ShootingWeapon
     public float ProyectileSpread { get { return _proyectileSpread; } set { _proyectileSpread = value; } }
     public float ProyectileSpeed { get { return _proyectileSpeed; } set { _proyectileSpeed = value; } }
 
-    public override void Start()
-    {
-        base.Start();
-        InitializeAttack += InitiateProyectile;
-    }
     public override void Attack()
     {
         base.Attack();
         WeaponControl.WeaponAnimator.ChangeAnim("Attack");
         Proyectile proyectile = Instantiate(GameManager.gm.prefabHolder.Prefabs["Proyectile"]).GetComponent<Proyectile>();
         InitializeAttack?.Invoke(proyectile);
+        InitiateProyectile(proyectile, FirePoint.position, FirePoint.rotation);
+    }
+    public override void Attack(Vector2 attackPosOffset, float attackRotationOffset, bool isSecondaryAttack)
+    {
+        base.Attack(attackPosOffset, attackRotationOffset, isSecondaryAttack);
+
+        WeaponControl.WeaponAnimator.ChangeAnim("Attack");
+        Proyectile proyectile = Instantiate(GameManager.gm.prefabHolder.Prefabs["Proyectile"]).GetComponent<Proyectile>();
+        InitializeAttack?.Invoke(proyectile);
+        var firepointRotation = FirePoint.eulerAngles.z * Mathf.Deg2Rad;
+        //Vector2 offsetYDirection = ; // (p1, p2) * (o1, o2) = p1*o1 + p2*o2 = 0 => o2p2 = -o1p1 => p2 = -o1/o2p1
+        var pixelOffset = attackPosOffset / 32;
+        Vector2 offsetXDirection = -new Vector2(Mathf.Cos(firepointRotation), Mathf.Sin(firepointRotation)).normalized;//it's inverted because the firepoint faces left
+        Vector2 offsetYDirection = new Vector2(1, (-offsetXDirection.x/offsetXDirection.y) * 1).normalized;
+        Debug.Log("FIREPOINT ANGLE:" + FirePoint.eulerAngles.z + " DIRECTION: " + offsetXDirection);
+        Vector2 offsetXMovement = offsetXDirection * pixelOffset.x;
+        Vector2 offsetYMovement = offsetYDirection * pixelOffset.y;
+
+        Vector2 offsetMovement = offsetXMovement + offsetYMovement;
+        
+        proyectile.IsSecondaryAttack = isSecondaryAttack;
+
+        InitiateProyectile(proyectile,(Vector2)FirePoint.position + (offsetXDirection.x * offsetXDirection.y > 0 ? offsetMovement : -offsetMovement), FirePoint.rotation);// I only tested this with y movement, idk with x
 
     }
-    void InitiateProyectile(Attack affectedAttack)
+    void InitiateProyectile(Proyectile proyectile, Vector2 proyectilePosition, Quaternion proyectileRotation)
     {
-        var proyectile = affectedAttack as Proyectile;
-        proyectile.transform.position = FirePoint.position;
-        proyectile.transform.rotation = FirePoint.rotation;
+        proyectile.transform.position = proyectilePosition;
+        proyectile.transform.rotation = proyectileRotation;
 
         float proyectileSpeed = _proyectileSpeed;
         float proyectileLifeTime = WeaponStats.Range;
