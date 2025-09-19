006845805413b9f74dbe753e6bf10f63cef525ea
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-09-19 02:16:47 -0300

    refactor(SortingOrderByY): just put the world y position multiplied by -100 for decimal precision

--- Endless_Survivor/Assets/Scripts/Reusable/SortingOrderByY.cs
+++ Endless_Survivor/Assets/Scripts/Reusable/SortingOrderByY.cs
@@ -4,26 +4,29 @@ using UnityEngine;
 
 public class SortingOrderByY : MonoBehaviour
 {
-    [SerializeField] int _sortingOrder;
+    //[SerializeField] int _sortingOrder;
     [SerializeField] SpriteRenderer[] _affectedRenderers;
-    static readonly int _minOrder = -15;
-    public static int MinOrder {  get { return _minOrder; } }
-    public int SortingOrder { set { _sortingOrder = value; } }
+    //static readonly int _minOrder = -15;
+    //public static int MinOrder {  get { return _minOrder; } }
+    //public int SortingOrder { set { _sortingOrder = value; } }
     public SpriteRenderer[] AffectedRenderers {  set { _affectedRenderers = value; } }
 
     private void LateUpdate()
     {
-        float distY = PlayerControl.pc.transform.position.y - transform.position.y;
-        if(distY < 0)
-        {
-            foreach(var renderer in _affectedRenderers)
-                renderer.sortingOrder = _minOrder + _sortingOrder;
-        }
-        else
-        {
-            foreach(var renderer in _affectedRenderers)
-                renderer.sortingOrder = PlayerControl.pc.MainRenderer.sortingOrder + _sortingOrder;
+        //float distY = PlayerControl.pc.transform.position.y - transform.position.y;
+        //if(distY < 0)
+        //{
+        //    foreach(var renderer in _affectedRenderers)
+        //        renderer.sortingOrder = _minOrder + _sortingOrder;
+        //}
+        //else
+        //{
+        //    foreach(var renderer in _affectedRenderers)
+        //        renderer.sortingOrder = PlayerControl.pc.MainRenderer.sortingOrder + _sortingOrder;
 
-        }
+        //}
+
+        foreach (var renderer in _affectedRenderers)
+            renderer.sortingOrder = (int)(-renderer.transform.position.y * 100);
     }
 }
