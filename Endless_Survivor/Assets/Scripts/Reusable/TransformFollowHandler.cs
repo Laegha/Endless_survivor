d1a776cdfb2d70ad518a097490ca100e1bd2d3df
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-09-19 00:51:15 -0300

    feat(TransformFollowHandler): class that takes a child and a parent transforms and copies either its position, rotation or both

---
+++ Endless_Survivor/Assets/Scripts/Reusable/TransformFollowHandler.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformFollowHandler
+{
+    public Transform child;
+    public Transform parent;
+    public bool copyPosition;
+    public bool copyRotation;
+
+    public TransformFollowHandler(Transform child, Transform parent, bool copyPosition, bool copyRotation)
+    {
+        this.child = child;
+        this.parent = parent;
+        this.copyPosition = copyPosition;
+        this.copyRotation = copyRotation;
+    }
+
+    public void Update()
+    {
+        if(copyPosition)
+            child.position = parent.position;
+        if(copyRotation)
+            child.rotation = parent.rotation;
+    }
+}
