235c91526350e6c623e2bb3879d269b9d2644266
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-09-21 23:48:55 -0300

    feat(ColliderData):Stores the basic data to generate a collider. Could use a layer mask

---
+++ Endless_Survivor/Assets/Scripts/Reusable/ColliderData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderData 
+{
+    public enum ColliderType
+    {
+        Capusle,
+        Box
+    }
+    public ColliderType colType;
+    public Vector2 size;
+    public Vector2 position;
+    public CapsuleDirection2D capsuleDirection;
+}
