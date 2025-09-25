bf718c5a6d69d089044bf06f43bfbbafd930cdc1
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-09-25 15:09:59 -0300

    feat(SpawnParticlesOnAttackAttackEffect)

---
+++ Endless_Survivor/Assets/Scripts/AttackEffects/SpawnParticlesOnAttackAttackEffect.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnParticlesOnAttackAttackEffect : AttackEffect
+{
+    new public static bool isUsable => true;
+    [SerializeField] ParticleSystem _onEnemyHitParticles;
+    [SerializeField] float _particlesDuration;
+    public SpawnParticlesOnAttackAttackEffect(AttackEffect original, Attack affectedAttack) : base(original, affectedAttack)
+    {
+
+    }
+    public override void Initiate(AttackEffect original, Attack affectedAttack)
+    {
+        base.Initiate(original, affectedAttack);
+        var originalSpawnParticles = original as SpawnParticlesOnAttackAttackEffect;
+        _onEnemyHitParticles = originalSpawnParticles._onEnemyHitParticles;
+        _particlesDuration = originalSpawnParticles._particlesDuration;
+        OnAttack += SpawnParticles;
+    }
+    void SpawnParticles()
+    {
+        ParticleConfig particles = new(_onEnemyHitParticles, AffectedAttack.ParentWeapon.transform.position, Quaternion.identity, _particlesDuration);
+        ParticleManager.pm.SpawnParticles(particles);
+    }
+}
