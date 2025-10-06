255d2ce19653a923ba4be6759b25489197f979e6
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-10-06 20:52:00 -0300

    refactor(SupportObjControl): Changed the renderer array for a list to allow more usage of it

--- Endless_Survivor/Assets/Scripts/SupportObjects/SupportObjectControl.cs
+++ Endless_Survivor/Assets/Scripts/SupportObjects/SupportObjectControl.cs
@@ -7,10 +7,10 @@ public class SupportObjectControl : MonoBehaviour
     [SerializeField] SupportObjectBehaviourManager _behaviourManager;
     [SerializeField] Transform _colliderHolder;
     [SerializeField] CustomAnimator _animator;
-    [SerializeField] SpriteRenderer[] _renderers;
+    [SerializeField] List<SpriteRenderer> _renderers;
 
     public SupportObjectBehaviourManager BehaviourManager { get { return _behaviourManager; } }
     public Transform ColliderHolder { get { return _colliderHolder; } }
     public CustomAnimator Animator { get { return _animator; } }
-    public SpriteRenderer[] Renderers { get { return _renderers; } }
+    public List<SpriteRenderer> Renderers { get { return _renderers; } }
 }
