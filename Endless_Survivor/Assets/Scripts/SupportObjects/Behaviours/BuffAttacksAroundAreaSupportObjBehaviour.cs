f802749e7757c8408c87d246af7f0fc5bdbf437d
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-10-06 20:59:21 -0300

    feat(BuffAttacksAroundAreaSupportObjBehaviour): Buffs attacks on enter and spawns an indicator for it that follows

---
+++ Endless_Survivor/Assets/Scripts/SupportObjects/Behaviours/BuffAttacksAroundAreaSupportObjBehaviour.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffAttacksAroundAreaSupportObjBehaviour : UseAreaAroundSupportObjBehaviour
+{
+    new public static bool isUsable => true;
+    [SerializeField] float _attackDamageMultiplierIncrease = 2;
+    //[SerializeField] AttackGfxInterface _attackGfxChange;
+    [SerializeField] GameObject _buffedAttackGfxIndicatorPrefab;
+    List<TransformFollowHandler> _indicatorsFollowHandlers = new();
+    public override void Initiate(SupportObjectControl control, SupportObjectBehaviour original)
+    {
+        base.Initiate(control, original);
+        var buffAttackOriginal = original as BuffAttacksAroundAreaSupportObjBehaviour;
+        _attackDamageMultiplierIncrease = buffAttackOriginal._attackDamageMultiplierIncrease;
+        _buffedAttackGfxIndicatorPrefab = buffAttackOriginal._buffedAttackGfxIndicatorPrefab;
+        OnObjEnterArea += BuffAttack;
+        OnUpdate += UpdateFollowHandlers;
+    }
+    void BuffAttack(GameObject enteredObj)
+    {
+        Attack attack = enteredObj.GetComponent<Attack>();
+        if (attack == null)
+            return;
+        attack.AttackDamageMultiplier += _attackDamageMultiplierIncrease;
+        var buffIndicator = GameObject.Instantiate(_buffedAttackGfxIndicatorPrefab);
+        TransformFollowHandler buffIndicatorFollowHandler = new(buffIndicator.transform, attack.transform, true, false);
+        _indicatorsFollowHandlers.Add(buffIndicatorFollowHandler);
+    }
+    void UpdateFollowHandlers()
+    {
+        var indicatorsFollowHandlersCopy = new List<TransformFollowHandler>(_indicatorsFollowHandlers);
+        foreach(TransformFollowHandler handler in indicatorsFollowHandlersCopy)
+        {
+            if(handler.parent == null)
+            {
+                GameObject.Destroy(handler.child.gameObject);
+                _indicatorsFollowHandlers.Remove(handler);
+                continue;
+            }
+            handler.Update();
+        }
+    }
+}
