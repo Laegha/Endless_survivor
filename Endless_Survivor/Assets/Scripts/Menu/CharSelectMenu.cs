ce81447afa255e8168dd7e4925fe59b9524d50d8
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-10-06 20:33:31 -0300

    fix(CharSelectMenu): Scale grid to match the height of the displayed characters
    This way the scroll doesn't bug

--- Endless_Survivor/Assets/Scripts/Menu/CharSelectMenu.cs
+++ Endless_Survivor/Assets/Scripts/Menu/CharSelectMenu.cs
@@ -10,7 +10,14 @@ public class CharSelectMenu : MonoBehaviour
     [SerializeField] CharacterButton _selectCharBtnPrefab;
     [SerializeField] GameObject _newCharIndicatorPrefab;
     [SerializeField] HorizontalLayoutGroup _gridRowPrefab;
-    [SerializeField] RectTransform _buttonGrid;
+    [SerializeField] RectTransform _buttonGridTr;
+    [SerializeField] RectTransform _returnButton;
+    VerticalLayoutGroup _buttonGrid;
+
+    private void Start()
+    {
+        _buttonGrid = _buttonGridTr.GetComponent<VerticalLayoutGroup>();
+    }
 
     public void DisplayMenu()
     {
@@ -19,7 +26,8 @@ public class CharSelectMenu : MonoBehaviour
     }
     async void DisplayCharacters()
     {
-        var currentRow = Instantiate(_gridRowPrefab, _buttonGrid);
+        var currentRow = Instantiate(_gridRowPrefab, _buttonGridTr);
+        int gridRows = 1;
         float rowFilledSpace = 0;
         var unlockedCharacters = await UnlockmentsManager.UnlockedCharacters();
         foreach (var character in unlockedCharacters)
@@ -33,20 +41,25 @@ public class CharSelectMenu : MonoBehaviour
                 Instantiate(_newCharIndicatorPrefab, characterButton.imageTargetSize.transform);
 
             rowFilledSpace += _selectCharBtnPrefab.imageTargetSize.sizeDelta.x + currentRow.spacing;
-            if (rowFilledSpace + _selectCharBtnPrefab.imageTargetSize.sizeDelta.x > _buttonGrid.sizeDelta.x)
+            if (rowFilledSpace + _selectCharBtnPrefab.imageTargetSize.sizeDelta.x > _buttonGridTr.sizeDelta.x)
             {
-                currentRow = Instantiate(_gridRowPrefab, _buttonGrid);
+                currentRow = Instantiate(_gridRowPrefab, _buttonGridTr);
+                gridRows ++;
                 rowFilledSpace = 0;
             }
         }
+        float rowFilledSize = (_buttonGrid.spacing + _gridRowPrefab.GetComponent<RectTransform>().sizeDelta.y) * gridRows;
+        _buttonGridTr.sizeDelta = Vector2.right * _buttonGridTr.sizeDelta + Vector2.up * (rowFilledSize + _buttonGrid.padding.top + _returnButton.sizeDelta.y);
+
+
     }
 
     public void CloseMenu()
     {
-        Transform[] buttons = _buttonGrid.GetComponentsInChildren<Transform>();
+        Transform[] buttons = _buttonGridTr.GetComponentsInChildren<Transform>();
         foreach (Transform button in buttons)
         {
-            if (button != _buttonGrid)
+            if (button != _buttonGridTr)
                 Destroy(button.gameObject);
         }
         _menuObj.SetActive(false);
