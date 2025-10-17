61212e94c79112154f86f23c83827f2337288647
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-10-17 02:39:28 -0300

    feat(PlayerGFXChanger): Handles material overrides and particles for the player

---
+++ Endless_Survivor/Assets/Scripts/Reusable/PlayerGFXChanger.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class PlayerGFXChanger
+{
+    //allow to stack
+    //MAYBE add something related to costumes
+    public MaterialOverride materialOverride;
+    public ParticleSystem addedParticles;
+    ParticleSystem _instantiatedParticles;
+    bool _isApplied = false;
+
+    public void ApplyGFX()
+    {
+        if (_isApplied)
+            return;
+        _isApplied = true;
+        PlayerControl.pc.PlayerMaterialManager.SetMaterialOverride(materialOverride);
+        ParticleConfig addingParticles = new(addedParticles, PlayerControl.pc.transform.position, Quaternion.identity, -1, PlayerControl.pc.transform);
+        _instantiatedParticles = ParticleManager.pm.SpawnParticles(addingParticles);
+    }
+    public void ApplyGFX(float gfxDuration)
+    {
+        ApplyGFX();
+        GameManager.gm.DelayAction(gfxDuration, UnApplyGFX, null);
+    }
+    public void UnApplyGFX()
+    {
+        _isApplied = false;
+        PlayerControl.pc.PlayerMaterialManager.UnsetMaterialOverride(materialOverride);
+        GameObject.Destroy(_instantiatedParticles);
+
+    }
+}
