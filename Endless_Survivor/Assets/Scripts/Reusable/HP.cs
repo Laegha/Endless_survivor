a9a59ad903d67692016aa4e33794fdc6c68a5304
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-10-17 02:40:59 -0300

    feat(HP): Heal function with an onHealed Action

--- Endless_Survivor/Assets/Scripts/Reusable/HP.cs
+++ Endless_Survivor/Assets/Scripts/Reusable/HP.cs
@@ -9,9 +9,11 @@ public class HP : MonoBehaviour
     int _maxHP;
     float _incomingDamageMultiplier = 1;
     Action _onDamageTaken;
+    Action _onHealed;
     public int RemainingHP {  get { return _remainingHP; } set { _remainingHP = value; } }
     public int MaxHP {  get { return _maxHP; } set { _maxHP = value; } }
     public Action OnDamageTaken { get { return _onDamageTaken; } set { _onDamageTaken = value; } }
+    public Action OnHealed { get { return _onHealed; } set { _onHealed  = value; } }
     public float IncomingDamageMultiplier 
     {
         get { return _incomingDamageMultiplier > 0.1f ? _incomingDamageMultiplier : 0.1f; }
@@ -25,9 +27,14 @@ public class HP : MonoBehaviour
         MaxHP = maxHP;
         RemainingHP = maxHP;
     }
+    public virtual void Heal(int healedHP)
+    {
+        _remainingHP = Mathf.Clamp(_remainingHP + healedHP, 0, _maxHP);
+        _onHealed?.Invoke();
+    }
     public virtual void TakeDamage(int incomingDamage) 
     {
-        _onDamageTaken.Invoke();
+        _onDamageTaken?.Invoke();
         _remainingHP -= (int)(incomingDamage * _incomingDamageMultiplier);
         if (_remainingHP <= 0)
         {
