e365d637b939aa474afa844b9d971a28d11fa07a
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-10-26 19:47:47 -0300

    refactor(ChangeDefaultMaterialBehaviour): Add the enemy material with an authority of -1 so it gets applied before everything else

--- Endless_Survivor/Assets/Scripts/Enemies/Behaviours/ChangeDefaultMaterialBehaviour.cs
+++ Endless_Survivor/Assets/Scripts/Enemies/Behaviours/ChangeDefaultMaterialBehaviour.cs
@@ -8,30 +8,18 @@ using UnityEngine;
 public class ChangeDefaultMaterialBehaviour : EnemyBehaviour
 {
     [SerializeField] bool _changeAllRenderers;
-    [Header("USE ONLY IF CHANGEALLRENDERERS IS FALSE")]
-    [SerializeField] List<RendererPathMaterialInfo> _separateRenderersMaterials;
-    [Header("USE ONLY IF CHANGEALLRENDERERS IS TRUE")]
-    [SerializeField] Material _globalMaterial;
+    [SerializeField] Material _defaultMat;
     public override void Initialize(EnemyBehaviour original, EnemyControl enemyControl)
     {
         base.Initialize(original, enemyControl);
         var changeMaterialOriginal = original as ChangeDefaultMaterialBehaviour;
         _changeAllRenderers = changeMaterialOriginal._changeAllRenderers;
-        _separateRenderersMaterials = changeMaterialOriginal._separateRenderersMaterials;
-        _globalMaterial = changeMaterialOriginal._globalMaterial;
+        _defaultMat = changeMaterialOriginal._defaultMat;
     }
     public override void Start()
     {
         base.Start();
-        if(_changeAllRenderers)
-            EnemyControl.MaterialManager.ChangeDefaultMaterial(_globalMaterial);
-        else
-        {
-            foreach(var renderer in _separateRenderersMaterials)
-            {
-                EnemyControl.MaterialManager.ChangeDefaultMaterial(renderer.rendererPath, renderer.material);
-            }
-        }
-        EnemyControl.gameObject.name = "TESTING MATERIAL CHANFGE";
+        MaterialOverride defaultMaterialOverride = new(-1, _defaultMat);
+        EnemyControl.MaterialManager.SetMaterialOverride(defaultMaterialOverride);
     }
 }
