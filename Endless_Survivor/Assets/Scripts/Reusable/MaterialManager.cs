44d1093d8aade1ab3f78f0d3c896d42925a8b474
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-10-26 19:45:54 -0300

    refactor(MaterialManager): Instead of applying a different material to the renderers, apply all materials at once
    The script takes the texture from the sprite and applies each material using Graphics.Blit. then, creates a new sprite using the modified texture and applies it to the object

--- Endless_Survivor/Assets/Scripts/Reusable/MaterialManager.cs
+++ Endless_Survivor/Assets/Scripts/Reusable/MaterialManager.cs
@@ -3,78 +3,100 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class MaterialManager : MonoBehaviour
 {
     [SerializeField] List<SpriteRenderer> _renderers;
-    MaterialOverride _currentOverride;
-    List<MaterialOverride> _overridesQueue = new();   
     Dictionary<SpriteRenderer, Material> _defaultMaterials = new();
+    List<MaterialOverride> _overridesQueue = new();
+    Dictionary<SpriteRenderer, Sprite> _sprites = new();
 
     private void Awake()
     {
         foreach (var renderer in _renderers)
         {
             _defaultMaterials.Add(renderer, renderer.material);
+            _sprites.Add(renderer, null);
         }
     }
-
+    private void Update()
+    {
+        UpdateMaterials();
+    }
     public void SetMaterialOverride(MaterialOverride newMaterial)
     {
         _overridesQueue.Add(newMaterial);
-        if(_currentOverride == null || newMaterial.authority > _currentOverride.authority)
-            UpdateMaterials();
+        SortMaterials();
     }
     public void UnsetMaterialOverride(MaterialOverride unsetMaterial)
     {
         _overridesQueue.Remove(unsetMaterial);
-        if(unsetMaterial == _currentOverride)
-            UpdateMaterials();
+        SortMaterials();
 
     }
+    void SortMaterials() => _overridesQueue.Sort(new MaterialAuthorityComparer());
     void UpdateMaterials()
     {
-        _overridesQueue.Sort(new MaterialAuthorityComparer());
-        _currentOverride = _overridesQueue.LastOrDefault();
         foreach (var renderer in _renderers)
         {
-            renderer.material = _currentOverride != null ? _currentOverride.material : _defaultMaterials[renderer];
+            if (_sprites[renderer] == renderer.sprite)
+                continue;
+            //get input texture
+            //Rect subtexRect = renderer.sprite.rect;
+            //print("RECT: " + subtexRect);
+            //Color[] subtexPixels = renderer.sprite.texture.GetPixels((int)subtexRect.x, (int)subtexRect.y, (int)subtexRect.width, (int)subtexRect.height);
+            //Texture2D inputTex = new Texture2D((int)subtexRect.width, (int)subtexRect.height);
+            //inputTex.SetPixels(subtexPixels);
+            Texture2D inputTex = renderer.sprite.texture;
+            Rect textureRect = renderer.sprite.rect;
+            print("RECT: " + textureRect);
+
+            //create an output texture
+            RenderTexture tempRT = RenderTexture.GetTemporary(inputTex.width, inputTex.height);
+            Texture2D outputTex = inputTex;
+            //apply materials to output rt
+            foreach (var matOverride in _overridesQueue)
+            {
+                Graphics.Blit(outputTex, tempRT, matOverride.material);
+                RenderTexture.active = tempRT;
+
+                //extract the texture from the RT with the new material
+                Texture2D extractedTex = new(tempRT.width, tempRT.height);
+                extractedTex.ReadPixels(textureRect, (int)textureRect.x, (int)textureRect.y);
+                extractedTex.Apply();
+                RenderTexture.active = null;
+
+                outputTex = extractedTex;
+            }
+            outputTex.filterMode = inputTex.filterMode;
+
+            //Rect subtexRect = renderer.sprite.rect;
+            //Color[] subtexPixels = renderer.sprite.texture.GetPixels((int)subtexRect.x, (int)subtexRect.y, (int)subtexRect.width, (int)subtexRect.height);
+
+            //create sprite with outputTex and apply it
+            Vector2 pivot = renderer.sprite.pivot;
+            Sprite newSprite = Sprite.Create(outputTex, textureRect, new Vector2(pivot.x / textureRect.width, pivot.y / textureRect.height), renderer.sprite.pixelsPerUnit);
+            newSprite.name = renderer.sprite.name;
+            renderer.sprite = newSprite;
+            _sprites[renderer] = renderer.sprite;
+
         }
     }
     public void AddRenderer(SpriteRenderer renderer)
     {
         _renderers.Add(renderer);
         _defaultMaterials.Add(renderer, renderer.material);
-    }
-    public void ChangeDefaultMaterial(string rendererTransformPath, Material newMaterial)
-    {
-        var defaultMaterialsCopy = new Dictionary<SpriteRenderer, Material>(_defaultMaterials);
-        foreach(var defaultRenderer in defaultMaterialsCopy)
-        {
-            if(Utility.GetTransfromPath(defaultRenderer.Key.transform) == rendererTransformPath)
-            {
-                _defaultMaterials[defaultRenderer.Key] = newMaterial;
-                defaultRenderer.Key.material = newMaterial;
-                return;
-            }
-        }
-    }
-    public void ChangeDefaultMaterial(Material newMaterial)
-    {
-        var defaultMaterialsCopy = new Dictionary<SpriteRenderer, Material>(_defaultMaterials);
-        foreach (var renderer in defaultMaterialsCopy.Keys)
-        {
-            _defaultMaterials[renderer] = newMaterial;
-            renderer.material = newMaterial;
-        }
-
+        _sprites.Add(renderer, null);
     }
     public void CleanRenderers()
     {
         foreach (var renderer in _renderers)
         {
-            if(renderer == null)
+            if (renderer == null)
+            {
                 _defaultMaterials.Remove(renderer);
+            }
         }
         _renderers.RemoveAll(x => x == null);
     }
