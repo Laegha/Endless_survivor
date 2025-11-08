6755de93c1e73d5f96a35b51fbdbc712db583c6a
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-11-07 21:43:25 -0300

    feat(DamageOverTimeStatusEffect): Ends by time and continuously damages the enemy

---
+++ Endless_Survivor/Assets/Scripts/Enemies/EnemyStatusEffects/DamageOverTimeStatusEffect.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageOverTimeStatusEffect : EndByTimeStatusEffect
+{
+    new public static bool isUsable => true;
+    [SerializeField] int _damage = 1;
+    [SerializeField] float _damageFrequency = .5f;
+    float _damageTimer = 0;
+    public override void Initialize(EnemyControl affectedEnemyControl, EnemyStatusEffect original)
+    {
+        base.Initialize(affectedEnemyControl, original);
+        DamageOverTimeStatusEffect damageOverTimeOriginal = original as DamageOverTimeStatusEffect;
+        _damage = damageOverTimeOriginal._damage;
+        _damageFrequency = damageOverTimeOriginal._damageFrequency;
+
+    }
+    public override void Update()
+    {
+        base.Update();
+        _damageTimer += Time.deltaTime;
+        if(_damageTimer >= _damageFrequency)
+        {
+            AffectedEnemyControl.EnemyHP.TakeDamage(_damage);
+            _damageTimer = 0;
+        }
+    }
+}
