6c9476ecbc64648ea5086d8b9b151f18c9374ba8
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-11-07 21:50:50 -0300

    feat(CreateSupportObjOnDeathStatusEffect): Creates and initiates a supportObj when the enemy dies

---
+++ Endless_Survivor/Assets/Scripts/Enemies/EnemyStatusEffects/CreateSuportObjsOnDeathStatusEffect.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class CreateSuportObjsOnDeathStatusEffect : EnemyStatusEffect
+{
+    new public static bool isUsable => true;
+    [SerializeField] GenericAmmountHolder<SupportObjectData>[] _createdObjsDatas;
+    public override void Initialize(EnemyControl affectedEnemyControl, EnemyStatusEffect original)
+    {
+        base.Initialize(affectedEnemyControl, original);
+        var createSupportObjOriginal = original as CreateSuportObjsOnDeathStatusEffect;
+        _createdObjsDatas = createSupportObjOriginal._createdObjsDatas;
+    }
+    public override void EnemyKilled()
+    {
+        base.EnemyKilled();
+
+        foreach(var obj in _createdObjsDatas)
+        {
+            for(int i = 0; i < obj.ammount; i ++)
+            {
+                var spawnedObj = GameObject.Instantiate(GameManager.gm.prefabHolder.Prefabs["SupportObject"], AffectedEnemyControl.transform.position, Quaternion.identity).GetComponent<SupportObjectControl>();
+                obj.generic.TransferData(spawnedObj);
+            }
+
+        }
+    }
+}
