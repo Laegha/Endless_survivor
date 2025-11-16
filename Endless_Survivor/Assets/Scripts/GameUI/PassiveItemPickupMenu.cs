f25334c582b808dd6e4172837f140abd16b344f9
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-11-16 14:09:40 -0300

    feat(Pickup menus): Action onMenuOpen and handling rerolls by telling the RerollManager one was used and creating a new Weapon/PassiveItem

--- Endless_Survivor/Assets/Scripts/GameUI/PassiveItemPickupMenu.cs
+++ Endless_Survivor/Assets/Scripts/GameUI/PassiveItemPickupMenu.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,26 +11,22 @@ public class PassiveItemPickupMenu : MonoBehaviour
     [SerializeField] RectTransform _itemImageTargetSize;
     [SerializeField] TextMeshProUGUI _itemName;
     [SerializeField] TextMeshProUGUI _itemDescript;
+    [SerializeField] TextMeshProUGUI _rerollsLeftDisplay;
     PassiveItemData _currPickingItem;
     GameObject _currNewIndicator;
+    Action _onMenuOpen;
+    public Action OnMenuOpen { get { return _onMenuOpen; } set { _onMenuOpen = value; } }
     public void DisplayMenu(PassiveItemData pickingItem, bool isNew)
     {
         _menuGfx.SetActive(true);
         GameUIManager.uiManager.MenuDisplayed();
-        _currPickingItem = pickingItem;
-        _itemImage.sprite = pickingItem.ItemSprite;
-        Utility.ScaleImageToFitTarget(_itemImage.rectTransform, pickingItem.ItemSprite, _itemImageTargetSize.sizeDelta);
-        if (isNew)
-            _currNewIndicator = Instantiate(_newItemIndicator, _itemImageTargetSize);
-
-        _itemName.text = pickingItem.ItemName;
-        _itemDescript.text = pickingItem.ItemDescript;
+        SetItemMenu(pickingItem, isNew);
 
     }
     public void TakeItem()
     {
         PlayerControl.pc.PassiveItemManager.AddPassiveItem(_currPickingItem);
-        if(_currPickingItem != null )
+        if(_currPickingItem != null)
         {
             UnlockmentsManager.SetNotNewPassiveItem(_currPickingItem);
             Destroy(_currNewIndicator);
@@ -44,5 +39,30 @@ public class PassiveItemPickupMenu : MonoBehaviour
         _menuGfx.SetActive(false);
         GameUIManager.uiManager.MenuHid();
         PlayerControl.pc.WeaponManager.LevelUpWeapons();
+    }
+    public async void RerollItem()
+    {
+        if (RerollManager.rm.RerollsLeft <= 0)
+            return;
+
+        RerollManager.rm.UseReroll();
+        var availableItems = await UnlockmentsManager.UnlockedPassiveItems();
+        var newItem = availableItems[UnityEngine.Random.Range(0, availableItems.Count)];//get a random passive
+        while(newItem.element == _currPickingItem)
+            newItem = availableItems[UnityEngine.Random.Range(0, availableItems.Count)];//get a random passive
+        SetItemMenu(newItem.element, newItem.isNew);
+    }
+
+    void SetItemMenu(PassiveItemData pickingItem, bool isNew)
+    {
+        _currPickingItem = pickingItem;
+        _itemImage.sprite = pickingItem.ItemSprite;
+        Utility.ScaleImageToFitTarget(_itemImage.rectTransform, pickingItem.ItemSprite, _itemImageTargetSize.sizeDelta);
+        if (isNew)
+            _currNewIndicator = Instantiate(_newItemIndicator, _itemImageTargetSize);
+
+        _itemName.text = pickingItem.ItemName;
+        _itemDescript.text = pickingItem.ItemDescript;
+        _rerollsLeftDisplay.text = "" + RerollManager.rm.RerollsLeft;
     }
 }
--- Endless_Survivor/Assets/Scripts/GameUI/WeaponPickupMenu.cs
+++ Endless_Survivor/Assets/Scripts/GameUI/WeaponPickupMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -14,30 +15,23 @@ public class WeaponPickupMenu : MonoBehaviour
     [SerializeField] TextMeshProUGUI _weaponDmgDisplay;
     [SerializeField] TextMeshProUGUI _weaponAtkSpdDisplay;
     [SerializeField] TextMeshProUGUI _weaponRangeDisplay;
+    [SerializeField] TextMeshProUGUI _rerollsLeftDisplay;
     WeaponData _currDisplayingWeapon;
     WeaponStats _currWeaponStats;
     GameObject _currNewIndicator;
+    Action _onMenuOpen;
 
     public WeaponData CurrDisplayingWeapon {  get { return _currDisplayingWeapon; } }
     public WeaponStats CurrWeaponStats {  get { return _currWeaponStats; } }
+    public Action OnMenuOpen { get { return _onMenuOpen; } set { _onMenuOpen = value; } }
 
     public void DisplayMenu(WeaponData displayingWeapon, bool isNew)
     {
         //stop game
         _menuGfx.SetActive(true);
         GameUIManager.uiManager.MenuDisplayed();
-        _currDisplayingWeapon = displayingWeapon;
-        Utility.ScaleImageToFitTarget(_weaponImage.rectTransform, displayingWeapon.WeaponDisplaySprite, _weaponImageTargetSize.sizeDelta);
-        _weaponImage.sprite = displayingWeapon.WeaponDisplaySprite;
-        if (isNew)
-            _currNewIndicator = Instantiate(_newWeaponIndicator, _weaponImageTargetSize);
 
-        _currWeaponStats = new WeaponStats(_currDisplayingWeapon.WeaponStats);
-        _currWeaponStats.SetTrueLevelStats(_currDisplayingWeapon.StatsIncreaseScale, ScalingFunctions.CurrWaveLevel);
-        _weaponLevelDisplay.text = _currWeaponStats.TrueLevel + "";
-        _weaponDmgDisplay.text = _currWeaponStats.Damage + "";
-        _weaponAtkSpdDisplay.text = Utility.ChangeFloatDecimals(_currWeaponStats.AttackSpeed, 2) + "";
-        _weaponRangeDisplay.text = _currWeaponStats.Range + "";
+        SetMenuWeapon(displayingWeapon, isNew);
 
     }
     public void TakeWeapon()
@@ -56,5 +50,37 @@ public class WeaponPickupMenu : MonoBehaviour
     {
         GameUIManager.uiManager.MenuHid();
         _menuGfx.SetActive(false);
+    }
+
+    public async void RerollWeapon()
+    {
+        if (RerollManager.rm.RerollsLeft <= 0)
+            return;
+        RerollManager.rm.UseReroll();
+        var newWeapon = await RandomWeaponGetter.GetWeapon(true, _currDisplayingWeapon.WeaponPools);
+        while(newWeapon.element == _currDisplayingWeapon)
+            newWeapon = await RandomWeaponGetter.GetWeapon(true, _currDisplayingWeapon.WeaponPools);
+
+        SetMenuWeapon(newWeapon.element, newWeapon.isNew);
+    }
+    void SetMenuWeapon(WeaponData displayingWeapon, bool isNew)
+    {
+        _currDisplayingWeapon = displayingWeapon;
+
+        Utility.ScaleImageToFitTarget(_weaponImage.rectTransform, displayingWeapon.WeaponDisplaySprite, _weaponImageTargetSize.sizeDelta);
+        if (isNew)
+            _currNewIndicator = Instantiate(_newWeaponIndicator, _weaponImageTargetSize);
+
+        _weaponImage.sprite = displayingWeapon.WeaponDisplaySprite;
+
+        _currWeaponStats = new WeaponStats(_currDisplayingWeapon.WeaponStats);
+        _currWeaponStats.SetTrueLevelStats(_currDisplayingWeapon.StatsIncreaseScale, ScalingFunctions.CurrWaveLevel);
+
+
+        _weaponLevelDisplay.text = _currWeaponStats.TrueLevel + "";
+        _weaponDmgDisplay.text = _currWeaponStats.Damage + "";
+        _weaponAtkSpdDisplay.text = Utility.ChangeFloatDecimals(_currWeaponStats.AttackSpeed, 2) + "";
+        _weaponRangeDisplay.text = _currWeaponStats.Range + "";
+        _rerollsLeftDisplay.text = ""+RerollManager.rm.RerollsLeft;
     }
 }
