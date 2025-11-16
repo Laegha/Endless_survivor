4ed5b347b086e9d027e5897ff635cdb6f7217a78
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-11-16 13:53:31 -0300

    refactor(ShootPlayerEnemyBehaviour): Shoot and behaviour end controlled by the animation

--- Endless_Survivor/Assets/Scripts/Enemies/Behaviours/ShootPlayerBehaviour.cs
+++ Endless_Survivor/Assets/Scripts/Enemies/Behaviours/ShootPlayerBehaviour.cs
@@ -9,7 +9,7 @@ public class ShootPlayerBehaviour : EnemyBehaviour
     Transform _enemy;
     float _shootCooldown = 0;
     [SerializeField] CustomAnimation _shootingAnimation;
-    [SerializeField] float _distanceToStopShooting = 4;
+    [SerializeField] int _shootFrame = 0;
     [SerializeField] int _damage = 1;
     [SerializeField] float _atkSpeed = 2.5f;
     [SerializeField] float _proyectileSpeed = 1.5f;
@@ -22,9 +22,10 @@ public class ShootPlayerBehaviour : EnemyBehaviour
         base.Initialize(original, enemyControl);
         ShootPlayerBehaviour originalShootPlayer = original as ShootPlayerBehaviour;
         _shootingAnimation = new CustomAnimation(EnemyControl.Animator, originalShootPlayer._shootingAnimation);
+        _shootingAnimation.Events.Add(new(null, _shootFrame, Shoot));
         EnemyControl.Animator.AddAnimations(new List<CustomAnimation> { _shootingAnimation });
 
-        _distanceToStopShooting = originalShootPlayer._distanceToStopShooting;
+        _shootFrame = Mathf.Clamp(originalShootPlayer._shootFrame, 0, _shootingAnimation.Frames.Length -1);
         _damage = originalShootPlayer._damage;
         _atkSpeed = originalShootPlayer._atkSpeed;
         _proyectileSpeed = originalShootPlayer._proyectileSpeed;
@@ -37,41 +38,38 @@ public class ShootPlayerBehaviour : EnemyBehaviour
         _enemy = enemyControl.transform;
 
     }
-    public override void ActiveUpdate()
+    public override void PassiveUpdate()
     {
-        base.ActiveUpdate(); 
-        if (EnemyControl.Animator.CurrAnim == null || EnemyControl.Animator.CurrAnim.AnimationName != _shootingAnimation.AnimationName)
-            EnemyControl.Animator.ChangeAnim(_shootingAnimation.AnimationName);
+        base.PassiveUpdate();
         if (_shootCooldown > 0)
         {
             _shootCooldown -= Time.deltaTime;
-            return;
         }
+    }
+    public override void ActiveUpdate()
+    {
+        base.ActiveUpdate();
+
+        if (_shootCooldown > 0)
+            return;
+
+        if (EnemyControl.Animator.CurrAnim == null || EnemyControl.Animator.CurrAnim.AnimationName != _shootingAnimation.AnimationName)
+            EnemyControl.Animator.ChangeAnim(_shootingAnimation.AnimationName);
+
+        GameManager.gm.DelayAction(_shootingAnimation.AnimDuration, KillBehaviour, null);
+    }
+    void Shoot()
+    {
         Vector2 distance = _player.position - EnemyControl.transform.position;
-        if(distance.magnitude >= _distanceToStopShooting)
-        {
-            KillBehaviour();
-            return;
-        }
         Vector2 shootingOrientation = distance.normalized;
         Vector2 shootingPosition = (Vector2)_enemy.position + _firePointPosition * (shootingOrientation.x > 0 ? 1 : -1);
         Vector2 shootingDirection = ((Vector2)_player.position - shootingPosition).normalized;
 
-        RaycastHit2D hit = Physics2D.Raycast(shootingPosition, shootingDirection);
-        //if (!hit.collider.CompareTag("Player"))//Add layermask
-        //{
-        //    KillBehaviour();
-        //    return;
-        //}
-
-        Shoot(shootingPosition, shootingDirection);
-    }
-    void Shoot(Vector2 shootingPosition, Vector2 shootingDirection)
-    {
-        Debug.Log("Enemy Shooting");
         _shootCooldown = 1/ _atkSpeed;
+        
         float angle = Mathf.Atan2(shootingDirection.y, shootingDirection.x) * Mathf.Rad2Deg;
-        Proyectile proyectile = GameObject.Instantiate(GameManager.gm.prefabHolder.Prefabs["EnemyProyectile"], shootingPosition, Quaternion.Euler(0, 0, angle)).GetComponent<Proyectile>();
-        proyectile.Initiate(_damage,0, _proyectileSpeed, _proyectileLifetime, _proyectileData, _proyectileSpread);
+        EnemyProyectile proyectile = GameObject.Instantiate(GameManager.gm.prefabHolder.Prefabs["EnemyProyectile"], shootingPosition, Quaternion.Euler(0, 0, angle)).GetComponent<EnemyProyectile>();
+        proyectile.Initiate(_damage, _proyectileSpeed, _proyectileLifetime, _proyectileData, _proyectileSpread);
+        
     }
 }
