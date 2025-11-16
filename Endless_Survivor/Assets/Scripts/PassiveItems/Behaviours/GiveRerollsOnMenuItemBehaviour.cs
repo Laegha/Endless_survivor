18b53b7e55eb3cee347d661645d08d276a442592
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-11-16 14:25:25 -0300

    feat(GiveRerollsOnMenuItemBehaviour): Adds up to a given ammount of rerolls each time a given menu is opened (Weapon or Passive)

---
+++ Endless_Survivor/Assets/Scripts/PassiveItems/Behaviours/GiveRerollsOnMenuItemBehaviour.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveRerollsOnMenuItemBehaviour : PassiveItemBehaviour
+{
+    new public static bool isUsable => true;
+
+    [SerializeField] int _usePriority;
+    [SerializeField] int _rerollAmmount;
+    [SerializeField] bool _onWeaponPickup;
+    [SerializeField] bool _onPassivePickup;
+    Reroll _reroll;
+
+    public override void CopyValues(PassiveItemBehaviour original, PassiveItemBehaviourManager behaviourManager)
+    {
+        base.CopyValues(original, behaviourManager);
+        GiveRerollsOnMenuItemBehaviour giveRollsEveryMenuOriginal = original as GiveRerollsOnMenuItemBehaviour;
+        _usePriority = giveRollsEveryMenuOriginal._usePriority;
+        _rerollAmmount = giveRollsEveryMenuOriginal._rerollAmmount;
+        _reroll = new(_usePriority, _rerollAmmount);
+        _onWeaponPickup = giveRollsEveryMenuOriginal._onWeaponPickup;
+        _onPassivePickup = giveRollsEveryMenuOriginal._onPassivePickup;
+        if(_onWeaponPickup)
+            GameUIManager.uiManager.WeaponPickupMenu.OnMenuOpen += AddMissingRolls;
+        if(_onPassivePickup)
+            GameUIManager.uiManager.PassiveItemPickupMenu.OnMenuOpen += AddMissingRolls;
+
+    }
+    public void AddMissingRolls()
+    {
+        int usedRolls = _rerollAmmount - _reroll.rerollsLeft;
+        _reroll.rerollsLeft += usedRolls;
+        if (!RerollManager.rm.ContainsRoll(_reroll))
+        {
+            RerollManager.rm.AddReroll(_reroll);
+        }
+    }
+}
