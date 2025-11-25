1caf671c3fba011332b5e40266129dd477ff8b70
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-11-25 18:06:25 -0300

    feat(PlayerInputReader): Instead of reading the joystick, reads touch controls

--- Endless_Survivor/Assets/Scripts/Player/PlayerInputReader.cs
+++ Endless_Survivor/Assets/Scripts/Player/PlayerInputReader.cs
@@ -2,19 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//public class PlayerInputReader : MonoBehaviour
+//{
+//    [SerializeField] PlayerStateMachine _playerStateMachine;
+//    bl_Joystick joystick;
+
+//    private void Start()
+//    {
+//        joystick = FindObjectOfType<bl_Joystick>();
+//    }
+
+//    void Update()
+//    {
+//        Vector2 movement = new Vector2(joystick.Horizontal, joystick.Vertical);
+//        _playerStateMachine.Movement = movement.normalized;
+//    }
+//}
 public class PlayerInputReader : MonoBehaviour
 {
     [SerializeField] PlayerStateMachine _playerStateMachine;
-    bl_Joystick joystick;
+    TouchControls _controls;
 
     private void Start()
     {
-        joystick = FindObjectOfType<bl_Joystick>();
+        _controls = FindObjectOfType<TouchControls>();
     }
 
     void Update()
     {
-        Vector2 movement = new Vector2(joystick.Horizontal, joystick.Vertical);
+        Vector2 movement = _controls.DraggingDirection;
         _playerStateMachine.Movement = movement.normalized;
     }
 }
