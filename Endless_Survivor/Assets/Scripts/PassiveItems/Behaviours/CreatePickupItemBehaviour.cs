2731039c5cf77573d462fbda994b75976204b4b6
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-11-27 20:35:07 -0300

    feat(CreatPickupItemBehaviour): Passive item that generates pickups in special circumstances, marked with bools

---
+++ Endless_Survivor/Assets/Scripts/PassiveItems/Behaviours/CreatePickupItemBehaviour.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePickupItemBehaviour : PassiveItemBehaviour
+{
+    new public static bool isUsable => true;
+    [Range(0, 100)][SerializeField] float _dropChance;
+    [SerializeField] List<RouletteElementChance<PickupData>> _possiblePickups;
+    [SerializeField] bool _createOnEnemyKill;
+
+    public override void CopyValues(PassiveItemBehaviour original, PassiveItemBehaviourManager behaviourManager)
+    {
+        base.CopyValues(original, behaviourManager);
+        CreatePickupItemBehaviour createPickupOriginal = original as CreatePickupItemBehaviour;
+        _dropChance = createPickupOriginal._dropChance;
+        _possiblePickups = createPickupOriginal._possiblePickups;
+        _createOnEnemyKill = createPickupOriginal._createOnEnemyKill;
+
+        if (_createOnEnemyKill)
+            behaviourManager.onEnemyKilled += CreatePickupOnEnemyKill;
+    }
+
+    void CreatePickupOnEnemyKill(EnemyControl killedEnemy)
+    {
+        float rand = Random.Range(0, 100);
+        if(_dropChance < rand)
+            return;
+        var dropedPickup = Utility.GetRouletteElement(_possiblePickups);
+        Utility.GeneratePickup(dropedPickup, killedEnemy.transform.position);
+        Debug.Log("Gwenerating " + dropedPickup);
+    }
+}
