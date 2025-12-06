d4b8544e300b10f9d6d47b294ecd44b521ad1ef4
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-12-06 16:02:06 -0300

    feat(HealPlayerOnEnemyHitItemBehaviour): Heals the player when a weapon of a given type hits an enemy

---
+++ Endless_Survivor/Assets/Scripts/PassiveItems/Behaviours/HealPlayerOnEnemyHitItemBehaviour.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealPlayerOnEnemyHitItemBehaviour : PassiveItemBehaviour
+{
+    new public static bool isUsable => true;
+    [SerializeField] WeaponAttackControllerTypeHelper.AttackControllerTypes[] _weaponsThatHeal;
+    [SerializeField] int _healAmmount;
+    List<Type> _enlistedTypes = new();
+    public override void CopyValues(PassiveItemBehaviour original, PassiveItemBehaviourManager behaviourManager)
+    {
+        base.CopyValues(original, behaviourManager);
+        var healPlayerOriginal = original as HealPlayerOnEnemyHitItemBehaviour;
+        _healAmmount = healPlayerOriginal._healAmmount;
+        _weaponsThatHeal = healPlayerOriginal._weaponsThatHeal;
+        foreach(var weaponType in _weaponsThatHeal)
+        {
+            _enlistedTypes.Add(WeaponAttackControllerTypeHelper.GetWeaponTypeFromEnum(weaponType));
+        }
+        behaviourManager.onEnemyHit += HealPlayer;
+    }
+
+    void HealPlayer(EnemyControl hitEnemy, Attack recievedAttack)
+    {
+        if (!_enlistedTypes.Contains(recievedAttack.ParentWeapon.GetType()))
+            return;
+        PlayerControl.pc.PlayerHPManager.Heal(_healAmmount);
+    }
+
+}
