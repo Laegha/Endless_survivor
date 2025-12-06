b7508f374cdb67afc97b1ba28eee8e1454d086ac
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-12-06 01:29:26 -0300

    feat(MeleeAttack): Better handling of gfx and destroy attack when animation/weapon animation is finished

--- Endless_Survivor/Assets/Scripts/Attacks/MeleeAttack.cs
+++ Endless_Survivor/Assets/Scripts/Attacks/MeleeAttack.cs
@@ -14,28 +14,42 @@ public class MeleeAttack : Attack
     MeleeData _attackData;
     Vector2 _attackAreaStart;
     Vector2 _attackAreaEnd;
-
     new public AnimationChangeAttackGfxInterface AttackGfxInterface => new AnimationChangeAttackGfxInterface();
     private void Update()
     {
         _vfxRenderer.flipY = ParentWeapon.WeaponControl.Gfx.flipY;
     }
-    public void Attack(int damage, float knockbackForce, MeleeData attackData)
+    public void StartAttack(int damage, float knockbackForce, MeleeData attackData)
     {
         AttackDamage = damage;
         _knockbackForce = knockbackForce;
-        _attackData = attackData;
+        _attackData = new(attackData);
         _attackAreaStart = attackData.IsCircle ? (Vector2)transform.position - Vector2.one.normalized * attackData.CircleRadius : (Vector2)transform.position - attackData.BoxSize/2;
         _attackAreaEnd = attackData.IsCircle ? (Vector2)transform.position + Vector2.one.normalized * attackData.CircleRadius : (Vector2)transform.position + attackData.BoxSize/2;
         
         EffectsHandler.TryEffects(this);
-        var attackAnimation = new CustomAnimation(_vfxAnimator, attackData.AttackVfxAnimation);
-        attackAnimation.Events.Add(new(null, attackData.DamageFrame, ApplyDamage));
-        attackAnimation.Events.Add(new(null, attackAnimation.Frames.Length -1, () => Destroy(gameObject)));
-        _vfxAnimator.AddAnimations(new List<CustomAnimation> { attackAnimation });
-        _vfxAnimator.ChangeAnim(attackAnimation.AnimationName);
-        ParticleConfig particles = new ParticleConfig(attackData.AttackParticles, transform.position, transform.rotation, attackData.ParticleDuration, transform);
-        ParticleManager.pm.SpawnParticles(particles);
+        if(_attackData.AttackVfxAnimation.Frames.Length > 0)
+        {
+            var attackAnimation = new CustomAnimation(_vfxAnimator, _attackData.AttackVfxAnimation);
+            _vfxAnimator.AddAnimations(new List<CustomAnimation> { attackAnimation });
+            _vfxAnimator.ChangeAnim(attackAnimation.AnimationName);
+
+        }
+        if(_attackData.AttackParticles != null)
+        {
+            ParticleConfig particles = new ParticleConfig(_attackData.AttackParticles, transform.position, transform.rotation, _attackData.ParticleDuration, transform);
+            ParticleManager.pm.SpawnParticles(particles);
+
+        }
+
+        var meleeWeapon = ParentWeapon as MeleeWeaponAttackController;
+        meleeWeapon.OnAttackApply += ApplyDamage;
+
+        var weaponAnimDuration = ParentWeapon.WeaponControl.WeaponAnimator.Animations.Find(x => x.AnimationName == ParentWeapon.AnimationName).AnimDuration;
+        var vfxAnimDuration = _attackData.AttackVfxAnimation.Frames.Length > 0 ? _attackData.AttackVfxAnimation.AnimDuration : 0;
+        if(_attackData.DropVfxOnAttack)
+            Destroy(_vfxAnimator.gameObject, Mathf.Max(vfxAnimDuration, weaponAnimDuration));
+        Destroy(gameObject, Mathf.Max(vfxAnimDuration, weaponAnimDuration));
 
     }
     void ApplyDamage()
@@ -51,6 +65,10 @@ public class MeleeAttack : Attack
             Vector2 hitDirection = (enemyCol.transform.position - PlayerControl.pc.transform.position).normalized;
             enemyControl.EnemyHP.TakeDamage(AttackDamage, hitDirection, _knockbackForce);
         }
+        var meleeWeapon = ParentWeapon as MeleeWeaponAttackController;
+        meleeWeapon.OnAttackApply -= ApplyDamage;
+        _vfxAnimator.transform.SetParent(null);
+    }
     public override void ChangeGfx(AttackGfxInterface gfxInterface)
     {
         var animationInterface = gfxInterface as AnimationChangeAttackGfxInterface;
