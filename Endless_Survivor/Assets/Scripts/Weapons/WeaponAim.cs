3325e21fefc6abdf4ac020a4b9d42c0f70f14a54
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-12-06 15:27:30 -0300

    refactor(WeaponAim): Use WeaponTargets to get the rotation of the weapon instead of just targeting enemies

--- Endless_Survivor/Assets/Scripts/Weapons/WeaponAim.cs
+++ Endless_Survivor/Assets/Scripts/Weapons/WeaponAim.cs
@@ -1,6 +1,7 @@
 using System;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 
@@ -11,9 +12,24 @@ public class WeaponAim : MonoBehaviour
     SpriteRenderer _spriteRenderer;
     Transform _directionBase;
     Func<Vector2> _distCheckPosition;
+    List<WeaponTarget> _attackTargets = new();
 
     RaycastHit2D _currTrackingEnemyHit;
     public RaycastHit2D CurrTrackingEnemyHit { get {  return _currTrackingEnemyHit; } }
+    public List<WeaponTarget> AttackTargets {  get { return _attackTargets; } }
+    List<WeaponTarget> TargetedObjs
+    {
+        get
+        {
+            var result = new List<WeaponTarget>(_attackTargets);
+            foreach(var enemy in WaveManager.wm.Enemies)
+            {
+                result.Add(new(enemy, 0));
+            }
+            return result;
+        }
+    }
+
     private void Awake()
     {
         _directionBase = transform;
@@ -28,11 +44,28 @@ public class WeaponAim : MonoBehaviour
 
     void Update()
     {
-        if (WaveManager.wm.Enemies.Count == 0)
+        var attackTargetsCopy = new List<WeaponTarget>(_attackTargets);
+        foreach(var attackTarget in attackTargetsCopy)
+        { 
+            if(attackTarget.obj == null)
+            {
+                _attackTargets.Remove(attackTarget);
+            }
+        }
+
+        if (TargetedObjs.Count == 0)
             return;
 
-        Transform closestEnemy = Utility.GetClosestTo(WaveManager.wm.Enemies, PlayerControl.pc.transform)[0].transform;
-        Vector2 direction = closestEnemy.position - _directionBase.position;
+        var targets = TargetedObjs;
+        targets.Sort(new WeaponTargetComparer(PlayerControl.pc.transform, _weapon.WeaponStats.Range));
+        if (AttackTargets.Count > 0)
+        {
+            print("Targeting " + targets[0].obj.transform.name);
+            print("Should target: " + AttackTargets[0].obj.transform.name + " with priopiryty of " + AttackTargets[0].priority);
+        }
+
+        Transform closestTarget = targets[0].obj.transform;
+        Vector2 direction = closestTarget.position - _directionBase.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, angle);
 
@@ -40,10 +73,11 @@ public class WeaponAim : MonoBehaviour
             _spriteRenderer.flipY = true;
         else
             _spriteRenderer.flipY = false;
-        Vector2 distDirection = (Vector2)closestEnemy.position - _distCheckPosition();
-        var closestEnemyHit = Physics2D.Raycast(_distCheckPosition(), distDirection, Mathf.Infinity, Utility.GetCollidableLayers("PlayerAttack"));
-        _currTrackingEnemyHit = closestEnemyHit;
-        float distance = closestEnemyHit.distance;
+
+        Vector2 distDirection = (Vector2)closestTarget.position - _distCheckPosition();
+        var closestTargetHit = Physics2D.Raycast(_distCheckPosition(), distDirection, Mathf.Infinity, Utility.GetCollidableLayers("PlayerAttack"));
+        _currTrackingEnemyHit = closestTargetHit;
+        float distance = closestTargetHit.distance;
         //print("DISTANCE WITH CLOSEST ENEMY: " + distance + ". CLOSEST ENEMY: " + closestEnemyHit.collider);
         if (distance <= _weapon.WeaponStats.Range)
         {
