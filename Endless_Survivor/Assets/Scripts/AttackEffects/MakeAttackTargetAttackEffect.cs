aab1dee538f8360edc333c30cf984d4699ee4d2f
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-12-06 15:59:14 -0300

    feat(MakeAttackTargetAttackEffect): Adds the attack to the weapon targets

---
+++ Endless_Survivor/Assets/Scripts/AttackEffects/MakeAttackTargetAttackEffect.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeAttackTargetAttackEffect : AttackEffect
+{
+    new public static bool isUsable => true;
+    [SerializeField] int _targetPriority = 1;
+    public MakeAttackTargetAttackEffect(AttackEffect original, Attack affectedAttack) : base(original, affectedAttack) { }
+    public override void Initiate(AttackEffect original, Attack affectedAttack)
+    {
+        base.Initiate(original, affectedAttack);
+        var makeTargetOriginal = original as MakeAttackTargetAttackEffect;
+        _targetPriority = makeTargetOriginal._targetPriority;
+        AffectedAttack.ParentWeapon.WeaponControl.WeaponAim.AttackTargets.Add(new(affectedAttack.gameObject, _targetPriority));
+
+    }
+}
