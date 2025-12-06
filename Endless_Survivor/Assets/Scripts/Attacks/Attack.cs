f7f0f70b7683d23efc0ad6d27d111655274b9536
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-12-06 01:32:40 -0300

    feat(Attack): base for gfx interface and count piercing

--- Endless_Survivor/Assets/Scripts/Attacks/Attack.cs
+++ Endless_Survivor/Assets/Scripts/Attacks/Attack.cs
@@ -8,11 +8,16 @@ public abstract class Attack : MonoBehaviour
     WeaponAttackController _parentWeapon;
     int _attackDamage = 0;
     float _attackDamageMultiplier = 1;
+    int _attackPiercing = 0;
     bool _isSecondaryAttack;
+    public AttackGfxInterface AttackGfxInterface => new AttackGfxInterface();
     public abstract AttackEffectArea AttackEffectArea { get; }
     public AttackEffectsHandler EffectsHandler { get { return _effectsHandler; } }
     public int AttackDamage { get { return (int)(_attackDamage * _attackDamageMultiplier); } set { _attackDamage = value; } }
     public float AttackDamageMultiplier { get { return _attackDamageMultiplier; } set { _attackDamageMultiplier = value; } }
     public WeaponAttackController ParentWeapon { get { return _parentWeapon; } set { _parentWeapon = value; } }
+    public int AttackPiercing { get { return _attackPiercing; } set { _attackPiercing = value; } }
     public bool IsSecondaryAttack { get { return _isSecondaryAttack; } set { _isSecondaryAttack = value; } }
+
+    public abstract void ChangeGfx(AttackGfxInterface gfxInterface);
 }
