c99fc47fb9e940eee77fbe3535254de09299d2e7
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-12-10 07:26:15 -0300

    feat(ThrowAtEnemySupportObjBehaviour): Calculating distance and direction and handling movement
    Should be done with a TransformMover

--- Endless_Survivor/Assets/Scripts/SupportObjects/Behaviours/ThrowAtEnemySupportObjBehaviour.cs
+++ Endless_Survivor/Assets/Scripts/SupportObjects/Behaviours/ThrowAtEnemySupportObjBehaviour.cs
@@ -7,10 +7,46 @@ using UnityEngine;
 public class ThrowAtEnemySupportObjBehaviour : SupportObjectBehaviour
 {
     new public static int maxStacks => 1;
+    [SerializeField] CustomAnimation _thrownAnimation;
+    [SerializeField] float _throwSpeed = 10;
+    [SerializeField] float _collisionCheckRadius = .2f;
+    [SerializeField] LayerMask _collidedLayers;
+    //[SerializeField CustomAnimation _windupAnimation;
+
+    Vector2 _throwDirection;
+    float _totalDistance;
+    float _lapsedDistance;
 
     public override void Initiate(SupportObjectControl control, SupportObjectBehaviour original)
     {
         base.Initiate(control, original);
         var throwOriginal = original as ThrowAtEnemySupportObjBehaviour;
+        _throwSpeed = throwOriginal._throwSpeed;
+        _collisionCheckRadius = throwOriginal._collisionCheckRadius;
+        _collidedLayers = throwOriginal._collidedLayers;
+        OnStart += CalculateDirection;
+        OnUpdate += MoveTowardsEnemy;
+    }
+    void CalculateDirection()
+    {
+        var closestEnemy = Utility.GetClosestTo(WaveManager.wm.Enemies, PlayerControl.pc.transform)[0];
+        var throwVector = closestEnemy.transform.position - ObjControl.transform.position;
+        _throwDirection = throwVector.normalized;
+        _totalDistance = throwVector.magnitude;
+    }
+    void MoveTowardsEnemy()
+    {
+        Vector2 movementDelta = _throwDirection * Time.deltaTime * _throwSpeed;
+        ObjControl.transform.Translate(movementDelta);
+        _lapsedDistance += movementDelta.magnitude;
+        var collidingObjs = Physics2D.OverlapCircleAll(ObjControl.transform.position, _collisionCheckRadius, _collidedLayers);
+        if(collidingObjs.Length > 0)
+        {
+            GameObject.Destroy(ObjControl.gameObject);
+            return;
+        }
+        if(_lapsedDistance >= _totalDistance)
+            return;
+        GameObject.Destroy(ObjControl.gameObject);
     }
 }
