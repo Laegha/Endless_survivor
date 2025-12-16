f0e23fb4e628f9eecdfeadf31684a046fd034226
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-12-16 02:44:48 -0300

    feat(WeaponizeSupportObjBehaviour): Creates an invisible hand with a weapon in the place of the support obj and changes the animator to the one of the weapon

---
+++ Endless_Survivor/Assets/Scripts/SupportObjects/Behaviours/WeaponizeSupportObjBehaviour.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class WeaponizeSupportObjBehaviour : SupportObjectBehaviour
+{
+    [SerializeField] WeaponData _objWeapon;
+    WeaponAttackManager _weaponAttackManager;
+    new public static int maxStacks => 1;
+    public override void Initiate(SupportObjectControl control, SupportObjectBehaviour original)
+    {
+        base.Initiate(control, original);
+        var weaponizeOriginal = original as WeaponizeSupportObjBehaviour;
+        _objWeapon = weaponizeOriginal._objWeapon;
+
+        OnStart += CreateWeapon;
+        WaveManager.wm.OnWaveStarted += UpdateWeaponStats;
+        
+    }
+    void CreateWeapon()
+    {
+        GameObject theoricHand = new("TheoricHand");
+        theoricHand.transform.SetParent(ObjControl.transform, false);
+        //theoricHand.transform.position = Vector2.zero;
+
+        var weaponObj = GameObject.Instantiate(GameManager.gm.prefabHolder.Prefabs["Weapon"], theoricHand.transform);
+        weaponObj.transform.position = Vector2.zero;
+        weaponObj.transform.rotation = Quaternion.identity;
+
+        var weaponStats = new WeaponStats(_objWeapon.WeaponStats);
+        weaponStats.SetTrueLevelStats(_objWeapon.StatsIncreaseScale, ScalingFunctions.CurrWaveLevel);
+
+        var weaponControl = weaponObj.GetComponent<WeaponControl>();
+        _weaponAttackManager = weaponControl.WeaponAttackManager;
+        _weaponAttackManager.Initiate(_objWeapon.AttackConditions, weaponStats, _objWeapon);
+
+        foreach (var renderer in ObjControl.Renderers)
+        {
+            renderer.enabled = false;
+        }
+        var objAnimations = ObjControl.Animator.Animations;
+        ObjControl.Animator = weaponControl.WeaponAnimator;
+        ObjControl.Animator.AddAnimations(objAnimations);
+    }
+    void UpdateWeaponStats()
+    {
+        var weaponStats = new WeaponStats(_objWeapon.WeaponStats);
+        weaponStats.SetTrueLevelStats(_objWeapon.StatsIncreaseScale, ScalingFunctions.CurrWaveLevel);
+        _weaponAttackManager.WeaponStats = weaponStats;
+    }
+}
