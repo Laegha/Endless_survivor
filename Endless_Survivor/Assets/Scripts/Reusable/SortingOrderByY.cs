161420e475948fb237e92e0e2f034acf53e7e5d2
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-12-17 03:24:40 -0300

    feat(SortingOrderByY): Added getter to affected renderers

--- Endless_Survivor/Assets/Scripts/Reusable/SortingOrderByY.cs
+++ Endless_Survivor/Assets/Scripts/Reusable/SortingOrderByY.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class SortingOrderByY<T> : MonoBehaviour where T : Component
 {
-    [SerializeField] T[] sortedElements;
+    [SerializeField] T[] _sortedElements;
     [SerializeField] int _offset;
     static SortingOrderByY<T> _highestSorter;
     public static int highestOrder;
     Dictionary<T, int> _elementsOrders = new();
-    public T[] AffectedRenderers {  set { sortedElements = value; } }
+    public T[] AffectedRenderers { get { return _sortedElements; } set { _sortedElements = value; } }
+    public int Offset { get { return _offset; } set { _offset = value; } }
     public Dictionary<T, int> ElementsOrders { get { return _elementsOrders; } }
     private void Start()
     {
-        foreach(var element in sortedElements) 
+        foreach(var element in _sortedElements) 
             _elementsOrders.Add(element, 0);
     }
 
@@ -21,7 +22,7 @@ public class SortingOrderByY<T> : MonoBehaviour where T : Component
     {
         int thisHighestOrder = 0;
         bool sortingOrderDefined = false;
-        foreach (var sortedElement in sortedElements)
+        foreach (var sortedElement in _sortedElements)
         {
             int elementOrder = (int)(-sortedElement.transform.position.y * 100) + _offset;
             _elementsOrders[sortedElement] = elementOrder;
