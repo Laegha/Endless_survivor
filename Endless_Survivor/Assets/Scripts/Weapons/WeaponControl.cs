14e97bcc8aee72eb785d091d7c503f5d4c9d5d4b
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-12-17 03:24:07 -0300

    feat(WeaponControl): Reference to RendererSortingByY

--- Endless_Survivor/Assets/Scripts/Weapons/WeaponControl.cs
+++ Endless_Survivor/Assets/Scripts/Weapons/WeaponControl.cs
@@ -8,10 +8,12 @@ public class WeaponControl : MonoBehaviour
     [SerializeField] WeaponAttackManager _weaponAttackManager;
     [SerializeField] CustomAnimator _weaponAnimator;
     [SerializeField] SpriteRenderer _gfx;
+    [SerializeField] RendererSortingByY _rendererSorter;
     [SerializeField] ParticleSystem _onLevelUpParticle;
     public WeaponAim WeaponAim { get { return _weaponAim; } }
     public WeaponAttackManager WeaponAttackManager { get { return _weaponAttackManager; } }
     public CustomAnimator WeaponAnimator { get { return _weaponAnimator; } }
     public SpriteRenderer Gfx { get { return _gfx; } }
+    public RendererSortingByY GfxSorter { get { return _rendererSorter; } }
     public ParticleSystem OnLevelUpParticle { get { return _onLevelUpParticle; } }
 }
