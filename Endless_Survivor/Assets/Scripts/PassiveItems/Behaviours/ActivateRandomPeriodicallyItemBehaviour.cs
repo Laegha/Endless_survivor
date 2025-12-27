159bb3d38f704d8625024d2af2c068ae5ec60c50
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-12-27 19:45:19 -0300

    feat(ActivateRandomPeriodicallyItemBehaviour): Activates a random behaviour every given ammount of time

---
+++ Endless_Survivor/Assets/Scripts/PassiveItems/Behaviours/ActivateRandomPeriodicallyItemBehaviour.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ActivateRandomPeriodicallyItemBehaviour : PassiveItemBehaviour
+{
+    new public static bool isUsable => true;
+    [SerializeField] List<RouletteElementChance<string>> _possibleActivatedIds;
+    [SerializeField] RandomBetweenTwoConstants _timeBetweenActivations;
+    float _activationTimer;
+
+    public override void CopyValues(PassiveItemBehaviour original, PassiveItemBehaviourManager behaviourManager)
+    {
+        base.CopyValues(original, behaviourManager);
+        var activateRandOriginal = original as ActivateRandomPeriodicallyItemBehaviour;
+        _possibleActivatedIds = new(activateRandOriginal._possibleActivatedIds);
+        ResetTimer();
+        behaviourManager.onUpdate += ReduceActivationTimer;
+    }
+    void ReduceActivationTimer()
+    {
+        _activationTimer -= Time.deltaTime;
+        if(_activationTimer < 0)
+        {
+            ResetTimer();
+            ActivateRandomBehaviour();
+        }
+    }
+    void ResetTimer()
+    {
+        _activationTimer = _timeBetweenActivations.rand;
+    }
+    void ActivateRandomBehaviour()
+    {
+        string activatedId = Utility.GetRouletteElement(_possibleActivatedIds);
+        var activatedBehaviour = BehaviourManager.ItemBehaviours.Find(x => x.BehaviourId == activatedId);
+        activatedBehaviour.Activate();
+    }
+}
