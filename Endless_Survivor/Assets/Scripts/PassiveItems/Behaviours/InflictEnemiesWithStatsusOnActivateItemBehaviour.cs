13ae4f6699fa5823bb50e1edd8d50690bcf2220f
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-12-27 19:37:42 -0300

    feat(InflictEnemiesWithStatsusOnActivateItemBehaviour): Inflicts every enemy with a random effect from a list

---
+++ Endless_Survivor/Assets/Scripts/PassiveItems/Behaviours/InflictEnemiesWithStatsusOnActivateItemBehaviour.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InflictEnemiesWithStatsusOnActivateItemBehaviour : PassiveItemBehaviour
+{
+    [SerializeField] List<RouletteElementChance<EnemyStatusEffectData>> _inflictedStatusEffects;
+    [SerializeField] RandomBetweenTwoConstants _effectsPerEnemy;
+    public override void CopyValues(PassiveItemBehaviour original, PassiveItemBehaviourManager behaviourManager)
+    {
+        base.CopyValues(original, behaviourManager);
+        var inflictEffectsOriginal = original as InflictEnemiesWithStatsusOnActivateItemBehaviour;
+        _inflictedStatusEffects = new List<RouletteElementChance<EnemyStatusEffectData>>(inflictEffectsOriginal._inflictedStatusEffects);
+        _effectsPerEnemy = inflictEffectsOriginal._effectsPerEnemy;
+    }
+    public override void Activate()
+    {
+        base.Activate();
+        var enemies = WaveManager.wm.Enemies;
+
+        foreach (var enemy in enemies)
+        {
+            int effectsAmmount = (int)_effectsPerEnemy.rand;
+            for(int i = 0; i < effectsAmmount; i++)
+            {
+                var addedEffectData = Utility.GetRouletteElement(_inflictedStatusEffects);
+                enemy.GetComponent<EnemyControl>().StatusEffectManager.AddEffects(addedEffectData.StatusEffects, addedEffectData);
+
+            }
+        }
+    }
+}
