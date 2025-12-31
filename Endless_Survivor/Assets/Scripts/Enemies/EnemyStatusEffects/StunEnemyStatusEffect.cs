431817b04e077a021782ebec41a5b27208782af3
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2025-12-31 01:32:44 -0300

    feat(StunEnemyStatusEffect): Turns the stun variable to true and stops the rigidbody on Start. on End, if there are no more stuns on that enemy, does the oposite

---
+++ Endless_Survivor/Assets/Scripts/Enemies/EnemyStatusEffects/StunEnemyStatusEffect.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class StunEnemyStatusEffect : EndByTimeStatusEffect
+{
+    new public static bool isUsable => true;
+    static Dictionary<EnemyControl, int> _stunAmmountPerEnemy = new();
+    public override void Start()
+    {
+        base.Start();
+        if(_stunAmmountPerEnemy.ContainsKey(AffectedEnemyControl))
+            _stunAmmountPerEnemy[AffectedEnemyControl]++;
+        else
+            _stunAmmountPerEnemy.Add(AffectedEnemyControl, 1);
+        AffectedEnemyControl.RbForcesController.ChangeCurrForce(new(new(0, 0), 0, 10000, ForceMode2D.Impulse, 0));
+        AffectedEnemyControl.BehaviourManager.IsStunned = true;
+    }
+    public override void End()
+    {
+        base.End();
+        _stunAmmountPerEnemy[AffectedEnemyControl]--;
+        if (_stunAmmountPerEnemy[AffectedEnemyControl] > 0)
+            return;
+        _stunAmmountPerEnemy.Remove(AffectedEnemyControl);
+        AffectedEnemyControl.BehaviourManager.IsStunned = false;
+    }
+}
