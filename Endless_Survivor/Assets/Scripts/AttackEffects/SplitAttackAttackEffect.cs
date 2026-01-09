6ce0b4779e04373e9e90694223f7ac51eb7356e0
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-01-09 15:01:34 -0300

    feat(SplitAttackAttackEffect): Attack effect that makes the attack split on attack

---
+++ Endless_Survivor/Assets/Scripts/AttackEffects/SplitAttackAttackEffect.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplitAttackAttackEffect : AttackEffect
+{
+    new public static bool isUsable => true;
+    [SerializeField] int _splitAmmount = 2;
+    [SerializeField] float _distBetweenSplits;
+    public SplitAttackAttackEffect(AttackEffect original, Attack affectedAttack) : base(original, affectedAttack) { }
+    public override void Initiate(AttackEffect original, Attack affectedAttack)
+    {
+        base.Initiate(original, affectedAttack);
+        var splitOriginal = original as SplitAttackAttackEffect;
+        _splitAmmount = splitOriginal._splitAmmount;
+        _distBetweenSplits = splitOriginal._distBetweenSplits;
+
+        OnAttack += SplitAttack;
+    }
+    void SplitAttack()
+    {
+        List<GameObject> enemies = WaveManager.wm.Enemies;
+        var closestEnemies = Utility.GetClosestTo(enemies, AffectedAttack.transform);
+        Vector2 splitAttackPos = AffectedAttack.transform.position;
+        for (int i = 0; i < _splitAmmount; i++)
+        {
+            int targetEnemyIndex = i >= closestEnemies.Count ? Random.Range(0, closestEnemies.Count) : i;
+            GameObject splitTargetedEnemy = Utility.GetClosestTo(enemies, AffectedAttack.transform)[targetEnemyIndex];
+            Vector2 splitAttackDir = (splitTargetedEnemy.transform.position - AffectedAttack.transform.position).normalized;
+            AffectedAttack.ParentWeapon.Attack(splitAttackPos, splitAttackDir, true);
+
+            splitAttackPos = (Vector2)AffectedAttack.transform.position + (Vector2)AffectedAttack.transform.up * _distBetweenSplits * (i + 1) * Mathf.Pow(-1, i + 1);
+        }
+        GameObject.Destroy(AffectedAttack.gameObject);
+    }
+}
