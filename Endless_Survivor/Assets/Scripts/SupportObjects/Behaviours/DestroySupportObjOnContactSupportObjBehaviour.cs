bda01bd290758dc44d659d910adbfe875507a322
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-01-09 15:04:04 -0300

    feat(DestroySupportObjOnContactSupportObjBehaviour): Uses an area to detect collision

---
+++ Endless_Survivor/Assets/Scripts/SupportObjects/Behaviours/DestroySupportObjOnContactSupportObjBehaviour.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySupportObjOnContactSupportObjBehaviour : UseAreaAroundSupportObjBehaviour
+{
+    new public static int maxStacks => 1;
+
+    public override void Initiate(SupportObjectControl control, SupportObjectBehaviour original)
+    {
+        base.Initiate(control, original);
+        OnObjEnterArea += (GameObject collided) => GameObject.Destroy(ObjControl.gameObject);
+
+    }
+}
