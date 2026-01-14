69e18b9bd4fc33d3694455085c4ddc8878a0ea47
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-01-14 16:49:38 -0300

    feat(Biome): GetSide function that takes a direction and returns a new BiomeSide with the matching information.  Group biome border tiles separately

--- Endless_Survivor/Assets/Scripts/MapManaging/Biome.cs
+++ Endless_Survivor/Assets/Scripts/MapManaging/Biome.cs
@@ -1,12 +1,12 @@
-using System.Collections;
+using Microsoft.Win32;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class Biome
 {
     List<Tile> _biomeTiles = new();
+    List<Tile> _borderTiles = new();
     BiomeData _biomeData;
 
     Wave _lastWave;
@@ -16,15 +16,31 @@ public class Biome
 
     public void SetDirty() => _biomeTiles.RemoveAll(x => x == null);
 
-    //public List<Tile> Side()
-    //{
-    //    var bottomSide = Walls.Where(x => x.transform.position.x == _biomeTiles[0].transform.position.x).ToList();//bottom side
-    //    var leftSide = Walls.Where(x => x.transform.position.y == _biomeTiles[0].transform.position.y).ToList();//left side
-    //    var topSide = Walls.Where(x => x.transform.position.x == leftSide.Last().transform.position.x).ToList();
-    //    var rightSide = Walls.Where(x => x.transform.position.y == bottomSide.Last().transform.position.y).ToList();
+    //Sides ONLY WORK if _biomeTiles[0] is the bottom left WHICH SHOULD ALWAYS BE THE CASE
+    public BiomeSide GetSide(Vector2 direction)
+    {
+        //List<Tile> sideTiles = _borderTiles.Where(tile => tile.TileDir.x == direction.x || tile.TileDir.y == direction.y).ToList();
+        List<Tile> sideTiles = new();
+        if(direction ==  Vector2.up)
+            sideTiles = _borderTiles.Where(x => x.transform.position.y == _borderTiles.Last().transform.position.y).ToList();
+        if(direction ==  Vector2.right)
+            sideTiles = _borderTiles.Where(x => x.transform.position.x == _borderTiles.Last().transform.position.x).ToList();
+        if(direction ==  Vector2.down)
+            sideTiles = _borderTiles.Where(x => x.transform.position.y == _borderTiles.First().transform.position.y).ToList();
+        if(direction ==  Vector2.left)
+            sideTiles = _borderTiles.Where(x => x.transform.position.x == _borderTiles.First().transform.position.x).ToList();
 
-    //}
-
+        if(sideTiles.Count == 0)
+            return null;
+        Vector2 absDir = new(Mathf.Abs(direction.x), Mathf.Abs(direction.y));
+        Vector2 startPosOffset = new(sideTiles[0].transform.position.x * absDir.x, sideTiles[0].transform.position.y * absDir.y);
+        Vector2 startPosBase = (Vector2)_biomeTiles[0].transform.position - new Vector2(_biomeTiles[0].transform.position.x * absDir.x, _biomeTiles[0].transform.position.y * absDir.y);
+        
+        //this should cancel out the unnecessary offset, and only consider what's relevant for the direction (vertical if up/down, horizontal if right/left)
+        Vector2 startPos = sideTiles[0].transform.position;
+        BiomeSide side = new(sideTiles, direction, startPos);
+        return side;
+    }
     public Biome(BiomeData biomeData)
     {
         _biomeData = biomeData;
@@ -41,12 +57,17 @@ public class Biome
                 //if(intersectionTiles.Contains(tile => tile.transform.position == new Vector2(x, y))
                 //generate intersection tile with oposite direction to said tile
                 Tile generatedTile = GameObject.Instantiate(GameManager.gm.prefabHolder.Prefabs["Tile"].GetComponent<Tile>());
+
                 generatedTile.transform.position = startingPoint + new Vector2(x, y);
                 generatedTile.TileBiome = this;
-                _biomeTiles.Add(generatedTile);
+
+                if(y == 0 || x == 0 || y == generationInfo.biomeSize.y || x == generationInfo.biomeSize.x)
+                    _borderTiles.Add(generatedTile);
+
                 generatedTiles.Add(generatedTile);
             }
         }
+        _biomeTiles.AddRange(generatedTiles);
         return generatedTiles;
     }
 
