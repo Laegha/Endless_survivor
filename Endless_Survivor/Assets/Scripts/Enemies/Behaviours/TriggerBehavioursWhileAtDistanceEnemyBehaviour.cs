6dd722cd4d3a09eeb1d645aa3b2047dacc8fbf6e
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-01-18 21:16:18 -0300

    feat(TriggerBehavioursWhileAtDistanceEnemyBehaviour): Activates behaviours when the enemy is at a certain distance to the player and kills them when that condition is no longer true

--- Endless_Survivor/Assets/Scripts/Enemies/Behaviours/TriggerBehavioursWhileAtDistanceEnemyBehaviour.cs
+++ Endless_Survivor/Assets/Scripts/Enemies/Behaviours/TriggerBehavioursWhileAtDistanceEnemyBehaviour.cs
@@ -2,42 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerBehavioursAtDistance : EnemyBehaviour
+public class TriggerBehavioursWhileAtDistanceEnemyBehaviour : EnemyBehaviour
 {
     new public static int maxStacks => -1;
     [SerializeReference] List<string> _behavioursToTrigger = new List<string>();
     [SerializeField] float _distanceToTrigger;
     Transform _player;
+    bool _isActivated = false;
 
     public override void Initialize(EnemyBehaviour original, EnemyControl enemyControl)
     {
         base.Initialize(original, enemyControl);
-        TriggerBehavioursAtDistance originalTriggerBehavioursAtDistance = original as TriggerBehavioursAtDistance;
-        _behavioursToTrigger = originalTriggerBehavioursAtDistance._behavioursToTrigger;
-        _distanceToTrigger = originalTriggerBehavioursAtDistance._distanceToTrigger;
-        _player = GameObject.FindObjectOfType<PlayerControl>().transform;
+        var originalTriggerBehavioursWhileAtDistance = original as TriggerBehavioursWhileAtDistanceEnemyBehaviour;
+        _behavioursToTrigger = originalTriggerBehavioursWhileAtDistance._behavioursToTrigger;
+        _distanceToTrigger = originalTriggerBehavioursWhileAtDistance._distanceToTrigger;
+        _player = PlayerControl.pc.transform;
     }
     public override void PassiveUpdate()
     {
         base.PassiveUpdate();
-        if(IsActive)
+        if (IsActive)
             return;
         float distance = Vector2.Distance(EnemyControl.transform.position, _player.position);
-        if(distance <= _distanceToTrigger)
+        if (distance <= _distanceToTrigger)
         {
             foreach (var behaviour in _behavioursToTrigger)
                 EnemyControl.BehaviourManager.ActivateBehaviour(behaviour);
             EnemyControl.BehaviourManager.ActivateBehaviour(this);
+            _isActivated = true;
         }
     }
     public override void ActiveUpdate()
     {
         base.ActiveUpdate();
+        float distance = Vector2.Distance(EnemyControl.transform.position, _player.position);
+        if (distance <= _distanceToTrigger || !_isActivated)
+            return;
+        _isActivated = false;
         foreach (var behaviour in _behavioursToTrigger)
-        {
-            if (EnemyControl.BehaviourManager.GetBehaviour(behaviour).IsActive)
-                return;
-        }
+            EnemyControl.BehaviourManager.GetBehaviour(behaviour).KillBehaviour();
         KillBehaviour();
     }
 }
