1e4a3a28f484f31dac3d342b54dfa93cf088e44e
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-01-20 17:44:20 -0300

    refactor(CreateSupportObjAfterAnimWeaponInteraction): Now creates the object after an animation, played with an animated object

--- Endless_Survivor/Assets/Scripts/WeaponInteractions/Interactions/CreateSupportObjAfterAnimWeaponInteraction.cs
+++ Endless_Survivor/Assets/Scripts/WeaponInteractions/Interactions/CreateSupportObjAfterAnimWeaponInteraction.cs
@@ -4,9 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
-public class CreateSupportObjWeaponInteraction : WeaponInteraction
+public class CreateSupportObjAfterAnimWeaponInteraction : WeaponInteraction
 {
     new public static int maxStacks => -1;
+    [SerializeField] CustomAnimation _animBeforeSpawning;
     [SerializeField] SupportObjectData _createdSupportObj;
     [SerializeField] Vector2 _offsetFromPlayer;
     [SerializeField] float _rotation;
@@ -14,7 +15,8 @@ public class CreateSupportObjWeaponInteraction : WeaponInteraction
     public override void Initialize(WeaponInteraction original, WeaponInteractionData interactionData, List<WeaponAttackManager> affectedWeapons)
     {
         base.Initialize(original, interactionData, affectedWeapons);
-        var createSupportObjOriginal = original as CreateSupportObjWeaponInteraction;
+        var createSupportObjOriginal = original as CreateSupportObjAfterAnimWeaponInteraction;
+        _animBeforeSpawning = createSupportObjOriginal._animBeforeSpawning;
         _createdSupportObj = createSupportObjOriginal._createdSupportObj;
         _offsetFromPlayer = createSupportObjOriginal._offsetFromPlayer;
         _rotation = createSupportObjOriginal._rotation;
@@ -23,6 +25,19 @@ public class CreateSupportObjWeaponInteraction : WeaponInteraction
     public override void InteractionStart()
     {
         base.InteractionStart();
+        CustomAnimator animatorObj = GameObject.Instantiate(GameManager.gm.prefabHolder.Prefabs["AnimatedObject"], PlayerControl.pc.transform.position, Quaternion.identity).GetComponent<CustomAnimator>();
+
+        CustomAnimation animationCopy = new(animatorObj, _animBeforeSpawning);
+        animationCopy.Events.Add(new(null, _animBeforeSpawning.Frames.Length - 1, CreateSupportObj));
+        animationCopy.Events.Add(new(null, _animBeforeSpawning.Frames.Length - 1, () => GameObject.Destroy(animatorObj.gameObject)));
+
+        animatorObj.AddAnimations(new (){ animationCopy });
+        animatorObj.ChangeAnim(animationCopy.AnimationName);
+
+    }
+
+    void CreateSupportObj()
+    {
         //cast a raycast from the player to the suposed spawn position, colliding only with walls. if the raycast hits a wall, create the support obj in the hit point instead
         Vector2 desiredSpawnPos = (Vector2)PlayerControl.pc.transform.position + _offsetFromPlayer;
         Vector2 spawnCheckVector = desiredSpawnPos - (Vector2)PlayerControl.pc.transform.position;
@@ -33,6 +48,6 @@ public class CreateSupportObjWeaponInteraction : WeaponInteraction
 
         Vector2 actualSpawnPos = spawnObstacle ? spawnObstacle.point : desiredSpawnPos;
 
-        Utility.GenerateSupportObj(_createdSupportObj, actualSpawnPos, Quaternion.Euler(0,0, _rotation));
+        Utility.GenerateSupportObj(_createdSupportObj, actualSpawnPos, Quaternion.Euler(0, 0, _rotation));
     }
 }
