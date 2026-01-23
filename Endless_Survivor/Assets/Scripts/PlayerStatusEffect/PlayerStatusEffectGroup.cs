6433a74613d93dd09fbb6f234444a1c17d283bf5
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-01-23 01:26:28 -0300

    fix(PlayerStatusEffectGroup): No longer deriving from monobehaviour

--- Endless_Survivor/Assets/Scripts/PlayerStatusEffect/PlayerStatusEffectGroup.cs
+++ Endless_Survivor/Assets/Scripts/PlayerStatusEffect/PlayerStatusEffectGroup.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStatusEffectGroup : MonoBehaviour
+public class PlayerStatusEffectGroup
 {
     public PlayerStatusEffectData effectData;
     List<PlayerStatusEffect> _groupEffects;
