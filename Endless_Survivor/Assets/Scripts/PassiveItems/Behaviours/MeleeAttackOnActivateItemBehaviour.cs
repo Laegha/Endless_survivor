7a0418635a4fbed192caf70cd14e16fb9304cc3d
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-01-29 03:21:26 -0300

    feat(MeleeAttackOnActivateItemBehaviour): Passive Item behaviour that spawns a melee attack on the player's position when activated

---
+++ Endless_Survivor/Assets/Scripts/PassiveItems/Behaviours/MeleeAttackOnActivateItemBehaviour.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttackOnActivateItemBehaviour : PassiveItemBehaviour
+{
+    new public static int maxStacks => -1;
+    [SerializeField] MeleeData _meleeData;
+    [SerializeField] DamageInfo _damage;
+    [SerializeField] float _knockBack;
+    public override void CopyValues(PassiveItemBehaviour original, PassiveItemBehaviourManager behaviourManager)
+    {
+        base.CopyValues(original, behaviourManager);
+        var meleeAttackOriginal = original as MeleeAttackOnActivateItemBehaviour;
+        _meleeData = meleeAttackOriginal._meleeData;
+        _damage = meleeAttackOriginal._damage;
+        _knockBack = meleeAttackOriginal._knockBack;
+    }
+
+    public override void Activate()
+    {
+        base.Activate();
+        
+        MeleeAttack meleeAttack = GameObject.Instantiate(GameManager.gm.prefabHolder.Prefabs["Melee"], PlayerControl.pc.transform.position, Quaternion.identity).GetComponent<MeleeAttack>();
+        meleeAttack.TriggersPassiveItemHit = false;
+        meleeAttack.transform.SetParent(PlayerControl.pc.transform, true);
+        meleeAttack.StartAttack((int)_damage.CalculatedDamage, _knockBack, _meleeData);
+        meleeAttack.ApplyDamage();
+    }
+}
