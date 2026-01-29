a22180b4de80fcf33454d526fbeedbcb2334511c
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-01-29 03:21:53 -0300

    feat(ActivateAfterHittingEnoughEnemiesItemBehaviour): on enemy hit increases a counter, when it reaches a certain number triggers the given effects

---
+++ Endless_Survivor/Assets/Scripts/PassiveItems/Behaviours/ActivateAfterHittingEnoughEnemiesItemBehaviour.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateAfterHittingEnoughEnemiesItemBehaviour : PassiveItemBehaviour
+{
+    new public static int maxStacks => 1;
+    [SerializeField] RandomBetweenTwoConstants _hitsNeeded;
+    [SerializeField] string[] _activatedBehaviours;
+
+    int _currGoal = 0;
+    int _hitEnemies = 0;
+    public override void CopyValues(PassiveItemBehaviour original, PassiveItemBehaviourManager behaviourManager)
+    {
+        base.CopyValues(original, behaviourManager);
+        var activateAfterHttingOriginal = original as ActivateAfterHittingEnoughEnemiesItemBehaviour;
+        _hitsNeeded = activateAfterHttingOriginal._hitsNeeded;
+        _activatedBehaviours = activateAfterHttingOriginal._activatedBehaviours;
+        _currGoal = (int)_hitsNeeded.rand;
+        behaviourManager.onEnemyHit += IncreaseCounter;
+    }
+    void IncreaseCounter(EnemyControl hitEnemy, Attack usedAttack)
+    {
+        _hitEnemies++;
+        Debug.Log("CURR HIT ENEMIES " + _hitEnemies);
+        if (_hitEnemies < _currGoal)
+            return;
+
+        _currGoal = (int)_hitsNeeded.rand;
+        foreach (string id in _activatedBehaviours)
+        {
+            var activatedBehaviour = BehaviourManager.ItemBehaviours.Find(x => x.BehaviourId == id);
+            activatedBehaviour.Activate();
+        }
+
+    }
+}
