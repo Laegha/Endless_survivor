d401ea4b619b13e7f4d5b1ef8c53b471f1eb8364
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-01-30 16:50:29 -0300

    feat(MeleeAttackBehaviour): Added animations for each direction

--- Endless_Survivor/Assets/Scripts/Enemies/Behaviours/MeleeAttackBehaviour.cs
+++ Endless_Survivor/Assets/Scripts/Enemies/Behaviours/MeleeAttackBehaviour.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -16,13 +17,21 @@ public class MeleeAttackBehaviour : EnemyBehaviour
     [SerializeField] float _attackRange = 1;
     [SerializeField] float _attackRadius = 1;
 
-    [SerializeField] CustomAnimation _enemyAttackAnimation;
+    [SerializeField] CustomAnimation _attackUpAnimation;
+    [SerializeField] CustomAnimation _attackRightAnimation;
+    [SerializeField] CustomAnimation _attackDownAnimation;
+    [SerializeField] CustomAnimation _attackLeftAnimation;
+
     [SerializeField] bool _usesVfx;
     [SerializeField] CustomAnimation _attackVFXAnimation;
     [SerializeField] int _triggerVFXFrame;
-    [SerializeField] int _triggerDamageFrame;
+    [SerializeField] int _vfxRendererOffset;
+    [Tooltip("If there are multiple animations, make sure that this frame isn't out of range for any of them")][SerializeField] int _triggerDamageFrame;
     [SerializeField] SFXInfo _attackSfx;
     CustomAnimator _vfxAnimator;
+
+
+    List<CustomAnimation> _attackAnimations = new();
     public override void Initialize(EnemyBehaviour original, EnemyControl enemyControl)
     {
         base.Initialize(original, enemyControl);
@@ -34,24 +43,63 @@ public class MeleeAttackBehaviour : EnemyBehaviour
         _attackRange = originalMeleeAttack._attackRange;
         _attackRadius = originalMeleeAttack._attackRadius;
         _usesVfx = originalMeleeAttack._usesVfx;
-        
+
         //copy animations from SO
-        _enemyAttackAnimation = new CustomAnimation(EnemyControl.Animator, originalMeleeAttack._enemyAttackAnimation);
-        _attackVFXAnimation = new CustomAnimation(_vfxAnimator, originalMeleeAttack._attackVFXAnimation);
+        if (originalMeleeAttack._attackUpAnimation.Frames.Length > 0)
+        {
+            _attackUpAnimation = new CustomAnimation(EnemyControl.Animator, originalMeleeAttack._attackUpAnimation);
+
+            _attackUpAnimation.Events.Add(new(null, _attackUpAnimation.Frames.Length - 1, AnimationEnd));
+            _attackAnimations.Add(_attackUpAnimation);
+        }
+        if (originalMeleeAttack._attackRightAnimation.Frames.Length > 0)
+        {
+            _attackRightAnimation = new CustomAnimation(EnemyControl.Animator, originalMeleeAttack._attackRightAnimation);
+
+            _attackRightAnimation.Events.Add(new(null, _attackRightAnimation.Frames.Length - 1, AnimationEnd));
+            _attackAnimations.Add(_attackRightAnimation);
+
+        }
+        if (originalMeleeAttack._attackDownAnimation.Frames.Length > 0)
+        {
+            _attackDownAnimation = new CustomAnimation(EnemyControl.Animator, originalMeleeAttack._attackDownAnimation);
+
+            _attackDownAnimation.Events.Add(new(null, _attackDownAnimation.Frames.Length - 1, AnimationEnd));
+            _attackAnimations.Add(_attackDownAnimation);
+
+        }
+        if (originalMeleeAttack._attackLeftAnimation.Frames.Length > 0)
+        {
+            _attackLeftAnimation = new CustomAnimation(EnemyControl.Animator, originalMeleeAttack._attackLeftAnimation);
+
+            _attackLeftAnimation.Events.Add(new(null, _attackLeftAnimation.Frames.Length - 1, AnimationEnd));
+            _attackAnimations.Add(_attackLeftAnimation);
+
+        }
         _triggerDamageFrame = originalMeleeAttack._triggerDamageFrame;
         _attackSfx = originalMeleeAttack._attackSfx;
 
-        _enemyAttackAnimation.Events.Add(new(null, _enemyAttackAnimation.Frames.Length - 1, AnimationEnd));
 
         if (_usesVfx)
         {
             _triggerVFXFrame = originalMeleeAttack._triggerVFXFrame;
+            _vfxRendererOffset = originalMeleeAttack._vfxRendererOffset;
         
             _vfxAnimator = GameObject.Instantiate(GameManager.gm.prefabHolder.Prefabs["AnimatedObject"], enemyControl.transform).GetComponent<CustomAnimator>();
-            
-            _enemyAttackAnimation.Events.Add(new(null, _triggerVFXFrame, TriggerAttackVFX));
+            _vfxAnimator.GetComponent<RendererSortingByY>().Offset = _vfxRendererOffset;
+            _attackVFXAnimation = new CustomAnimation(_vfxAnimator, originalMeleeAttack._attackVFXAnimation);
+
+            foreach (var anim in _attackAnimations)
+            {
+                anim.Events.Add(new(null, _triggerVFXFrame, TriggerAttackVFX));
+
+            }
             _attackVFXAnimation.Events.Add(new(null, _triggerDamageFrame, TriggerDamage));
-            _attackVFXAnimation.Events.Add(new(null, _attackVFXAnimation.Frames.Length -1, () => _vfxAnimator.ChangeAnim("Idle")));
+            _attackVFXAnimation.Events.Add(new(null, _attackVFXAnimation.Frames.Length - 1, () =>
+            {
+                _vfxAnimator.EndAnimation(_attackVFXAnimation.AnimationName);
+                _vfxAnimator.ChangeAnim("Idle");
+            }));
 
 
             CustomAnimation vfxIdleAnim = new CustomAnimation(null, "Idle", new Sprite[1]);
@@ -59,11 +107,15 @@ public class MeleeAttackBehaviour : EnemyBehaviour
         }
         else
         {
-            _enemyAttackAnimation.Events.Add(new(null, _triggerDamageFrame, TriggerDamage));
+            foreach (var anim in _attackAnimations)
+            {
+                anim.Events.Add(new(null, _triggerDamageFrame, TriggerDamage));
+
+            }
 
         }
 
-        EnemyControl.Animator.AddAnimations(new List<CustomAnimation> { _enemyAttackAnimation });
+        EnemyControl.Animator.AddAnimations(_attackAnimations);
     }
     public override void ActiveUpdate()
     {
@@ -78,10 +130,19 @@ public class MeleeAttackBehaviour : EnemyBehaviour
     }
     void Attack()
     {
-        EnemyControl.Animator.ChangeAnim(_enemyAttackAnimation.AnimationName);
+        CustomAnimation currAnim = Utility.GetAnimFromDirection(_attackDirection, _attackUpAnimation, _attackRightAnimation, _attackDownAnimation, _attackLeftAnimation);
+        if (EnemyControl.Animator.CurrAnim == null || EnemyControl.Animator.CurrAnim.AnimationName != currAnim.AnimationName)
+        {
+            if (_attackAnimations.Any(x => x.AnimationName == EnemyControl.Animator.CurrAnim.AnimationName))
+            {
+                EnemyControl.Animator.EndAnimation(EnemyControl.Animator.CurrAnim.AnimationName);
+            }
+            EnemyControl.Animator.ChangeAnim(currAnim.AnimationName);
+        }
     }
     void TriggerAttackVFX()
     {
+        Debug.Log("TIGGE VFX EVENT");
         SoundFXManager.sm.PlaySfx(_attackSfx, EnemyControl.transform.position);
         _vfxAnimator.transform.localPosition = _attackDirection * _attackRange;
         float angle = Mathf.Atan2(_attackDirection.y, _attackDirection.x) * Mathf.Rad2Deg;
@@ -111,7 +172,8 @@ public class MeleeAttackBehaviour : EnemyBehaviour
     public override void KillBehaviour()
     {
         base.KillBehaviour();
-        EnemyControl.Animator.EndAnimation(_enemyAttackAnimation);
+        if(_attackAnimations.Any(x => x.AnimationName == EnemyControl.Animator.CurrAnim.AnimationName))
+            EnemyControl.Animator.EndAnimation(EnemyControl.Animator.CurrAnim.AnimationName);
         _attacked = false;
     }
 }
