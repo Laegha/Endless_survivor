f0d7aee4a77f503d7af1cb465a7f80bd5b08fcc6
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-01-30 16:50:15 -0300

    feat(ChasePlayerBehaviour): Added animations for each direction

--- Endless_Survivor/Assets/Scripts/Enemies/Behaviours/ChasePlayerBehaviour.cs
+++ Endless_Survivor/Assets/Scripts/Enemies/Behaviours/ChasePlayerBehaviour.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [Serializable]
@@ -7,7 +8,12 @@ public class ChasePlayerBehaviour : EnemyBehaviour
 {
     new public static int maxStacks => 1;
     [SerializeField] float _enemySpeed;
-    [SerializeField] CustomAnimation _chasingAnimation;
+    [SerializeField] CustomAnimation _chasingUpAnimation;
+    [SerializeField] CustomAnimation _chasingRightAnimation;
+    [SerializeField] CustomAnimation _chasingDownAnimation;
+    [SerializeField] CustomAnimation _chasingLeftAnimation;
+    
+    List<CustomAnimation> _chasingAnimations = new List<CustomAnimation>();
     Transform _player;
 
     bool _stopped;
@@ -18,10 +24,34 @@ public class ChasePlayerBehaviour : EnemyBehaviour
 
         ChasePlayerBehaviour originalChasePlayer = original as ChasePlayerBehaviour;
         _enemySpeed = originalChasePlayer._enemySpeed;
-        _chasingAnimation = originalChasePlayer._chasingAnimation;
+
+        if (originalChasePlayer._chasingUpAnimation.Frames.Length > 0)
+        {
+            _chasingUpAnimation = new CustomAnimation(EnemyControl.Animator, originalChasePlayer._chasingUpAnimation);
+            _chasingAnimations.Add(_chasingUpAnimation);
+        }
+        if (originalChasePlayer._chasingRightAnimation.Frames.Length > 0)
+        {
+            _chasingRightAnimation = new CustomAnimation(EnemyControl.Animator, originalChasePlayer._chasingRightAnimation);
+            _chasingAnimations.Add(_chasingRightAnimation);
+
+        }
+        if (originalChasePlayer._chasingDownAnimation.Frames.Length > 0)
+        {
+            _chasingDownAnimation = new CustomAnimation(EnemyControl.Animator, originalChasePlayer._chasingDownAnimation);
+            _chasingAnimations.Add(_chasingDownAnimation);
+
+        }
+        if (originalChasePlayer._chasingLeftAnimation.Frames.Length > 0)
+        {
+            _chasingLeftAnimation = new CustomAnimation(EnemyControl.Animator, originalChasePlayer._chasingLeftAnimation);
+            _chasingAnimations.Add(_chasingLeftAnimation);
+
+        }
+
+        EnemyControl.Animator.AddAnimations(_chasingAnimations);
 
         _player = GameObject.FindObjectOfType<PlayerControl>().transform;
-        enemyControl.Animator.AddAnimations(new List<CustomAnimation>() { _chasingAnimation });
     }
 
     public override void PassiveUpdate()
@@ -36,9 +66,19 @@ public class ChasePlayerBehaviour : EnemyBehaviour
         base.ActiveUpdate();
         if (_stopped)
             return;
-        if (EnemyControl.Animator.CurrAnim == null || EnemyControl.Animator.CurrAnim.AnimationName != _chasingAnimation.AnimationName)
-            EnemyControl.Animator.ChangeAnim(_chasingAnimation.AnimationName);
         Vector2 direction = (_player.position - EnemyControl.transform.position).normalized;
+
+        CustomAnimation currAnim = Utility.GetAnimFromDirection(direction, _chasingUpAnimation, _chasingRightAnimation, _chasingDownAnimation, _chasingLeftAnimation);
+        if (EnemyControl.Animator.CurrAnim == null || EnemyControl.Animator.CurrAnim.AnimationName != currAnim.AnimationName)
+        {
+            if (_chasingAnimations.Any(x => x.AnimationName == EnemyControl.Animator.CurrAnim?.AnimationName))
+            {
+                EnemyControl.Animator.EndAnimation(EnemyControl.Animator.CurrAnim.AnimationName);
+            }
+            EnemyControl.Animator.ChangeAnim(currAnim.AnimationName);
+        }
+
+
         EnemyControl.RbForcesController.ChangeCurrForce(new(direction, _enemySpeed, 0, ForceMode2D.Force));
     }
     public override void KillBehaviour()
