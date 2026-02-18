ca6b8ce650df6e1e14e9bff8fcff482169b1af57
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-02-18 18:31:18 -0300

    feat(DestroyAfterTimeSupportObjBehaviour)

---
+++ Endless_Survivor/Assets/Scripts/SupportObjects/Behaviours/DestroyAfterTimeSupportObjBehaviour.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTimeSupportObjBehaviour : SupportObjectBehaviour
+{
+    new public static int maxStacks => 1;
+    [SerializeField] RandomBetweenTwoConstants _destroyTime;
+    public override void Initiate(SupportObjectControl control, SupportObjectBehaviour original)
+    {
+        base.Initiate(control, original);
+        var destroyAfterTimeOriginal = original as DestroyAfterTimeSupportObjBehaviour;
+        _destroyTime = destroyAfterTimeOriginal._destroyTime;
+
+        GameObject.Destroy(ObjControl.gameObject, _destroyTime.rand);
+    }
+}
