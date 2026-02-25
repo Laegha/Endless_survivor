be0960656163824f1a8de31390302e3d44b16225
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-02-25 16:50:16 -0300

    feat(PointBasedChangeCondition): Added a separate static dictionary that holds actions if a weapon has its points reset

--- Endless_Survivor/Assets/Scripts/Weapons/AttackManagement/AttackChangeConditions/PointBasedChangeCondition.cs
+++ Endless_Survivor/Assets/Scripts/Weapons/AttackManagement/AttackChangeConditions/PointBasedChangeCondition.cs
@@ -6,26 +6,40 @@ public class PointBasedChangeCondition : WeaponAttackChangeCondition
 {
     new public static bool isUsable => true;
     static Dictionary<WeaponAttackManager, int> _weaponsPoints = new Dictionary<WeaponAttackManager, int>();
+    static Dictionary<WeaponAttackManager, System.Action> _onPointsReset = new();
     [SerializeField] int _neededPoints;
     [SerializeField] string _triggeredAttackId;
+    [SerializeField] bool _usesPointsOnTrigger;
 
     public static Dictionary<WeaponAttackManager, int> WeaponPoints {  get { return _weaponsPoints; } }
+    public static Dictionary<WeaponAttackManager, System.Action> OnPointsReset {  get { return _onPointsReset; } }
 
     public override void CopyData(WeaponAttackManager weaponAM, WeaponAttackChangeCondition original)
     {
         base.CopyData(weaponAM, original);
         var pointBasedOriginal = original as PointBasedChangeCondition;
         if (!_weaponsPoints.ContainsKey(WeaponAM))
+        {
             _weaponsPoints.Add(weaponAM, 0);
+            _onPointsReset.Add(weaponAM, null);
+        }
         _neededPoints = pointBasedOriginal._neededPoints;
         _triggeredAttackId = pointBasedOriginal._triggeredAttackId;
+        _usesPointsOnTrigger = pointBasedOriginal._usesPointsOnTrigger;
     }
     public override bool IsConditionMet()
     {
-        return _weaponsPoints[WeaponAM] == _neededPoints;
+        bool isMet = _weaponsPoints[WeaponAM] == _neededPoints;
+        if (isMet && _usesPointsOnTrigger)
+        {
+            _onPointsReset[WeaponAM]?.Invoke();
+            _weaponsPoints[WeaponAM] = 0;
+        }
+        return isMet;
     }
     public override WeaponAttackController GetAttackController()
     {
+        Debug.Log("Needed points: " + _neededPoints + " triggering attack " +  _triggeredAttackId);
         return WeaponAM.AttackControllers.Find(x => x.AttackId == _triggeredAttackId);
     }
 }
