baa7eb44f5d92cc82fb12d6354078022d7a6b3f9
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-03-03 18:35:51 -0300

    feat(GiveHPSupportObjBehaviour): Adds an HP component to the support obj and a frame after start subscribes on damage, on heal and on death actions

---
+++ Endless_Survivor/Assets/Scripts/SupportObjects/Behaviours/GiveHPSupportObjBehaviour.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GiveHPSupportObjBehaviour : SupportObjectBehaviour
+{
+    new public static int maxStacks => 1;
+    [SerializeField] int _objMaxHp;
+    HP _supportObjHP;
+    Action _onDamage;
+    Action _onHeal;
+    Action _onDeath;
+    public HP SupportObjHP { get { return _supportObjHP; } }
+    public Action OnDamage { get {  return _onDamage; } set { _onDamage = value; } }
+    public Action OnHeal { get {  return _onHeal; } set { _onHeal= value; } }
+    public Action OnDeath {  get { return _onDeath; } set { _onDeath = value; } }
+    public override void Initiate(SupportObjectControl control, SupportObjectBehaviour original)
+    {
+        base.Initiate(control, original);
+        var giveHPOriginal = original as GiveHPSupportObjBehaviour;
+        _objMaxHp = giveHPOriginal._objMaxHp;
+        _supportObjHP = ObjControl.AddComponent<HP>();
+        _supportObjHP.InitializeHP(_objMaxHp);
+        OnStart += () => GameManager.gm.DelayActionAFrame(SubscribeActionsToHP, null);
+    }
+
+    void SubscribeActionsToHP()
+    {
+        _supportObjHP.OnDamageTaken += _onDamage;
+        _supportObjHP.OnDamageTaken += CheckDeath;
+        _supportObjHP.OnHealed += _onHeal;
+        
+    }
+
+    void CheckDeath()
+    {
+        if (_supportObjHP.RemainingHP > 0)
+            return;
+        _onDeath?.Invoke();
+    }
+}
