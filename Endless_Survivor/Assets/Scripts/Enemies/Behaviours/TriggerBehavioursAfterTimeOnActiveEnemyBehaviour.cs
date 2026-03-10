51f4be6d59b3858644ab2d20eaa6f3a6d584adb6
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-03-10 04:20:17 -0300

    feat(TriggerBehavioursAfterTimeOnActiveEnemyBehaviour): has a timer that increases while active and a bool to activate itself on start

---
+++ Endless_Survivor/Assets/Scripts/Enemies/Behaviours/TriggerBehavioursAfterTimeOnActiveEnemyBehaviour.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerBehavioursAfterTimeOnActiveEnemyBehaviour : EnemyBehaviour
+{
+    new public static int maxStacks => 1;
+    [SerializeField] List<string> _triggeredBehaviours;
+    [SerializeField] float _timeToTrigger;
+    [SerializeField] bool _triggerOnStart;
+
+    float _timer;
+
+    public override void Initialize(EnemyBehaviour original, EnemyControl enemyControl)
+    {
+        base.Initialize(original, enemyControl);
+        var triggerAfterTimeOriginal = original as TriggerBehavioursAfterTimeOnActiveEnemyBehaviour;
+        _triggeredBehaviours = triggerAfterTimeOriginal._triggeredBehaviours;
+        _timeToTrigger = triggerAfterTimeOriginal._timeToTrigger;
+        _triggerOnStart = triggerAfterTimeOriginal._triggerOnStart;
+    }
+    public override void Start()
+    {
+        base.Start();
+        if(_triggerOnStart)
+            EnemyControl.BehaviourManager.ActivateBehaviour(this);
+    }
+    public override void ActiveUpdate()
+    {
+        base.PassiveUpdate();
+        _timer += Time.deltaTime;
+        if (_timer < _timeToTrigger)
+            return;
+        _timer = 0;
+        foreach (var triggeredBehaviour in _triggeredBehaviours)
+            EnemyControl.BehaviourManager.ActivateBehaviour(triggeredBehaviour);
+        KillBehaviour();
+    }
+
+}
