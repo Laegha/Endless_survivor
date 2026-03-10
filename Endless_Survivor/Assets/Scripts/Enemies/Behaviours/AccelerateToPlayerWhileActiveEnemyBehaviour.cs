72228859ad992bfda837a0444dd2354b50697d2c
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-03-10 04:08:27 -0300

    feat(AccelerateToPlayerWhileActiveEnemyBehaviour): On active, updates the rb force to the direction of the player with a speed that accelerates

---
+++ Endless_Survivor/Assets/Scripts/Enemies/Behaviours/AccelerateToPlayerWhileActiveEnemyBehaviour.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class AccelerateToPlayerWhileActiveEnemyBehaviour : EnemyBehaviour
+{
+    new public static int maxStacks => -1;
+    [SerializeField] float _acceleration;
+    [SerializeField] float _initialSpeed;
+    [SerializeField] float _maxSpeed;
+    [SerializeField] bool _instantStopOnUnactive;
+    [Tooltip("Only use if instantStopOnUnactive")][SerializeField] float _deacceleration;
+    [SerializeField] DirectionalCustomAnimation _movingAnimations;
+
+    bool _isDeaccelerating;
+    float _speed;
+    Vector2 _lastDirection;
+
+    public override void Initialize(EnemyBehaviour original, EnemyControl enemyControl)
+    {
+        base.Initialize(original, enemyControl);
+        var accelerateOriginal = original as AccelerateToPlayerWhileActiveEnemyBehaviour;
+        _acceleration = accelerateOriginal._acceleration;
+        _initialSpeed = accelerateOriginal._initialSpeed;
+        _maxSpeed = accelerateOriginal._maxSpeed;
+        _instantStopOnUnactive = accelerateOriginal._instantStopOnUnactive;
+        _deacceleration = accelerateOriginal._deacceleration;
+
+        _movingAnimations = new(EnemyControl.Animator, accelerateOriginal._movingAnimations);
+        EnemyControl.Animator.AddAnimations(_movingAnimations.NonNullAnimations);
+    }
+
+    public override void PassiveUpdate()
+    {
+        base.PassiveUpdate();
+        if (_instantStopOnUnactive)
+            return;
+        if(IsActive || !_isDeaccelerating)
+            return;
+        _speed -= _deacceleration * Time.deltaTime;
+        EnemyControl.RbForcesController.ChangeCurrForce(new(_lastDirection, _speed, 0, ForceMode2D.Force));
+        if(_speed <= 0)
+        {
+            _isDeaccelerating = false;
+            _speed = _initialSpeed;
+            EnemyControl.Animator.EndAnimation(_movingAnimations.GetAnim(_lastDirection));
+        }
+
+    }
+    public override void ActiveUpdate()
+    {
+        base.ActiveUpdate();
+        Vector2 direction = (PlayerControl.pc.transform.position - EnemyControl.transform.position).normalized;
+        _lastDirection = direction;
+
+        var currAnim = _movingAnimations.GetAnim(direction);
+        if (EnemyControl.Animator.CurrAnim != null && currAnim.AnimationName != EnemyControl.Animator.CurrAnim.AnimationName && _movingAnimations.NonNullAnimations.Any(x => x.AnimationName == EnemyControl.Animator.CurrAnim.AnimationName))
+            EnemyControl.Animator.EndAnimation(EnemyControl.Animator.CurrAnim.AnimationName);
+
+        EnemyControl.Animator.ChangeAnim(currAnim.AnimationName);
+
+        EnemyControl.RbForcesController.ChangeCurrForce(new(direction, _speed, 0, ForceMode2D.Force));
+        if(_speed < _maxSpeed)
+            _speed += _acceleration * Time.deltaTime;
+    }
+    public override void KillBehaviour()
+    {
+        base.KillBehaviour();
+        EnemyControl.RbForcesController.Stop();
+        if (_instantStopOnUnactive)
+        {
+            _speed = _initialSpeed;
+            EnemyControl.Animator.EndAnimation(_movingAnimations.GetAnim(_lastDirection));
+        }
+        else
+            _isDeaccelerating = true;
+    }
+}
