2ad1c922e31e49bd38840e25359d2b0e399d3727
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-03-16 16:14:34 -0300

    refactor(PlayerSpawner): Instead of giving offset to the collider, move every other object of the player in the opposite direction so that the collider stays on 0,0 , where the enemy bullets are aimed

--- Endless_Survivor/Assets/Scripts/Player/PlayerSpawner.cs
+++ Endless_Survivor/Assets/Scripts/Player/PlayerSpawner.cs
@@ -17,8 +17,17 @@ public class PlayerSpawner : MonoBehaviour
         playerControl.WeaponManager.MaxWeapons = selectedChar.InitialMaxWeapons;
         CapsuleCollider2D playerCollider = player.GetComponent<CapsuleCollider2D>();
         playerCollider.size = selectedChar.ColliderSize;
-        playerCollider.offset = selectedChar.ColliderOffset;
+        playerCollider.offset = Vector2.zero;
+        //playerCollider.offset = selectedChar.ColliderOffset;
         playerCollider.direction = selectedChar.ColliderDirection;
+
+        foreach(Transform child in playerControl.transform.GetComponentInChildren<Transform>())
+        {
+            if(child == playerCollider.transform) 
+                continue;
+            child.localPosition -= new Vector3(selectedChar.ColliderOffset.x, selectedChar.ColliderOffset.y, 0);
+        }
+
         //generate initial weapons and passives
         foreach(WeaponData weaponData in selectedChar.InitialWeapons)
         {
