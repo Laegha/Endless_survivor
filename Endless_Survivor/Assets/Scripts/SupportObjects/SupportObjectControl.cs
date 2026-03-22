fa837536c0fc48e23192c7d07fac4fd21ca0fbfa
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-03-22 19:13:19 -0300

    feat(SupportObjectControl): Reference to the RendererSortingByY

--- Endless_Survivor/Assets/Scripts/SupportObjects/SupportObjectControl.cs
+++ Endless_Survivor/Assets/Scripts/SupportObjects/SupportObjectControl.cs
@@ -8,9 +8,11 @@ public class SupportObjectControl : MonoBehaviour
     [SerializeField] Transform _colliderHolder;
     [SerializeField] CustomAnimator _animator;
     [SerializeField] List<SpriteRenderer> _renderers;
+    [SerializeField] RendererSortingByY _rendererSorter;
 
     public SupportObjectBehaviourManager BehaviourManager { get { return _behaviourManager; } }
     public Transform ColliderHolder { get { return _colliderHolder; } }
     public CustomAnimator Animator { get { return _animator; } set { _animator = value; } }
     public List<SpriteRenderer> Renderers { get { return _renderers; } }
+    public RendererSortingByY RendererSorter { get { return _rendererSorter; } }
 }
