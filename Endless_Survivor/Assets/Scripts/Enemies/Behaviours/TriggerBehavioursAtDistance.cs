6182ee411ec9676752fdf74b6e9c9b54bd65c883
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-04-01 02:08:53 -0300

    feat(TriggerBehavioursAtDistance): trigger when far and wait for additional behaviours as cooldown to trigger again, aside from the ones this behaviour activates

--- Endless_Survivor/Assets/Scripts/Enemies/Behaviours/TriggerBehavioursAtDistance.cs
+++ Endless_Survivor/Assets/Scripts/Enemies/Behaviours/TriggerBehavioursAtDistance.cs
@@ -7,6 +7,8 @@ public class TriggerBehavioursAtDistance : EnemyBehaviour
     new public static int maxStacks => -1;
     [SerializeReference] List<string> _behavioursToTrigger = new List<string>();
     [SerializeField] float _distanceToTrigger;
+    [SerializeField] bool _triggerWhenFar;
+    [SerializeField] List<string> _behavioursToWaitBeforeTriggeringAgain;
     Transform _player;
 
     public override void Initialize(EnemyBehaviour original, EnemyControl enemyControl)
@@ -15,6 +17,15 @@ public class TriggerBehavioursAtDistance : EnemyBehaviour
         TriggerBehavioursAtDistance originalTriggerBehavioursAtDistance = original as TriggerBehavioursAtDistance;
         _behavioursToTrigger = originalTriggerBehavioursAtDistance._behavioursToTrigger;
         _distanceToTrigger = originalTriggerBehavioursAtDistance._distanceToTrigger;
+        _triggerWhenFar = originalTriggerBehavioursAtDistance._triggerWhenFar;
+        _behavioursToWaitBeforeTriggeringAgain = new(originalTriggerBehavioursAtDistance._behavioursToWaitBeforeTriggeringAgain);
+        foreach(string behaviour in _behavioursToTrigger)
+        {
+            if (_behavioursToWaitBeforeTriggeringAgain.Contains(behaviour))
+                continue;
+            _behavioursToWaitBeforeTriggeringAgain.Add(behaviour);
+        }
+
         _player = GameObject.FindObjectOfType<PlayerControl>().transform;
     }
     public override void PassiveUpdate()
@@ -23,7 +34,7 @@ public class TriggerBehavioursAtDistance : EnemyBehaviour
         if(IsActive)
             return;
         float distance = Vector2.Distance(EnemyControl.transform.position, _player.position);
-        if(distance <= _distanceToTrigger)
+        if((!_triggerWhenFar && distance <= _distanceToTrigger) || (_triggerWhenFar && distance >= _distanceToTrigger))
         {
             foreach (var behaviour in _behavioursToTrigger)
                 EnemyControl.BehaviourManager.ActivateBehaviour(behaviour);
@@ -33,7 +44,7 @@ public class TriggerBehavioursAtDistance : EnemyBehaviour
     public override void ActiveUpdate()
     {
         base.ActiveUpdate();
-        foreach (var behaviour in _behavioursToTrigger)
+        foreach (var behaviour in _behavioursToWaitBeforeTriggeringAgain)
         {
             if (EnemyControl.BehaviourManager.GetBehaviour(behaviour).IsActive)
                 return;
