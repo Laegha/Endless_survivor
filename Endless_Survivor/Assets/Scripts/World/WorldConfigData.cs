abb3b3994d92a6bae582786223dc4eb017ab5333
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-04-01 02:13:09 -0300

    feat(WorldConfigData): Scriptable object that holds the configuration for instensity and enemy spawning stuff

---
+++ Endless_Survivor/Assets/Scripts/World/WorldConfigData.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldConfigData : ScriptableObject
+{
+    [SerializeField] float _minEnemySpawnDist;
+    [SerializeField] float _maxEnemySpawnDist;
+    [SerializeField] RandomBetweenTwoConstants _timeBetweenEnemySpawn;
+    [SerializeField] RandomBetweenTwoConstants _enemiesPerSpawn;
+    [SerializeField] int _initialIntensityGoal;
+    [SerializeField] int _intensityGoalIncrease;
+    [SerializeField] int _enemyHPPercentToIntensityIncrease;
+    [SerializeField] int _intensityLevelsForNewBiome;
+
+    public float MinEnemySpawnDist { get { return _minEnemySpawnDist; } }
+    public float MaxEnemySpawnDist { get { return _maxEnemySpawnDist; } }
+    public RandomBetweenTwoConstants TimeBetweenEnemySpawn { get { return _timeBetweenEnemySpawn; } }
+    public RandomBetweenTwoConstants EnemiesPerSpawn { get { return _enemiesPerSpawn; } }
+    public int InitialInstensityGoal { get { return _initialIntensityGoal; } }
+    public int IntensityGoalIncrease { get { return _intensityGoalIncrease; } }
+    public int EnemyHPPercentToIntensityIncrease { get { return _enemyHPPercentToIntensityIncrease; } }
+    public int IntensityLevelsForNewBiome { get { return _intensityLevelsForNewBiome; } }
+}
