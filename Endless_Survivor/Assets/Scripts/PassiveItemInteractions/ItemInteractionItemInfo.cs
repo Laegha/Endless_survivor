687be89ac0754487620355aba31ad4abaa3fe36c
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-05-19 21:13:59 -0300

    feat(ItemInteractionItemInfo): stores the information for one of the items in the interaction, such as its item data, count, and if it's removed upon interaction

---
+++ Endless_Survivor/Assets/Scripts/PassiveItemInteractions/ItemInteractionItemInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInteractionItemInfo
+{
+    public PassiveItemData itemNeededForTheInteraction;
+    public int neededCount;
+    public bool isDestroyed;
+}
