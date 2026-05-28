2451588f45d9914371b518df0dd1f94546fa7e9e
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-05-28 13:29:35 -0300

    feat(PickupDataWithSpawnAnim): Class that handles the spawn of a pickup after playing an animation with an animated object

---
+++ Endless_Survivor/Assets/Scripts/Reusable/PickupDataWithSpawnAnim.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class PickupDataWithSpawnAnim
+{
+    [SerializeField] CustomAnimation _spawnAnim;
+    [SerializeField] PickupData _pickupData;
+
+    public void SpawnPickupAfterAnim(Vector2 spawnPos, float delayAfterAnimEnd = 0)
+    {
+        float animatedObjDuration = _spawnAnim.AnimDuration + delayAfterAnimEnd;
+        AnimatedObjConfig animationConfig = new(_spawnAnim, spawnPos, Quaternion.identity, animatedObjDuration);
+        GameManager.gm.DelayAction(animatedObjDuration, () => Utility.GeneratePickup(_pickupData, spawnPos), null);
+    }
+}
