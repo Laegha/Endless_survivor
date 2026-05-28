4bf2c99878c99c6a0ff6aeb6b7b740c8caf019be
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-05-28 13:56:44 -0300

    feat(IncreasePlayerStatsPassiveItemBehaviour): Increases stats when picked, reduces stats when removed

---
+++ Endless_Survivor/Assets/Scripts/PassiveItems/Behaviours/IncreasePlayerStatsPassiveItemBehaviour.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncreasePlayerStatsPassiveItemBehaviour : PassiveItemBehaviour
+{
+    new public static int maxStacks => 1;
+    [SerializeField] PlayerStats _increasingStats;
+    PlayerStats _inversedIncreasingStats;
+    public override void CopyValues(PassiveItemBehaviour original, PassiveItemBehaviourManager behaviourManager)
+    {
+        base.CopyValues(original, behaviourManager);
+        _inversedIncreasingStats = new(_increasingStats);
+        _inversedIncreasingStats.HPRegeneration *= -1;
+        _inversedIncreasingStats.Defense *= -1;
+        _inversedIncreasingStats.MaxSpeed *= -1;
+        _inversedIncreasingStats.MinSpeed *= -1;
+        _inversedIncreasingStats.Acceleration *= -1;
+
+        behaviourManager.onPicked += IncreaseStats;
+
+    }
+    void IncreaseStats()
+    {
+        PlayerControl.pc.PlayerStats.IncreaseStats(_increasingStats);
+    }
+    public override void RemoveBehaviour()
+    {
+        PlayerControl.pc.PlayerStats.IncreaseStats(_inversedIncreasingStats);
+        
+    }
+}
