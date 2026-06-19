1148158f942a59d83e80e543dc07d111378b7e97
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-06-19 03:55:46 -0300

    feat(RunExit): Component that ends run with a win when player enters trigger

---
+++ Endless_Survivor/Assets/Scripts/MapManaging/RunExit.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RunExit : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.root != PlayerControl.pc.transform)
+            return;
+
+        GameProgressionManager.gpm.EndRun(PlayerControl.pc.CharacterData.ExitRunAnimation, GameProgressionManager.RunEndType.Win);
+    }
+}
