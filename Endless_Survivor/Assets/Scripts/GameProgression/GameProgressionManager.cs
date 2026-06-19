6cdbfc4479aa74b4728d11e99f6fd0e400f129c2
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-06-19 03:54:02 -0300

    feat(GameProgressionManager): End run management, playing a given animation on the player and changing to a win or lose scene after a certain delay

--- Endless_Survivor/Assets/Scripts/GameProgression/GameProgressionManager.cs
+++ Endless_Survivor/Assets/Scripts/GameProgression/GameProgressionManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class GameProgressionManager : MonoBehaviour
 {
+    public enum RunEndType
+    {
+        Win,
+        Lose
+    }
     static GameProgressionManager instance;
     public static GameProgressionManager gpm
     {
@@ -12,16 +17,44 @@ public class GameProgressionManager : MonoBehaviour
             return instance;
         }
     }
-    int _totalBosses = -1;
+    const float _endRunDelay = 1;
+    int _totalBosses = 0;
     int _killedBosses = 0;
-    public void BiomeGenerated(int addedBosses)
+    private void Awake()
     {
-        _totalBosses += addedBosses;
+        instance = this;
+    }
+    public void AddBoss()
+    {
+        _totalBosses ++;
     }
     public void BossKilled()
     {
         _killedBosses++;
         if (_killedBosses >= _totalBosses)
             Debug.Log("KILLED ALL BOSSES");
+    }
+    public void EndRun(CustomAnimation playerAnimation, RunEndType endType)
+    {
+        Time.timeScale = 0;
+        PlayerControl.pc.gameObject.SetActive(false);
+        AnimatedObjConfig playerAnimConfig = new(playerAnimation, PlayerControl.pc.transform.position, Quaternion.identity, playerAnimation.AnimDuration);
+        var exitCharAnimator = AnimatedObjsManager.aom.SpawnAnimatedObj(playerAnimConfig);
+        //exitCharAnimator.AffectedByTimeScale = false;
+        float timeToEndRun = playerAnimation.AnimDuration + _endRunDelay;
+        GameManager.gm.DelayAction(timeToEndRun, () => GameProgressionManager.gpm.LoadScreen(endType), null);
+    }
+
+    void LoadScreen(RunEndType endType)
+    {
+        switch(endType)
+        {
+            case RunEndType.Win:
+                SceneLoadingFunctions.slf.Win();
+                break;
+            case RunEndType.Lose:
+                SceneLoadingFunctions.slf.GameOver();
+                break;
+        }
     }
 }
