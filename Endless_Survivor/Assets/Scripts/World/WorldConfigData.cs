282336f2dc0f3fe20f045c51a19401c2aff1711a
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-06-20 03:22:04 -0300

    feat(WorldConfigData): Info for run exit, prefab, message and pointer

--- Endless_Survivor/Assets/Scripts/World/WorldConfigData.cs
+++ Endless_Survivor/Assets/Scripts/World/WorldConfigData.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New WorldConfig", menuName = "ScriptableObjects/World config", order = 0)]
@@ -14,7 +15,11 @@ public class WorldConfigData : ScriptableObject
     [SerializeField] int _enemyHPPercentToIntensityIncrease;
     [SerializeField] int _intensityLevelsForNewBiome;
     [SerializeField] UIMessageInfo _intensityIncreaseMessageInfo;
-
+    [InspectorLabel("Exit run info")]
+    [SerializeField] RunExit _runExitPrefab;
+    [SerializeField] UIMessageInfo _onExitCreatedMessageInfo;
+    [SerializeField] Color _exitPointerColor;
+    [SerializeField] Sprite _exitPointerIcon;
     public float MinEnemySpawnDist { get { return _minEnemySpawnDist; } }
     public float MaxEnemySpawnDist { get { return _maxEnemySpawnDist; } }
     public RandomBetweenTwoConstants TimeBetweenEnemySpawn { get { return _timeBetweenEnemySpawn; } }
@@ -24,4 +29,8 @@ public class WorldConfigData : ScriptableObject
     public int EnemyHPPercentToIntensityIncrease { get { return _enemyHPPercentToIntensityIncrease; } }
     public int IntensityLevelsForNewBiome { get { return _intensityLevelsForNewBiome; } }
     public UIMessageInfo IntensityIncreaseMessageInfo {  get { return _intensityIncreaseMessageInfo; } }
+    public RunExit RunExitPrefab { get { return _runExitPrefab; } }
+    public UIMessageInfo OnExitCreatedMessageInfo { get { return _onExitCreatedMessageInfo; } }
+    public Color ExitPointerColor { get { return _exitPointerColor; } } 
+    public Sprite ExitPointerIcon { get { return _exitPointerIcon; } } 
 }
