24e2e3d07041c6d0097d5516997bf0c6b2f4f0ac
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-07-29 01:29:26 -0300

    build: Add a folder for official builds and a separate one for APKs for quick testing

---
+++ EndlessInvasion_Builds/AppBundles/Beta_0.1_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,41 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+IL2CPP_EXTERN_C_CONST int32_t* g_FieldOffsetTable[];
+IL2CPP_EXTERN_C_CONST Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	11389,
+	g_Il2CppGenericTypes,
+	7610,
+	g_Il2CppGenericInstTable,
+	76986,
+	g_Il2CppGenericMethodFunctions,
+	37532,
+	g_Il2CppTypeTable,
+	95149,
+	g_Il2CppMethodSpecTable,
+	11522,
+	g_FieldOffsetTable,
+	11522,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
