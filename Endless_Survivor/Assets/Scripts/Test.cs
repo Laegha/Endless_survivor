abdc65628c07dba24863aaf38f04a36a3ff5077a
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-08-04 04:47:37 -0300

    SO Update

--- Endless_Survivor/Assets/Scripts/Test.cs
+++ Endless_Survivor/Assets/Scripts/Test.cs
@@ -5,14 +5,7 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    [SerializeField] LineXInfo _lineinf;
-    [SerializeField] LineRenderer _line;
-    LineXInfo lineinf;
-    private void Start()
-    {
-        lineinf = new(_lineinf, Vector2.zero, Vector2.right, 5);
-        lineinf.DrawLine(_line);
-    }
+
     private void Update()
     {
 
