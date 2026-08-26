58f4af65320b4de1e4e92126b69ada9bcd80f0ff
Francisco Larghero <110439827+Laegha@users.noreply.github.com>
2026-08-26 13:53:10 -0300

    fix(ILinePattern): Start position now depends on count when centered

--- Endless_Survivor/Assets/Scripts/Interfaces/Patterns/ILinePattern.cs
+++ Endless_Survivor/Assets/Scripts/Interfaces/Patterns/ILinePattern.cs
@@ -16,7 +16,7 @@ public class ILinePattern : IPattern
     public IEnumerable<Vector2> GetPositions(Vector2 origin, int count)
     {
         Vector2 newSpacing = new(_spacing.x * (_hAlignment == HorizontalAlignment.Right ? -1 : 1), _spacing.y * (_vAlignment == VerticalAlignment.Top ? -1 : 1));
-        Vector2 startPos = origin - new Vector2(_hAlignment == HorizontalAlignment.Center ? newSpacing.x / 2 : 0, _vAlignment == VerticalAlignment.Center ? newSpacing.y / 2 : 0);
+        Vector2 startPos = origin - new Vector2(_hAlignment == HorizontalAlignment.Center ? newSpacing.x * (count -1) / 2 : 0, _vAlignment == VerticalAlignment.Center ? newSpacing.y * (count - 1) / 2 : 0);
         
         for (int i = 0; i < count; i++)
         {
